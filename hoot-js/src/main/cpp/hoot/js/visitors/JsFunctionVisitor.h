ec9e4ec68897f2086cff5ae75c3d948e81e48a6c
Jason R. Surratt <jason.surratt@digitalglobe.com>
2015-09-03 16:00:08 -0600

    Created a script for marking pois for validation
    1. refs #7018 Create script for preparing a data set for validation

--- hoot-js/src/main/cpp/hoot/js/visitors/JsFunctionVisitor.h
+++ hoot-js/src/main/cpp/hoot/js/visitors/JsFunctionVisitor.h
@@ -2,6 +2,7 @@
 #define JSFUNCTIONVISITOR_H
 
 // hoot
+#include <hoot/core/OsmMapConsumer.h>
 #include <hoot/core/elements/ElementVisitor.h>
 #include <hoot/core/schema/OsmSchema.h>
 #include <hoot/js/util/JsFunctionConsumer.h>
@@ -13,20 +14,25 @@ namespace hoot
 /**
  * A filter that will either keep or remove matches.
  */
-class JsFunctionVisitor : public ElementVisitor, public JsFunctionConsumer
+class JsFunctionVisitor : public ElementVisitor, public OsmMapConsumer, public JsFunctionConsumer
 {
 public:
 
   static string className() { return "hoot::JsFunctionVisitor"; }
 
-  JsFunctionVisitor() {}
+  JsFunctionVisitor() : _map(0) {}
 
   virtual void addFunction(Persistent<Function> func) { _func = Persistent<Function>(func); }
+
+  virtual void setOsmMap(OsmMap* map) { _map = map; }
+
+  virtual void setOsmMap(const OsmMap*) {}
 
   virtual void visit(const ConstElementPtr& e);
 
 private:
   Persistent<Function> _func;
+  OsmMap* _map;
 };
 
 }
