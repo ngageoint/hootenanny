092eef24199a846c99ae1891eb68abd4c5b4cc54
Brian Hatchl <brian.hatchl@digitalglobe.com>
2015-10-09 17:45:47 -0400

    remove commented out code in test

--- hoot-services/src/test/java/hoot/services/db/postgres/PostgresUtilsTest.java
+++ hoot-services/src/test/java/hoot/services/db/postgres/PostgresUtilsTest.java
@@ -39,22 +39,14 @@ public class PostgresUtilsTest {
 		+ "\"hoot:review:choices:2\"=>\"{\\\"label\\\":\\\"Assessed\\\",\\\"description\\\":\\\"The point is on a building, but you can't verify its type (e.g. hair salon).\\\",\\\"changes\\\":{\\\"replaceTags\\\":{\\\"hgis:imagery_confirmed\\\":\\\"assessed\\\",\\\"hgis:accuracy\\\":\\\"high\\\"},\\\"appendTags\\\":{\\\"source:geometry\\\":\\\"${BASEMAP_IMAGE_SOURCE}\\\",\\\"source:geometry:sensor\\\":\\\"${BASEMAP_IMAGE_SENSOR}\\\",\\\"source:geometry:date\\\":\\\"${BASEMAP_IMAGE_DATETIME}\\\",\\\"source:geometry:id\\\":\\\"${BASEMAP_IMAGE_ID}\\\"}}}\", "
 		+ "\"hoot:review:choices:3\"=>\"{\\\"label\\\":\\\"Reported\\\",\\\"description\\\":\\\"Imagery is pixelated or cloudy -- can not be assessed.\\\",\\\"changes\\\":{\\\"replaceTags\\\":{\\\"hgis:imagery_confirmed\\\":\\\"reported\\\"},\\\"appendTags\\\":{\\\"source:geometry\\\":\\\"${BASEMAP_IMAGE_SOURCE}\\\",\\\"source:geometry:sensor\\\":\\\"${BASEMAP_IMAGE_SENSOR}\\\",\\\"source:geometry:date\\\":\\\"${BASEMAP_IMAGE_DATETIME}\\\",\\\"source:geometry:id\\\":\\\"${BASEMAP_IMAGE_ID}\\\"}}}\"";
 
-		// Old one is missing last \" so parser fails
-		/*	String fullExample = "\"poi\"=>\"yes\", \"name\"=>\"Garden of the Gods\", \"uuid\"=>\"{87ba9abc-16dc-4e59-a9cc-fef70051fd97}\", "
-		+ "\"leisure\"=>\"park\", \"hoot:status\"=>\"1\", \"error:circular\"=>\"1000\", \"hoot:review:note\"=>\"Flagged for imagery validation\",\"hoot:review:needs\"=>\"yes\", "
-		+ "\"hoot:review:choices:1\"=>\"{\\\"label\\\":\\\"Confirmed\\\",\\\"description\\\":\\\"You can look at the point and tell what it is (e.g. mosque or airport)\\\",\\\"changes\\\":{\\\"replaceTags\\\":{\\\"hgis:imagery_confirmed\\\":\\\"confirmed\\\",\\\"hgis:accuracy\\\":\\\"high\\\"},\\\"appendTags\\\":{\\\"source:geometry\\\":\\\"${BASEMAP_IMAGE_SOURCE}\\\",\\\"source:geometry:sensor\\\":\\\"${BASEMAP_IMAGE_SENSOR}\\\",\\\"source:geometry:date\\\":\\\"${BASEMAP_IMAGE_DATETIME}\\\",\\\"source:geometry:id\\\":\\\"${BASEMAP_IMAGE_ID}\\\"}}}\", "
-		+ "\"hoot:review:choices:2\"=>\\\"{\\\"label\\\":\\\"Assessed\\\",\\\"description\\\":\\\"The point is on a building, but you can't verify its type (e.g. hair salon).\\\",\\\"changes\\\":{\\\"replaceTags\\\":{\\\"hgis:imagery_confirmed\\\":\\\"assessed\\\",\\\"hgis:accuracy\\\":\\\"high\\\"},\\\"appendTags\\\":{\\\"source:geometry\\\":\\\"${BASEMAP_IMAGE_SOURCE}\\\",\\\"source:geometry:sensor\\\":\\\"${BASEMAP_IMAGE_SENSOR}\\\",\\\"source:geometry:date\\\":\\\"${BASEMAP_IMAGE_DATETIME}\\\",\\\"source:geometry:id\\\":\\\"${BASEMAP_IMAGE_ID}\\\"}}}\", "
-		+ "\"hoot:review:choices:3\"=>\\\"{\\\"label\\\":\\\"Reported\\\",\\\"description\\\":\\\"Imagery is pixelated or cloudy -- can not be assessed.\\\",\\\"changes\\\":{\\\"replaceTags\\\":{\\\"hgis:imagery_confirmed\\\":\\\"reported\\\"},\\\"appendTags\\\":{\\\"source:geometry\\\":\\\"${BASEMAP_IMAGE_SOURCE}\\\",\\\"source:geometry:sensor\\\":\\\"${BASEMAP_IMAGE_SENSOR}\\\",\\\"source:geometry:date\\\":\\\"${BASEMAP_IMAGE_DATETIME}\\\",\\\"source:geometry:id\\\":\\\"${BASEMAP_IMAGE_ID}\\\"}}}";
-		*/Map<String,String> tags = PostgresUtils._parseTags(fullExample);
+		Map<String,String> tags = PostgresUtils._parseTags(fullExample);
 		Assert.assertNotNull(tags);
 		Assert.assertEquals(tags.size(), 11);
 		Assert.assertEquals(tags.get("poi").toString(), "yes");
 		Assert.assertEquals(tags.get("hoot:review:note").toString(), "Flagged for imagery validation");
-/*		Assert.assertEquals("\"{\\\"label\\\":\\\"Assessed\\\",\\\"description\\\":\\\"The point is on a building, but you can't verify its type (e.g. hair salon).\\\",\\\"changes\\\":{\\\"replaceTags\\\":{\\\"hgis:imagery_confirmed\\\":\\\"assessed\\\",\\\"hgis:accuracy\\\":\\\"high\\\"},\\\"appendTags\\\":{\\\"source:geometry\\\":\\\"${BASEMAP_IMAGE_SOURCE}\\\",\\\"source:geometry:sensor\\\":\\\"${BASEMAP_IMAGE_SENSOR}\\\",\\\"source:geometry:date\\\":\\\"${BASEMAP_IMAGE_DATETIME}\\\",\\\"source:geometry:id\\\":\\\"${BASEMAP_IMAGE_ID}\\\"}}}\"",
-			tags.get("hoot:review:choices:2").toString());*/
-		
+
 		String expected = "{\\\"label\\\":\\\"Assessed\\\",\\\"description\\\":\\\"The point is on a building, but you can't verify its type (e.g. hair salon).\\\",\\\"changes\\\":{\\\"replaceTags\\\":{\\\"hgis:imagery_confirmed\\\":\\\"assessed\\\",\\\"hgis:accuracy\\\":\\\"high\\\"},\\\"appendTags\\\":{\\\"source:geometry\\\":\\\"${BASEMAP_IMAGE_SOURCE}\\\",\\\"source:geometry:sensor\\\":\\\"${BASEMAP_IMAGE_SENSOR}\\\",\\\"source:geometry:date\\\":\\\"${BASEMAP_IMAGE_DATETIME}\\\",\\\"source:geometry:id\\\":\\\"${BASEMAP_IMAGE_ID}\\\"}}}";
-		 
+
 		Assert.assertEquals(expected,
 				tags.get("hoot:review:choices:2").toString());
 	}
