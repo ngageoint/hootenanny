161807f4f18112ba7f5f9f1307c4e61fbe7729f7
Greg Schmidt <greg.schmidt@digitalglobe.com>
2015-10-29 18:26:42 -0400

    Updated DB vars to use DB_ instead of PG<vars>
    Changes made:
    - updated to use the DB_ vars instead of the psql global vars
    - udpated setup, clean and ServicesOsmApiDbTEst code to use those vars instead of PG...
    - added the PGPASSWORD var back... but in locally only...needed for removing password prompt
    
    Changes Description:
    - n/a

--- scripts/CleanOsmApiDB.sh
+++ scripts/CleanOsmApiDB.sh
@@ -3,11 +3,12 @@
 # setup db, user, and password to avoid password prompt
 source $HOOT_HOME/conf/DatabaseConfig.sh
 
-export AUTH="-h $PGHOST -p $PGPORT -U $PGUSER"
+export PGPASSWORD=$DB_PASSWORD
+export AUTH="-h $DB_HOST -p $DB_PORT -U $DB_USER"
 
 # see if old db osmapi_test exists
-export flag=`psql $AUTH -lqt | cut -d \| -f 1 | grep -w $PGDATABASE | wc -l`
+export flag=`psql $AUTH -lqt | cut -d \| -f 1 | grep -w $DB_NAME_OSMAPI | wc -l`
 if [ "$flag" = "1" ]; then
 #  echo "Dropping old osm api db"
-  dropdb $AUTH $PGDATABASE
+  dropdb $AUTH $DB_NAME_OSMAPI
 fi
