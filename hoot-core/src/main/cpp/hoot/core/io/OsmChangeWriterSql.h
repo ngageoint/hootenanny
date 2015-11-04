1a92025d85af2df271ac73dbbcbc3754838108ff
Terry Ott <terry.ott@digitalglobe.com>
2015-11-04 10:22:51 -0500

    Worked on shared interface

--- hoot-core/src/main/cpp/hoot/core/io/OsmChangeWriterSql.h
+++ hoot-core/src/main/cpp/hoot/core/io/OsmChangeWriterSql.h
@@ -18,15 +18,42 @@ class OsmChangeWriterSql
 public:
   OsmChangeWriterSql();
 
+  // Jason
   void write(const QString& path, const ChangeSetProviderPtr cs);
 
 
 private:
   QFile _outputSql;
 
+  Tgs::BigMap<long, long> _idMappingsNode;
+  Tgs::BigMap<long, long> _idMappingsWay;
+  Tgs::BigMap<long, long> _idMappingsRelation;
+
+  // jason
   void _writeNewElement(const ConstElementPtr newElement);
+  // Terry
   void _updateExistingElement(const ConstElementPtr updatedElement);
+  // terry
   void _deleteExistingElement(const ConstElementPtr removedElement);
+  // jason
+  long _getNextId(const ElementType type);
+
+  // jason
+  void _create(const ConstNodePtr node);
+  void _create(const ConstWayPtr node);
+  void _create(const ConstRelationPtr node);
+
+  // jason
+  void _modify(const ConstNodePtr node);
+  void _modify(const ConstWayPtr node);
+  void _modify(const ConstRelationPtr node);
+
+  // jason
+  void _delete(const ConstNodePtr node);
+  void _delete(const ConstWayPtr node);
+  void _delete(const ConstRelationPtr node);
+
+
 
 };
 
