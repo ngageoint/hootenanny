400cbe70514f0191ed83408345ee94b7c23234f9
ming.su <ming.su@digitalglobe.com>
2015-12-16 18:45:15 +0000

    fix #135 - Add a test to ConvertCmdTest to convert from shp to osm

--- hoot-core-test/src/test/resources/cmd/slow/ConvertCmdTest.sh
+++ hoot-core-test/src/test/resources/cmd/slow/ConvertCmdTest.sh
@@ -3,11 +3,13 @@ set -e
 
 rm -rf test-output/cmd/ConvertCmdTest
 mkdir -p test-output/cmd/ConvertCmdTest
-hoot --convert test-files/ToyTestA.osm.pbf test-output/cmd/ConvertCmdTest/ToyTestA.osm
+hoot convert test-files/ToyTestA.osm.pbf test-output/cmd/ConvertCmdTest/ToyTestA.osm
 cat test-output/cmd/ConvertCmdTest/ToyTestA.osm
 
-hoot --convert test-files/ToyTestA.osm.pbf test-output/cmd/ConvertCmdTest/ToyTestA.shp
-ls test-output/cmd/ConvertCmdTest/ToyTestA.shp
+hoot convert test-files/ToyTestA.osm.pbf test-output/cmd/ConvertCmdTest/ToyTestA.shp
+# comparing shapefiles is kinda hard. We'll convert it to a .osm file first.
+hoot convert test-output/cmd/ConvertCmdTest/ToyTestA.shp test-output/cmd/ConvertCmdTest/ToyTestAShp.osm
+hoot is-match test-output/cmd/ConvertCmdTest/ToyTestAShp.osm test-files/cmd/ConvertCmdTest/ToyTestAShp.osm
 
 echo Check for invalid characters. You should see 3 sets of warnings.
 hoot --convert  test-files/io/InvalidCharacters.osm test-output/cmd/ConvertCmdTest/InvalidCharacters.osm
