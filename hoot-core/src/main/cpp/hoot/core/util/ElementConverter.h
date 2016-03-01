1a7294b4bae8f36836ff95a593afacf71f640d75
Ben Marchant <benjamin.marchant@digitalglobe.com>
2016-03-01 10:26:20 -0600

    Fixed const consitancy issue

--- hoot-core/src/main/cpp/hoot/core/util/ElementConverter.h
+++ hoot-core/src/main/cpp/hoot/core/util/ElementConverter.h
@@ -93,7 +93,7 @@ public:
    * Converts the given element to a geos geometry object. The tags are used with OsmSchema to
    * determine the geometry type.
    */
-  shared_ptr<geos::geom::Geometry> convertToGeometry(const shared_ptr<const Element>& e, const bool throwError=true, const bool statsFlag=false) const;
+  shared_ptr<geos::geom::Geometry> convertToGeometry(const shared_ptr<const Element>& e, bool throwError=true, const bool statsFlag=false) const;
   shared_ptr<geos::geom::Point> convertToGeometry(const ConstNodePtr& n) const;
   shared_ptr<geos::geom::Geometry> convertToGeometry(const WayPtr& w) const;
   shared_ptr<geos::geom::Geometry> convertToGeometry(const shared_ptr<const Way>& w, bool throwError, const bool statsFlag=false) const;
