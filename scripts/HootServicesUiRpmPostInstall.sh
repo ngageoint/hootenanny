4e487fad45dbab98a8f4ac0839205313ed614167
Brian Hatchl <brian.hatchl@digitalglobe.com>
2016-03-16 22:05:08 -0400

    add missing line continuation chars

--- scripts/HootServicesUiRpmPostInstall.sh
+++ scripts/HootServicesUiRpmPostInstall.sh
@@ -125,11 +125,10 @@ cd $TOMCAT_HOME/webapps/hoot-services/WEB-INF
 liquibase --contexts=default,production \
     --changeLogFile=classes/db/db.changelog-master.xml \
     --promptForNonLocalDatabase=false \
-    --driver=org.postgresql.Driver
-    --url=jdbc:postgresql:$DB_NAME
-    --username=$DB_USER
-    --password=$DB_PASSWORD
-    --changeLogFile=db/db.changelog-master.json
+    --driver=org.postgresql.Driver \
+    --url=jdbc:postgresql:$DB_NAME \
+    --username=$DB_USER \
+    --password=$DB_PASSWORD \
     --logLevel=warning \
     --classpath=lib/postgresql-9.1-901-1.jdbc4.jar \
     update
