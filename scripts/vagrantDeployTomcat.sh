0b36dfdb582014156263265e661adac7c4b04152
sisskind <josh.sisskind@digitalglobe.com>
2016-03-18 17:17:29 -0400

    Readding vagrantDeployTomcat.sh

---
+++ scripts/vagrantDeployTomcat.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export TOMCAT6_HOME=/var/lib/tomcat6
+
+rm -f $TOMCAT6_HOME/webapps/hoot-services.war
+cp `ls -t hoot-services/target/hoot-services-*.war | sed -n 1p` $TOMCAT6_HOME/webapps/hoot-services.war
+rm -rf $TOMCAT6_HOME/webapps/hootenanny-id/
+cp -R hoot-ui $TOMCAT6_HOME/webapps/hootenanny-id
+rm -rf $TOMCAT6_HOME/webapps/hootenanny-id/css/img
+cp -R $TOMCAT6_HOME/webapps/hootenanny-id/dist/img $TOMCAT6_HOME/webapps/hootenanny-id/css/
