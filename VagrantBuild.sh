99e55d1f5e60e9b4327390b9bc5a98a17139b60d
Matt C. Jackson <matthew.jackson@digitalglobe.com>
2016-03-23 16:19:28 -0600

    Broke the VagrantProvision script into a Provison and a Build script
    1. refs #476 Broke the VagrantProvision script into a Provison and a Build script

---
+++ VagrantBuild.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+cd $HOOT_HOME
+source ./SetupEnv.sh
+
+echo "### Configuring Hoot..."
+aclocal && autoconf && autoheader && automake && ./configure --quiet --with-rnd --with-services --with-uitests
+if [ ! -f LocalConfig.pri ] && ! grep --quiet QMAKE_CXX LocalConfig.pri; then
+    echo 'Customizing LocalConfig.pri...'
+    cp LocalConfig.pri.orig LocalConfig.pri
+    echo 'QMAKE_CXX=ccache g++' >> LocalConfig.pri
+fi
+echo "### Building Hoot... "
+echo "Will take several extra minutes to build the training data the initial time Hootenanny is installed only."
+make -s clean && make -sj$(nproc)
+echo "### Deploying web application..."
+# vagrant will auto start the tomcat service for us, so just copy the web app files w/o manipulating the server
+scripts/CopyWebAppsToTomcat.sh #&> /dev/null
+# docs build is always failing the first time during the npm install portion for an unknown reason, but
+# always passes the second time its run...needs fixed, but this is the workaround for now
+make -sj$(nproc) docs &> /dev/null || true
+make -sj$(nproc) docs
+hoot version
+
+echo "See VAGRANT.md for additional configuration instructions and then run 'vagrant ssh' to log into the Hootenanny virtual machine."
+echo "See $HOOT_HOME/docs on the virtual machine for Hootenanny documentation files."
+echo "Access the web application at http://localhost:8888/hootenanny-id"
+echo "If you wish to run the diagnostic tests, log into the virtual machine and run: 'cd $HOOT_HOME && make -sj$(nproc) test-all'"
+echo "If you wish to rebuild Hoot, log into the virtual machine and run: './VagrantBuild.sh'"
+echo "NOTE: if you rebuild Hoot, you will need to restart Tomcat and the Mapnik server."
+
