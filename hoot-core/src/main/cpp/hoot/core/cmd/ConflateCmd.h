b6f68b3bfd53edb3f24cc6fbc82f7ecc04fcc264
Brandon Witham <brandon.witham@digitalglobe.com>
2016-03-29 17:00:50 -0400

    temporarily disable map load change made by changeset diff code
    1. refs #423 - now fewer failing tests

--- hoot-core/src/main/cpp/hoot/core/cmd/ConflateCmd.h
+++ hoot-core/src/main/cpp/hoot/core/cmd/ConflateCmd.h
@@ -5,7 +5,7 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -125,7 +125,9 @@ public:
 
     // read input 1
     shared_ptr<OsmMap> map(new OsmMap());
-    loadMap(map, input1, true, Status::Unknown1);
+    //TODO: swap commented line with line following it for changeset diff writing
+    //loadMap(map, input1, true, Status::Unknown1);
+    loadMap(map, input1, false, Status::Unknown1);
 
     // read input 2
     if (!input2.isEmpty())
