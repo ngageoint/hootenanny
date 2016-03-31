a9abc45f397c74f1d06140298244b199c44156d4
Brian Hatchl <brian.hatchl@digitalglobe.com>
2016-03-31 17:39:20 -0400

    remove echo causing test failure local vs jenkins

--- scripts/exportrenderdb.sh
+++ scripts/exportrenderdb.sh
@@ -6,7 +6,6 @@ source $HOOT_HOME/conf/DatabaseConfig.sh
 export AUTH="-h $DB_HOST -p $DB_PORT -U $DB_USER"
 export PGPASSWORD=$DB_PASSWORD
 export RENDER_DB="$DB_NAME"_renderdb_$INPUT
-echo $RENDER_DB
 #
 #  Export hoot dataset to a PostGIS render db
 #
