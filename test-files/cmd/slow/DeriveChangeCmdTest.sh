8d348a56eaa18017cbe672f4a023812a3b12f9a8
Brandon Witham <brandon.witham@digitalglobe.com>
2016-04-08 13:15:29 -0400

    Remove conflation variable from DeriveChangeCmdTest
    1. refs #553

--- test-files/cmd/slow/DeriveChangeCmdTest.sh
+++ test-files/cmd/slow/DeriveChangeCmdTest.sh
@@ -3,7 +3,6 @@ set -e
 
 mkdir -p test-output/cmd/DeriveChangeCmdTest
 
-hoot conflate test-files/ToyTestA.osm test-files/ToyTestB.osm test-output/cmd/DeriveChangeCmdTest/conflate-out.osm
-hoot derive-change test-files/ToyTestA.osm test-output/cmd/DeriveChangeCmdTest/conflate-out.osm test-output/cmd/DeriveChangeCmdTest/changeset.osc
+hoot derive-change test-files/ToyTestA.osm test-files/cmd/slow/DeriveChangeCmdTest/conflate-out.osm test-output/cmd/DeriveChangeCmdTest/changeset.osc
 diff test-output/cmd/DeriveChangeCmdTest/changeset.osc test-files/cmd/slow/DeriveChangeCmdTest/changeset.osc 
 
