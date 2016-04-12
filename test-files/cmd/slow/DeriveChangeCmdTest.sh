d67a025e6d0e0921a4d6a882cf6f757584c2f22f
Brandon Witham <brandon.witham@digitalglobe.com>
2016-04-12 14:07:42 -0400

    update test
    1. refs #552

--- test-files/cmd/slow/DeriveChangeCmdTest.sh
+++ test-files/cmd/slow/DeriveChangeCmdTest.sh
@@ -7,6 +7,6 @@ hoot derive-change test-files/cmd/slow/DeriveChangeCmdTest/map1.osm test-files/c
 diff test-output/cmd/DeriveChangeCmdTest/changeset.osc test-files/cmd/slow/DeriveChangeCmdTest/changeset.osc 
 
 source $HOOT_HOME/conf/DatabaseConfig.sh
-hoot derive-change -D changeset.sql.use.internal.ids=true test-files/ToyTestA.osm test-files/cmd/slow/DeriveChangeCmdTest/conflate-out.osm test-output/cmd/DeriveChangeCmdTest/changeset.sql "postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME"
+hoot derive-change -D changeset.sql.use.internal.ids=true test-files/cmd/slow/DeriveChangeCmdTest/map1.osm test-files/cmd/slow/DeriveChangeCmdTest/map2.osm test-output/cmd/DeriveChangeCmdTest/changeset.sql "postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME"
 diff test-output/cmd/DeriveChangeCmdTest/changeset.sql test-files/cmd/slow/DeriveChangeCmdTest/changeset.sql
 
