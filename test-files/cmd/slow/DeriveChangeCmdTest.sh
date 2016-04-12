c6719bb3bfd6bd47807d9aad16f254ab5e1b2ac1
Brandon Witham <brandon.witham@digitalglobe.com>
2016-04-12 12:58:25 -0400

    update DeriveChangeCmdTest to use more relevant data
    1. refs #553

--- test-files/cmd/slow/DeriveChangeCmdTest.sh
+++ test-files/cmd/slow/DeriveChangeCmdTest.sh
@@ -3,6 +3,6 @@ set -e
 
 mkdir -p test-output/cmd/DeriveChangeCmdTest
 
-hoot derive-change test-files/ToyTestA.osm test-files/cmd/slow/DeriveChangeCmdTest/conflate-out.osm test-output/cmd/DeriveChangeCmdTest/changeset.osc
+hoot derive-change test-files/cmd/slow/DeriveChangeCmdTest/map1.osm test-files/cmd/slow/DeriveChangeCmdTest/map2.osm test-output/cmd/DeriveChangeCmdTest/changeset.osc
 diff test-output/cmd/DeriveChangeCmdTest/changeset.osc test-files/cmd/slow/DeriveChangeCmdTest/changeset.osc 
 
