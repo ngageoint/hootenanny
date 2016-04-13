df2137e019fa2c03d296f059f849582f83ca7a95
Brian Hatchl <brian.hatchl@digitalglobe.com>
2016-04-13 12:27:45 -0400

    Added map id param to test

--- hoot-services/src/test/java/hoot/services/controllers/ingest/RasterToTilesResourceTest.java
+++ hoot-services/src/test/java/hoot/services/controllers/ingest/RasterToTilesResourceTest.java
@@ -5,7 +5,7 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -75,7 +75,7 @@ public class RasterToTilesResourceTest {
 		oExpected.put("exectype","make");
 		oExpected.put("erroraswarning", "true");
 
-		String actual = rts._createCommand("test", "0-1 2-3", 500);
+		String actual = rts._createCommand("test", "0-1 2-3", 500, 1);
 
 		org.junit.Assert.assertEquals(oExpected.toString(), actual);
 	}
