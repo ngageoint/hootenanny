deeac597024b8f3a07d1cc1e491ea167c7b7efe5
sisskind <josh.sisskind@digitalglobe.com>
2016-04-13 10:21:00 -0400

    Update tests for map id

--- test-files/cmd/slow/ServiceExportRenderDbTest.sh
+++ test-files/cmd/slow/ServiceExportRenderDbTest.sh
@@ -6,7 +6,7 @@ export INPUT=ExRenDb
 # setup db, user, and password to avoid password prompt
 export AUTH="-h $DB_HOST -p $DB_PORT -U $DB_USER"
 export PGPASSWORD=$DB_PASSWORD
-export RENDER_DB="$DB_NAME"_renderdb_$INPUT
+export RENDER_DB="$DB_NAME"_renderdb_$MAP_ID
 export DB_URL=hootapidb://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME
 export HOOT_OPTS="-D hootapi.db.writer.create.user=true -D hootapi.db.writer.email=test@test.com -D hootapi.db.writer.overwrite.map=true -D hootapi.db.reader.email=test@test.com -D writer.include.debug=true --warn"
 
