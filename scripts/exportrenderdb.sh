59dfcccd7a0f99116c3d9bd27f95421a6280f3d2
Brian Hatchl <brian.hatchl@digitalglobe.com>
2016-04-13 14:24:46 -0400

    must query db for map id in test

--- scripts/exportrenderdb.sh
+++ scripts/exportrenderdb.sh
@@ -6,7 +6,7 @@ source $HOOT_HOME/conf/DatabaseConfig.sh
 # setup db, user, and password to avoid password prompt
 export AUTH="-h $DB_HOST -p $DB_PORT -U $DB_USER"
 export PGPASSWORD=$DB_PASSWORD
-export RENDER_DB="$DB_NAME"_renderdb_$MAP_ID
+export RENDER_DB="$DB_NAME"_renderdb_"$MAP_ID"
 #
 #  Export hoot dataset to a PostGIS render db
 #
--- test-files/cmd/slow/ServiceExportRenderDbTest.sh
+++ test-files/cmd/slow/ServiceExportRenderDbTest.sh
@@ -6,15 +6,17 @@ export INPUT=ExRenDb
 # setup db, user, and password to avoid password prompt
 export AUTH="-h $DB_HOST -p $DB_PORT -U $DB_USER"
 export PGPASSWORD=$DB_PASSWORD
-export RENDER_DB="$DB_NAME"_renderdb_$MAP_ID
 export DB_URL=hootapidb://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME
 export HOOT_OPTS="-D hootapi.db.writer.create.user=true -D hootapi.db.writer.email=test@test.com -D hootapi.db.writer.overwrite.map=true -D hootapi.db.reader.email=test@test.com -D writer.include.debug=true --warn"
 
-# Preventative clean up
-dropdb $AUTH $RENDER_DB &>/dev/null || true
-
 # Ingest the data
 hoot --convert $HOOT_OPTS test-files/$INPUT.osm $DB_URL/$INPUT
+
+export MAP_ID=$(psql -U $DB_USER -h $DB_HOST -p $DB_PORT -d $DB_NAME -qt -c "select id from maps where display_name = '$INPUT';" | tr -d ' ')
+export RENDER_DB="$DB_NAME"_renderdb_"$MAP_ID"
+
+# Preventative clean up
+dropdb $AUTH $RENDER_DB &>/dev/null || true
 
 # Write ingested db dataset to renderdb,
 # to test the streaming reader to ogr writer
@@ -26,5 +28,5 @@ scripts/exportrenderdb.sh # || true # Don't error out so test will continue to c
 hoot delete-map $HOOT_OPTS $DB_URL/$INPUT
 
 # Assert that hoot map and render db have been deleted
-psql -U $DB_USER -h $DB_HOST -p $DB_PORT -d $DB_NAME -c "select display_name from maps;" | grep -qw ExRenDb && echo "Error: delete-map did not remove ExRenDb dataset"
-psql -U $DB_USER -h $DB_HOST -p $DB_PORT -d $DB_NAME -lqt | cut -d \| -f 1 | grep -qw $RENDER_DB && echo "Error: delete-map did not remove ExRenDb render db"
+psql -U $DB_USER -h $DB_HOST -p $DB_PORT -d $DB_NAME -c "select display_name from maps;" | grep -qw $INPUT && echo "Error: delete-map did not remove $INPUT dataset"
+psql -U $DB_USER -h $DB_HOST -p $DB_PORT -d $DB_NAME -lqt | cut -d \| -f 1 | grep -qw $RENDER_DB && echo "Error: delete-map did not remove $INPUT render db"
