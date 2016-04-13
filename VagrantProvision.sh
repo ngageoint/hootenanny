3e93ef72e3217aded4943b069592ccfb6528c69f
Matt C. Jackson <matthew.jackson@digitalglobe.com>
2016-04-13 15:31:57 -0600

    Remove potential merge conflict
    1. refs #476 Remove potential merge conflict

--- VagrantProvision.sh
+++ VagrantProvision.sh
@@ -363,36 +363,13 @@ if [ -f $HOOT_HOME/hoot-services/src/main/resources/conf/local.conf ]; then
     rm -f $HOOT_HOME/hoot-services/src/main/resources/conf/local.conf
 fi
 
-# Update marker file date now that dependency and config stuff has run
-# The make command will exit and provide a warning to run 'vagrant provision'
-# if the marker file is older than this file (VagrantProvision.sh)
-touch Vagrant.marker
-
 mkdir -p $HOOT_HOME/ingest/processed
 sudo chown -R $username:tomcat6 $HOOT_HOME/ingest
 mkdir -p $HOOT_HOME/upload
 sudo chown -R $username:tomcat6 $HOOT_HOME/upload
-aclocal && autoconf && autoheader && automake && ./configure --with-rnd --with-services --with-uitests
-if [ ! -f LocalConfig.pri ] && ! grep --quiet QMAKE_CXX LocalConfig.pri; then
-    echo 'Customizing LocalConfig.pri...'
-    cp LocalConfig.pri.orig LocalConfig.pri
-    echo 'QMAKE_CXX=ccache g++' >> LocalConfig.pri
-fi
-echo "Building Hoot... "
-echo "Will take several extra minutes to build the training data the initial time Hootenanny is installed only."
-make -s clean && make -sj$(nproc)
-# vagrant will auto start the tomcat service for us, so just copy the web app files w/o manipulating the server
-scripts/CopyWebAppsToTomcat.sh #&> /dev/null
-# docs build is always failing the first time during the npm install portion for an unknown reason, but then 
-# always passes the second time its run...needs fixed, but this is the workaround for now
-make -sj$(nproc) docs &> /dev/null || true
-make -sj$(nproc) docs
-hoot version
 
-echo "See VAGRANT.md for additional configuration instructions and then run 'vagrant ssh' to log into the Hootenanny virtual machine."
-echo "See $HOOT_HOME/docs on the virtual machine for Hootenanny documentation files."
-echo "Access the web application at http://localhost:8888/hootenanny-id"
-echo "If you wish to run the diagnostic tests, log into the virtual machine and run: 'cd $HOOT_HOME && make -sj$(nproc) test-all'"
-
+# Update marker file date now that dependency and config stuff has run
+# The make command will exit and provide a warning to run 'vagrant provision'
+# if the marker file is older than this file (VagrantProvision.sh)
+touch Vagrant.marker
 # Now we are ready to build Hoot.  The VagrantBuild.sh script will build Hoot.
-
