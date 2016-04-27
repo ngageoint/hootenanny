1f13caa98f35d2f2afeb853e58ef104701378e10
Brandon Witham <brandon.witham@digitalglobe.com>
2016-04-27 17:04:09 -0400

    move base script back and rename
    1. refs #645

--- test-files/cmd/glacial/ServiceOsmApiDbDcConflateTest.sh
+++ test-files/cmd/glacial/ServiceOsmApiDbDcConflateTest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-scripts/OsmApiDbConflate.sh test-files/DcGisRoads.osm test-files/DcTigerRoads.osm -77.040,38.892,-77.033,38.896 ServiceOsmApiDbDcConflateTest
+test-files/cmd/glacial/OsmApiDbConflate.off test-files/DcGisRoads.osm test-files/DcTigerRoads.osm -77.040,38.892,-77.033,38.896 ServiceOsmApiDbDcConflateTest
--- test-files/cmd/glacial/ServiceOsmApiDbToyConflateTest.sh
+++ test-files/cmd/glacial/ServiceOsmApiDbToyConflateTest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-scripts/OsmApiDbConflate.sh test-files/ToyTestA.osm test-files/ToyTestB.osm -104.904,35.852,-104.895,38.856 ServiceOsmApiDbToyConflateTest
+test-files/cmd/glacial/OsmApiDbConflate.off test-files/ToyTestA.osm test-files/ToyTestB.osm -104.904,35.852,-104.895,38.856 ServiceOsmApiDbToyConflateTest
