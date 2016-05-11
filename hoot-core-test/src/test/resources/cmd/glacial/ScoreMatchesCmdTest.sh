a7785588028e9c2c000c45430b70fb14a8c73166
Micah Schicker <micah.schicker@digitalglobe.com>
2016-05-11 09:02:51 -0700

    Fix ScoreMatchesCmdTest. Was failing on Jenkins, because of differing paths to model files.

--- hoot-core-test/src/test/resources/cmd/glacial/ScoreMatchesCmdTest.sh
+++ hoot-core-test/src/test/resources/cmd/glacial/ScoreMatchesCmdTest.sh
@@ -18,6 +18,6 @@ hoot score-matches --confusion --optimize $map1 $map2 $outputfile |\
                    sed "s///g" |\
                    sed "s/^[0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}\.[0-9]\{3\} //g" |\
                    sed "s/M took: [0-9]\+\.*[0-9]*s/M took: s/g" |\
-                   sed "s/[0-9]\+\.[0-9]\+e\-[0-9]\+//g"
-
+                   sed "s/[0-9]\+\.[0-9]\+e\-[0-9]\+//g" |\
+                   sed "s/Loading \(model\|highway model\) from: .*\$/Loading model/"
 
