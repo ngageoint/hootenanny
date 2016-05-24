313e06627c737645f9292f34818e5b32c13d89a1
Ben Marchant <benjamin.marchant@digitalglobe.com>
2016-05-24 14:33:28 -0500

    Minor updates based on the diff from the commit

--- conf/DatabaseConfig.sh
+++ conf/DatabaseConfig.sh
@@ -2,7 +2,6 @@
 source $HOOT_HOME/conf/DatabaseConfigDefault.sh
 
 # Check for any local user database config changes
-if [ -e $HOOT_HOME/conf/DatabaseConfigLocal.sh ]
-then
+if [ -e $HOOT_HOME/conf/DatabaseConfigLocal.sh ]; then
 	source $HOOT_HOME/conf/DatabaseConfigLocal.sh
 fi
