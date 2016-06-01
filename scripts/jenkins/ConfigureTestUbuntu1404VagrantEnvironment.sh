b90b99bd396494c1daccd3b7a5dd3e46575bd1b3
Matt C. Jackson <matthew.jackson@digitalglobe.com>
2016-06-01 16:32:41 -0600

    Fixed :VeryClean.sh" for Jenkins

--- scripts/jenkins/ConfigureTestUbuntu1404VagrantEnvironment.sh
+++ scripts/jenkins/ConfigureTestUbuntu1404VagrantEnvironment.sh
@@ -11,6 +11,7 @@ set -x
 
 cd $HOOT_HOME
 
+# Just wipe out the files. Db cleaning comes later
 scripts/jenkins/VeryClean.sh
 
 # Maintain vagrant state in the parent directory so very clean will still work.
@@ -58,6 +59,9 @@ if [ $REBUILD_VAGRANT == 'true' ]; then
 else
     time -p vagrant up --provision-with nfs,hadoop --provider vsphere
 fi
+
+# Clean out the Database
+vagrant ssh -c "cd hoot; source ./SetupEnv.sh; cd hoot-services; make clean-db &> /dev/null"
 
 date +%F > ../BuildDate.txt
 
