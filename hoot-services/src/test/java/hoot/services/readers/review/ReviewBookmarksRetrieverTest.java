c9edcb92411777e0c009adb8bc59cc31eaf30ae9
sisskind <josh.sisskind@digitalglobe.com>
2016-06-24 08:36:37 -0400

    Tests passing for ReviewBookmarksRetrieverTest

--- hoot-services/src/test/java/hoot/services/readers/review/ReviewBookmarksRetrieverTest.java
+++ hoot-services/src/test/java/hoot/services/readers/review/ReviewBookmarksRetrieverTest.java
@@ -128,12 +128,8 @@ public class ReviewBookmarksRetrieverTest {
 
         String actual = q.toString();
         String expected = "from \"review_bookmarks\" \"review_bookmarks\"\n"
-                + "where \"review_bookmarks\".\"created_by\" = ?\n"
+                + "where \"review_bookmarks\".\"created_by\" in (?, ?)\n"
                 + "order by \"review_bookmarks\".\"created_at\" asc";
-        
-//        from "review_bookmarks" "review_bookmarks"
-//        where "review_bookmarks"."created_by" in (?, ?)
-//        order by "review_bookmarks"."created_at" asc
 
         org.junit.Assert.assertEquals(expected, actual);
 
@@ -149,10 +145,28 @@ public class ReviewBookmarksRetrieverTest {
 
         String actual = q.toString();
         String expected = "from \"review_bookmarks\" \"review_bookmarks\"\n"
-                + "where \"review_bookmarks\".\"map_id\" = ?\n" + "order by \"review_bookmarks\".\"created_at\" asc";
+                + "where \"review_bookmarks\".\"map_id\" in (?, ?)\n"
+                + "order by \"review_bookmarks\".\"created_at\" asc";
 
         org.junit.Assert.assertEquals(expected, actual);
 
     }
+    
+    @Test
+    @Category(UnitTest.class)
+    public void testRetrieveAllOrderByCreatedAtWithCreatorAndMapIdFilter() throws Exception {
+        Connection conn = null;
+
+        ReviewBookmarkRetriever r = new ReviewBookmarkRetriever(conn);
+        SQLQuery q = r._getAllQuery("createdAt", true, -1, -1, new Long[]{1L,2L}, new Long[]{1L,2L});
+
+        String actual = q.toString();
+        String expected = "from \"review_bookmarks\" \"review_bookmarks\"\n"
+        		+ "where \"review_bookmarks\".\"created_by\" in (?, ?) and \"review_bookmarks\".\"map_id\" in (?, ?)\n"
+                + "order by \"review_bookmarks\".\"created_at\" asc";
+
+        org.junit.Assert.assertEquals(expected, actual);
+
+    }    
 
 }
