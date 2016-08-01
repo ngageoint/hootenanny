c7b6f89d6bc8153265bfefb285b3b4bb3d41a62b
Brandon Witham <brandon.witham@digitalglobe.com>
2016-08-01 13:08:19 -0400

    Updated file license headers.

--- hoot-core/src/main/cpp/hoot/core/visitors/FindWaysVisitor.h
+++ hoot-core/src/main/cpp/hoot/core/visitors/FindWaysVisitor.h
@@ -5,7 +5,7 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -22,7 +22,7 @@
  * This will properly maintain the copyright information. DigitalGlobe
  * copyrights will be updated automatically.
  *
- * @copyright Copyright (C) 2015 DigitalGlobe (http://www.digitalglobe.com/)
+ * @copyright Copyright (C) 2016 DigitalGlobe (http://www.digitalglobe.com/)
  */
 #ifndef FINDWAYSVISITOR_H
 #define FINDWAYSVISITOR_H
