ec04c89aa3827a5088347001a71f5b8f9caf0441
Brian Hatchl <brian.hatchl@digitalglobe.com>
2016-08-23 18:17:14 -0400

    test out the emgcp toEnglish method

--- plugins/test/HootJs.js
+++ plugins/test/HootJs.js
@@ -5,9 +5,6 @@ var emgcp_osm = require('../emgcp_osm.js');
 var emgcp_rules = require('../emgcp_rules');
 var mgcp_schema = require('../mgcp_schema');
 
-
-
-
 describe('compare translation methods', function() {
 
     it('should be lossy to go from osm -> mgcp -> osm', function() {
@@ -27,13 +24,13 @@ describe('compare translation methods', function() {
             "Feature Code": "AL015:General Building Point Feature",
             "Feature Function": "Restaurant",
             "MGCP Feature universally unique identifier": "4632d15b-7c44-4ba1-a0c4-8cfbb30e39d4"
-        }, '', '');
+        }, '', 'Point');
 
         var expected = {
             "building": "yes",
             "uuid": "{4632d15b-7c44-4ba1-a0c4-8cfbb30e39d4}",
             "amenity": "restaurant"
-        }
+        };
 
         //Assert that poi=yes, amenity=cafe become building=yes, amenity=restaurant
         assert.equal(JSON.stringify(osm_json.attrs), JSON.stringify(expected));
@@ -46,11 +43,30 @@ describe('compare translation methods', function() {
             "place": "town",
             "name": "Manitou Springs",
             "uuid": "{4632d15b-7c44-4ba1-a0c4-8cfbb30e39d4}"
-        }
+        };
 
         // Test the conversion of osm to emgcp without using hoot to translate an osm map
-        var osm2mgcp = emgcp.toEnglish(osm, '', '');
-        console.log(osm2mgcp);
+        var osm2mgcp = emgcp.toEnglish(osm, '', 'Point');
+
+        var expected = { 'Feature Code': 'AL020:Built-Up Area Area Feature',
+                         Name: 'Manitou Springs',
+                         'MGCP Feature universally unique identifier': '4632d15b-7c44-4ba1-a0c4-8cfbb30e39d4',
+                         'Source Date and Time': 'UNK',
+                         'Source Description': 'N_A',
+                         'Name Identifier': 'UNK',
+                         'Named Feature Identifier': 'UNK',
+                         'Commercial Copyright Notice': 'UNK',
+                         'Associated Text': 'N_A',
+                         'Functional Use': 'Other',
+                         'Horizontal Accuracy Category': 'Accurate',
+                         'Built-up Area Density Category': 'Unknown',
+                         'Source Type': 'Unknown',
+                         'Condition of Facility': 'Unknown',
+                         'Relative Importance': 'Unknown'
+                    };
+
+        //Assert that poi=yes, amenity=cafe become building=yes, amenity=restaurant
+        assert.equal(JSON.stringify(osm2mgcp.attrs), JSON.stringify(expected));
     });
 
 });
