5a86021e65b7f2bf09256c33bf81a055c280e639
Brandon Witham <brandon.witham@digitalglobe.com>
2016-09-08 09:51:50 -0400

    add some new criterion
    1. refs #205

---
+++ hoot-core/src/main/cpp/hoot/core/filters/WayCriterion.h
@@ -0,0 +1,55 @@
+/*
+ * This file is part of Hootenanny.
+ *
+ * Hootenanny is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * --------------------------------------------------------------------
+ *
+ * The following copyright notices are generated automatically. If you
+ * have a new notice to add, please use the format:
+ * " * @copyright Copyright ..."
+ * This will properly maintain the copyright information. DigitalGlobe
+ * copyrights will be updated automatically.
+ *
+ * @copyright Copyright (C) 2015, 2016 DigitalGlobe (http://www.digitalglobe.com/)
+ */
+#ifndef WAYCRITERION_H
+#define WAYCRITERION_H
+
+// hoot
+#include "ElementCriterion.h"
+
+namespace hoot
+{
+
+/**
+ * A filter that will keep way features.
+ */
+class WayCriterion : public ElementCriterion
+{
+public:
+
+  static string className() { return "hoot::WayCriterion"; }
+
+  WayCriterion();
+
+  bool isSatisfied(const shared_ptr<const Element> &e) const;
+
+  virtual ElementCriterion* clone() { return new WayCriterion(); }
+
+};
+
+}
+
+#endif // WAYCRITERION_H
