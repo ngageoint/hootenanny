77fb8c2b03453f1d06cafa4812e4fbee667e15f1
Dmitriy Mylov <dmitriy.mylov@digitalglobe.com>
2016-09-11 23:11:53 -0400

    Added generic exception logger to log all exceptions that get thrown by the Hoot's Jersey Resource classes.

--- hoot-services/src/main/java/hoot/services/JerseyExceptionLogger.java
+++ hoot-services/src/main/java/hoot/services/JerseyExceptionLogger.java
@@ -2,20 +2,21 @@ package hoot.services;
 
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status.Family;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.Provider;
 
-import org.glassfish.jersey.spi.ExtendedExceptionMapper;
+import org.glassfish.jersey.server.monitoring.ApplicationEvent;
+import org.glassfish.jersey.server.monitoring.ApplicationEventListener;
+import org.glassfish.jersey.server.monitoring.RequestEvent;
+import org.glassfish.jersey.server.monitoring.RequestEventListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 @Provider
-public class JerseyExceptionLogger implements ExtendedExceptionMapper<Throwable> {
+public class JerseyExceptionLogger implements ApplicationEventListener, RequestEventListener {
     private static final Logger logger = LoggerFactory.getLogger(JerseyExceptionLogger.class);
 
     @Context
@@ -24,12 +25,23 @@ public class JerseyExceptionLogger implements ExtendedExceptionMapper<Throwable>
     @Context
     private UriInfo uriInfo;
 
-    @Context
-    private HttpHeaders headers;
-
 
     @Override
-    public boolean isMappable(Throwable exception) {
+    public void onEvent(ApplicationEvent applicationEvent) {}
+
+    @Override
+    public RequestEventListener onRequest(RequestEvent requestEvent) {
+        return this;
+    }
+
+    @Override
+    public void onEvent(RequestEvent paramRequestEvent) {
+        if(paramRequestEvent.getType() == RequestEvent.Type.ON_EXCEPTION) {
+            logException(paramRequestEvent.getException());
+        }
+    }
+
+    private void logException(Throwable exception) {
         String message = buildMessage();
 
         /* Primarily, we don't want to log client errors (i.e. 400's) as an error. */
@@ -39,51 +51,23 @@ public class JerseyExceptionLogger implements ExtendedExceptionMapper<Throwable>
         else {
             logger.info(message, exception);
         }
-
-        return false;
-    }
-
-    @Override
-    public Response toResponse(Throwable exception) {
-        logger.error("This should not have been called.");
-        throw new RuntimeException("This should not have been called");
     }
 
     private static boolean isServerError(Throwable throwable) {
-        /* Note: We consider anything that is not an instance of WebApplicationException a server error. */
         return (throwable instanceof WebApplicationException) && isServerError((WebApplicationException) throwable);
     }
 
     private static boolean isServerError(WebApplicationException exception) {
-        return exception.getResponse().getStatusInfo().getFamily() == Family.SERVER_ERROR;
+        return exception.getResponse().getStatusInfo().getFamily() == Response.Status.Family.SERVER_ERROR;
     }
 
     private String buildMessage() {
         StringBuilder message = new StringBuilder();
 
         message.append("Uncaught REST API exception:").append(System.lineSeparator());
-        message.append("URL: ").append(listQueryParamNames()).append(System.lineSeparator());
+        message.append("URL: ").append(uriInfo.getAbsolutePath()).append(System.lineSeparator());
         message.append("Method: ").append(this.request.getMethod()).append(System.lineSeparator());
-        message.append("Headers: ").append(listHeaderNames()).append(System.lineSeparator());
 
         return message.toString();
-    }
-
-    private String listQueryParamNames() {
-        StringBuilder buffer = new StringBuilder();
-        for (String param: this.uriInfo.getQueryParameters().keySet()) {
-            buffer.append(param);
-            buffer.append(System.lineSeparator());
-        }
-        return buffer.toString();
-    }
-
-    private String listHeaderNames() {
-        StringBuilder buffer = new StringBuilder();
-        for (String header: this.headers.getRequestHeaders().keySet()) {
-            buffer.append(header);
-            buffer.append(System.lineSeparator());
-        }
-        return buffer.toString();
     }
 }
