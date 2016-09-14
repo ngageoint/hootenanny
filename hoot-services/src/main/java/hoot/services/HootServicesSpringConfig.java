74ff466611f5fa19f88a2d004f886b340f5ecc08
Dmitriy Mylov <dmitriy.mylov@digitalglobe.com>
2016-09-14 13:56:00 -0400

    Use Environment (recommended) instead of @Value for property injection

--- hoot-services/src/main/java/hoot/services/HootServicesSpringConfig.java
+++ hoot-services/src/main/java/hoot/services/HootServicesSpringConfig.java
@@ -4,12 +4,12 @@ import javax.sql.DataSource;
 
 import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -18,23 +18,13 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableTransactionManagement
 @ComponentScan(basePackages = {"hoot.services"})
-@PropertySource(value = {"classpath:db/db.properties"})
+@PropertySource("classpath:db/db.properties")
 public class HootServicesSpringConfig {
 
-    @Value("${DB_HOST}")
-    private String dbHost;
+    @Autowired
+    private Environment env;
 
-    @Value("${DB_PORT}")
-    private String dbPort;
-
-    @Value("${DB_NAME}")
-    private String dbName;
-
-    @Value("${DB_USER}")
-    private String dbUser;
-
-    @Value("${DB_PASSWORD}")
-    private String dbPassword;
+    public HootServicesSpringConfig() {}
 
     @Bean
     public ApplicationContextUtils applicationContextUtils() {
@@ -45,9 +35,11 @@ public class HootServicesSpringConfig {
     public DataSource dataSource() {
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://" + dbHost + ":" + dbPort + "/" + dbName);
-        dataSource.setUsername(dbUser);
-        dataSource.setPassword(dbPassword);
+        dataSource.setUrl("jdbc:postgresql://" + env.getProperty("DB_HOST") + ":" +
+                                                 env.getProperty("DB_PORT") + "/" +
+                                                 env.getProperty("DB_NAME"));
+        dataSource.setUsername(env.getProperty("DB_USER"));
+        dataSource.setPassword(env.getProperty("DB_PASSWORD"));
         dataSource.setInitialSize(25);
         dataSource.setMaxActive(90);
         dataSource.setMaxIdle(30);
