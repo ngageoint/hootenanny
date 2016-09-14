2bf6632b218362cc52a72df4e910849584e02f45
Dmitriy Mylov <dmitriy.mylov@digitalglobe.com>
2016-09-14 13:49:48 -0400

    Added a TODO comment

--- hoot-services/src/main/java/hoot/services/SpringWebContainerInitializer.java
+++ hoot-services/src/main/java/hoot/services/SpringWebContainerInitializer.java
@@ -1,6 +1,8 @@
 package hoot.services;
 
 
+// TODO: Review this initializer when Hootnanny's is upgraded to Tomcat 8.5 which supports Servlet Spec 3.0
+// WebApplicationInitializer to be implemented in Servlet 3.0+
 //@Order(1)
 public class SpringWebContainerInitializer /*implements WebApplicationInitializer*/ {
 /*
