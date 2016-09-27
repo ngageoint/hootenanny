253a2b7625c6f0a159bb52c7c614c4c255af5e61
Brandon Witham <brandon.witham@digitalglobe.com>
2016-09-27 10:24:50 -0400

    make area matching experimental
    1. refs #205

--- rules/Area.js
+++ rules/Area.js
@@ -8,6 +8,7 @@ exports.matchThreshold = parseFloat(hoot.get("generic.polygon.match.threshold"))
 exports.missThreshold = parseFloat(hoot.get("generic.polygon.miss.threshold"));
 exports.reviewThreshold = parseFloat(hoot.get("generic.polygon.review.threshold"));
 exports.searchRadius = parseFloat(hoot.get("search.radius.generic.polygon"));
+exports.experimental = true;
 
 var sublineMatcher = new hoot.MaximalSublineStringMatcher();
 
