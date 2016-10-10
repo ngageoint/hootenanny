bd9e47954e5962632f7ff9fdbdedd24a9d449499
Brandon Witham <brandon.witham@digitalglobe.com>
2016-10-10 11:32:34 -0400

    remove total match count from MatchComparator
    1. refs #205

--- hoot-core/src/main/cpp/hoot/core/scoring/MatchComparator.cpp
+++ hoot-core/src/main/cpp/hoot/core/scoring/MatchComparator.cpp
@@ -755,13 +755,6 @@ QString MatchComparator::toString() const
   result += QString("total unnecessary review count: %1\n")
               .arg(_confusion[MatchType::Review][MatchType::Miss] +
                    _confusion[MatchType::Review][MatchType::Match]);
-  result += QString("total manual matches made: %1")
-              .arg(_confusion[MatchType::Match][MatchType::Miss] +
-                   _confusion[MatchType::Review][MatchType::Miss] +
-                   _confusion[MatchType::Match][MatchType::Match] +
-                   _confusion[MatchType::Review][MatchType::Match] +
-                   _confusion[MatchType::Match][MatchType::Review] +
-                   _confusion[MatchType::Review][MatchType::Review]);
   result += "\n";
 
   return result;
