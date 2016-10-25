05c1363487d028f08243827212c1a188a94b648a
Dmitriy Mylov <dmitriy.mylov@digitalglobe.com>
2016-10-25 14:55:33 -0400

    Reworked deleteRenderDb().  Previous implementation had bugs in it.

--- hoot-services/src/main/java/hoot/services/utils/DbUtils.java
+++ hoot-services/src/main/java/hoot/services/utils/DbUtils.java
@@ -216,10 +216,11 @@ public final class DbUtils {
      */
     public static void deleteRenderDb(String mapName) {
         List<Long> mapIds = getMapIdsByName(mapName);
+        Long mapId = null;
 
         try (Connection connection = getConnection()) {
             if (!mapIds.isEmpty()) {
-                long mapId = mapIds.get(0);
+                mapId = mapIds.get(0);
 
                 String currentCatalog;
                 try {
@@ -231,25 +232,100 @@ public final class DbUtils {
 
                 String dbNameByMapId = currentCatalog + "_renderdb_" + mapId;
                 try {
-                    deleteDb(dbNameByMapId);
+                    deleteDb(dbNameByMapId, connection);
                 }
                 catch (SQLException e1) {
-                    logger.warn("Error deleting {} database!  The database might not exist!  Cause: {}",
-                            dbNameByMapId, e1.getMessage());
+                    logger.warn("Error deleting {} database by map ID!", dbNameByMapId, e1);
 
                     String dbNameByMapName = currentCatalog + "_renderdb_" + mapName;
                     try {
-                        deleteDb(dbNameByMapName);
+                        deleteDb(dbNameByMapName, connection);
                     }
                     catch (SQLException e2) {
-                        logger.warn("Couldn't delete renderdb with name {} or {}.  Cause: {}",
-                                dbNameByMapId, dbNameByMapName, e2.getMessage());
+                        logger.warn("Couldn't delete {} by map name either!", dbNameByMapName, e2);
+                        throw e2;
                     }
                 }
             }
         }
         catch (SQLException e) {
-            logger.error("Error deleting renderdb for map = {}", mapName, e);
+            String message;
+            if (mapId != null) {
+                message = "Error deleting renderdb for map with ID = " + mapId;
+            }
+            else {
+                message = "Error deleting renderdb for map with name = " + mapName;
+            }
+
+            throw new RuntimeException(message, e);
+        }
+    }
+
+    private static void deleteDb(String dbName, Connection connection) throws SQLException {
+        // Straight SQL below. No DDL support in QueryDSL anymore. Have to do it the old-fashioned way.
+
+        try {
+            // NOTE: DROP DATABASE sql call cannot be run inside of a transaction.  That's why
+            // for the duration of this method we set auto commit to true to enable
+            // short running transactions - one transaction per execute* call.
+
+            if (!connection.getAutoCommit()) {
+                // Enable autoCommit
+                connection.setAutoCommit(true);
+            }
+
+            // 1) Make sure no one can connect to 'dbName' database.  Requires db owner privileges to execute.
+
+            String sql = "UPDATE pg_database SET datallowconn = 'false' WHERE datname = ?";
+            try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+                stmt.setString(1, dbName);
+                stmt.executeUpdate();
+            }
+
+            // 2) Force disconnection of all clients connected database, using pg_terminate_backend.
+            //    Requires superuser privileges.
+
+            String postgresqlDBVersion;  //Example: "PostgreSQL 9.2.1"
+
+            sql = "SELECT version()";
+            try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+                try (ResultSet rs = stmt.executeQuery()) {
+                    rs.next();
+                    postgresqlDBVersion = rs.getString(1);
+                }
+            }
+
+            if (postgresqlDBVersion.substring(11, 14).compareTo("9.2") < 0) {
+                // For Postgresql < 9.2 use:
+                sql = "SELECT pg_terminate_backend(pg_stat_activity.procpid) " +
+                        "FROM pg_stat_activity " +
+                        "WHERE pg_stat_activity.datname = ? AND procpid <> pg_backend_pid();";
+                try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+                    stmt.setString(1, dbName);
+                    stmt.executeQuery();
+                }
+            }
+            else {
+                // For Postgresql >= 9.2 use:
+                sql = "SELECT pg_terminate_backend(pg_stat_activity.pid) " +
+                        "FROM pg_stat_activity " +
+                        "WHERE pg_stat_activity.datname = ? AND pid <> pg_backend_pid()";
+                try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+                    stmt.setString(1, dbName);
+                    stmt.executeQuery();
+                }
+            }
+
+            // 3) Drop the database as the last step.  Requires database owner privilege.
+
+            sql = "DROP DATABASE \"" + dbName + "\"";
+            try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+                stmt.executeUpdate();
+            }
+        }
+        finally {
+            // Disable auto commit
+            connection.setAutoCommit(false);
         }
     }
 
@@ -510,52 +586,6 @@ public final class DbUtils {
                 try (PreparedStatement stmt = conn.prepareStatement(sql)) {
                     stmt.execute();
                 }
-            }
-        }
-    }
-
-    static void deleteDb(String dbName) throws SQLException {
-        try (Connection conn = getConnection()) {
-            // Straight SQL below. No DDL support in QueryDSL anymore. Have to do it the old-fashioned way.
-
-            // 1) Make sure no one can connect to this database.  Requires db owner privileges to execute.
-
-            String sql = "UPDATE pg_database SET datallowconn = 'false' WHERE datname = ?";
-            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-                stmt.setString(1, dbName);
-                stmt.executeUpdate();
-            }
-
-            // 2) Force disconnection of all clients connected database, using pg_terminate_backend.
-            //    Requires superuser privileges.
-
-            // For Postgresql < 9.2 use: SELECT pg_terminate_backend(procpid) FROM pg_stat_activity WHERE datname = 'mydb';
-            sql = "SELECT pg_terminate_backend(procpid) FROM pg_stat_activity WHERE datname = ?";
-            try {
-                try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-                    stmt.setString(1, dbName);
-                    stmt.executeQuery();
-                }
-            }
-            catch (Exception e) {
-                logger.debug("Failed to execute: {}", sql, e);
-            }
-
-            // For Postgresql >= 9.2 use: SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'mydb';
-            sql = "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = ?";
-            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-                stmt.setString(1, dbName);
-                stmt.executeQuery();
-            }
-            catch (Exception e) {
-                logger.debug("Failed to execute: {}", sql, e);
-            }
-
-            // 3) Drop the database as the last step.  Requires database owner privilege.
-
-            sql = "DROP DATABASE \"" + dbName + "\"";
-            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-                stmt.executeUpdate();
             }
         }
     }
