349cb7ee7bcfebd97182d3c92a5d4753a1823469
Brandon Witham <brandon.witham@digitalglobe.com>
2016-12-16 16:27:43 -0500

    consolidate db test email settings
    1. refs #1266

--- hoot-core/src/main/cpp/hoot/core/io/HootApiDbReader.cpp
+++ hoot-core/src/main/cpp/hoot/core/io/HootApiDbReader.cpp
@@ -5,7 +5,7 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -412,7 +412,7 @@ void HootApiDbReader::setConfiguration(const Settings& conf)
 {
   ConfigOptions configOptions(conf);
   setMaxElementsPerMap(configOptions.getMaxElementsPerPartialMap());
-  setUserEmail(configOptions.getHootapiDbReaderEmail());
+  setUserEmail(configOptions.getApiDbEmail());
   setBoundingBox(configOptions.getConvertBoundingBox());
 }
 
--- hoot-core/src/main/cpp/hoot/core/io/HootApiDbWriter.cpp
+++ hoot-core/src/main/cpp/hoot/core/io/HootApiDbWriter.cpp
@@ -5,7 +5,7 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -304,7 +304,7 @@ vector<long> HootApiDbWriter::_remapNodes(const vector<long>& nids)
 void HootApiDbWriter::setConfiguration(const Settings &conf)
 {
   ConfigOptions configOptions(conf);
-  setUserEmail(configOptions.getHootapiDbWriterEmail());
+  setUserEmail(configOptions.getApiDbEmail());
   setCreateUser(configOptions.getHootapiDbWriterCreateUser());
   setOverwriteMap(configOptions.getHootapiDbWriterOverwriteMap());
 }
