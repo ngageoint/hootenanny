26d2d11c4f7fe372da2ae4f98585b87ffe1beeba
Brandon Witham <brandon.witham@digitalglobe.com>
2016-12-19 10:02:46 -0500

    fix config options I missed
    1. refs #1266

--- hoot-core/src/main/cpp/hoot/core/io/ApiDbReader.h
+++ hoot-core/src/main/cpp/hoot/core/io/ApiDbReader.h
@@ -5,7 +5,7 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -50,6 +50,8 @@ namespace hoot
 class ApiDbReader
 {
 public:
+
+  static QString emailKey() { return "api.db.email"; }
 
   ApiDbReader();
   virtual ~ApiDbReader() {}
