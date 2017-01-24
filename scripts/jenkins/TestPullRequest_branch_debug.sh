f4343470f9de189b33624afe7da261f10e6fd67d
Brandon Witham <brandon.witham@digitalglobe.com>
2017-01-24 15:29:31 -0500

    add a script for custom jenkins test debugging 1. refs #1263

---
+++ scripts/jenkins/TestPullRequest_branch_debug.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# 
+
+set -x
+set -e
+
+cd $HOOT_HOME
+
+hoot --version --debug
+
+#export HOOT_TEST_DIFF=--diff
+#make -sj`nproc` pp-test
+#make -sj`nproc` plugins-test
+# Removing Poi test to troubleshoot hanging jobs on Jenkins
+#bin/HootTest $HOOT_TEST_DIFF --glacial --exclude=".*ConflatePoiStatsTest.*"
+#time -p make -sj`nproc` test-all-core
+
+# This is done in VagrantBuild.sh
+# cd $HOOT_HOME/docs
+# make -sj`nproc`
+
+#time -p make -sj`nproc` archive
+
+# TODO: make this smart enough to pass in custom test commands
+test-files/cmd/slow/ServiceDeriveChangesetCmdXmlJobIdTest.sh
