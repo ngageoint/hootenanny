74acd5a8d69aded1eac93ce42490ea8e21975806
Brandon Witham <brandon.witham@digitalglobe.com>
2017-01-25 14:57:49 -0500

    move network inputs reverse test back to slow

--- test-files/cmd/slow/NetworkConflateCmdInputReverseTest.sh
+++ test-files/cmd/slow/NetworkConflateCmdInputReverseTest.sh
@@ -4,6 +4,7 @@ set -e
 mkdir -p $HOOT_HOME/tmp/
 mkdir -p test-output/cmd/NetworkConflateCmdInputReverseTest
 
-#Let's see if we can conflate the other way, since that wasn't working at one point.
-hoot conflate -C Network.conf --warn test-files/DcTigerRoads.osm test-files/DcGisRoads.osm test-output/cmd/NetworkConflateCmdInputReverseTest/output.osm
+# Let's see if we can conflate the other way, since that wasn't working at one point.
+# The output is different, which is a little disturbing, but we'll deal with that later.
+hoot conflate --warn -C Network.conf test-files/DcTigerRoads.osm test-files/DcGisRoads.osm test-output/cmd/NetworkConflateCmdInputReverseTest/output.osm
 hoot is-match test-output/cmd/NetworkConflateCmdInputReverseTest/output.osm test-files/cmd/slow/NetworkConflateCmdInputReverseTest/output.osm || diff test-output/cmd/NetworkConflateCmdInputReverseTest/output.osm test-files/cmd/slow/NetworkConflateCmdInputReverseTest/output.osm 
