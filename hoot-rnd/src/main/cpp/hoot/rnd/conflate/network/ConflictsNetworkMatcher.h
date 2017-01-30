5820447ffc65cab668dafc2fd348e3b093232121
Brandon Witham <brandon.witham@digitalglobe.com>
2017-01-30 17:18:21 -0500

    cleanup

--- hoot-rnd/src/main/cpp/hoot/rnd/conflate/network/ConflictsNetworkMatcher.h
+++ hoot-rnd/src/main/cpp/hoot/rnd/conflate/network/ConflictsNetworkMatcher.h
@@ -5,7 +5,7 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -50,8 +50,7 @@ class ConflictsNetworkMatcherTest;
 /**
  *
  */
-class ConflictsNetworkMatcher :
-  public NetworkMatcher
+class ConflictsNetworkMatcher : public NetworkMatcher
 {
 public:
   static string className() { return "hoot::ConflictsNetworkMatcher"; }
