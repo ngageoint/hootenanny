2dd5080be06da80e7e58e4672c55f65141d48735
Brandon Witham <brandon.witham@digitalglobe.com>
2017-02-01 13:14:05 -0500

    more logging cleanup

--- hoot-core/src/main/cpp/hoot/core/util/Settings.cpp
+++ hoot-core/src/main/cpp/hoot/core/util/Settings.cpp
@@ -5,7 +5,7 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -158,7 +158,7 @@ void Settings::clear()
   // this can be very handy when determining why/when settings got cleared.
   if (this == _theInstance)
   {
-    LOG_INFO("Clearing global settings.");
+    LOG_DEBUG("Clearing global settings.");
   }
   _settings.clear();
 }
@@ -414,7 +414,7 @@ void Settings::loadEnvironment()
     parseCommonArguments(args);
     if (args.size() != 0)
     {
-      LOG_WARN("Error parsing all arguments in HOOT_OPTIONS: " << args);
+      throw HootException("Error parsing all arguments in HOOT_OPTIONS: " + args.join(";"));
     }
   }
 }
