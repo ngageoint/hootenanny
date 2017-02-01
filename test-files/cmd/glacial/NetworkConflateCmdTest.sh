b5379da44402ebc5004283488325e384b21224dd
Brandon Witham <brandon.witham@digitalglobe.com>
2017-02-01 09:56:06 -0500

    change suites for network tests

--- test-files/cmd/glacial/NetworkConflateCmdTest.sh
+++ test-files/cmd/glacial/NetworkConflateCmdTest.sh
@@ -5,7 +5,7 @@ mkdir -p $HOOT_HOME/tmp/
 mkdir -p test-output/cmd/NetworkConflateCmdTest
 
 hoot conflate --warn -C Network.conf -D uuid.helper.repeatable=true -D writer.include.debug=true test-files/DcGisRoads.osm test-files/DcTigerRoads.osm test-output/cmd/NetworkConflateCmdTest/output.osm
-hoot is-match test-output/cmd/NetworkConflateCmdTest/output.osm test-files/cmd/slow/NetworkConflateCmdTest/output.osm || diff test-output/cmd/NetworkConflateCmdTest/output.osm test-files/cmd/slow/NetworkConflateCmdTest/output.osm 
+hoot is-match test-output/cmd/NetworkConflateCmdTest/output.osm test-files/cmd/glacial/NetworkConflateCmdTest/output.osm || diff test-output/cmd/NetworkConflateCmdTest/output.osm test-files/cmd/glacial/NetworkConflateCmdTest/output.osm 
 
 # Check to make sure we don't bomb out on empty files
 hoot conflate --warn -C Network.conf -D uuid.helper.repeatable=true -D writer.include.debug=true test-files/Empty.osm test-files/Empty.osm tmp/dum.osm
