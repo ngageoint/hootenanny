5f1842727f366dbb310b554ddf27c39aba44b092
Brandon Witham <brandon.witham@digitalglobe.com>
2017-02-01 13:14:05 -0500

    more logging cleanup

--- hoot-core/src/main/cpp/hoot/core/conflate/highway/HighwayMatchCreator.cpp
+++ hoot-core/src/main/cpp/hoot/core/conflate/highway/HighwayMatchCreator.cpp
@@ -5,7 +5,7 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -164,14 +164,17 @@ public:
 
   Meters getSearchRadius(const shared_ptr<const Element>& e) const
   {
+    Meters searchRadius;
     if (_searchRadius >= 0)
     {
-      return _searchRadius;
+      searchRadius = _searchRadius;
     }
     else
     {
-      return e->getCircularError();
+      searchRadius = e->getCircularError();
     }
+    LOG_VART(searchRadius);
+    return searchRadius;
   }
 
   virtual void visit(const ConstElementPtr& e)
