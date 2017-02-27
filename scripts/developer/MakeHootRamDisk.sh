b77f7cc8ced0a69ef75fbd4880c2939846ad9032
Brandon Witham <brandon.witham@digitalglobe.com>
2017-02-27 22:42:33 +0000

    simplify script

--- scripts/developer/MakeHootRamDisk.sh
+++ scripts/developer/MakeHootRamDisk.sh
@@ -2,8 +2,4 @@
 
 sudo mkdir -p /media/ramdisk 
 sudo mount -t tmpfs -o size=2048M tmpfs /media/ramdisk
-cp -R hoot /media/ramdisk
-cd /media/ramdisk/hoot
-source ./SetupEnv.sh
-./configure -q --with-services --with-rnd --with-uitests
-make -j24
+cp -R ~/hoot /media/ramdisk
