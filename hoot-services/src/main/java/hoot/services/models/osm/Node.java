3a918c1b844c723fcf464b9b24e498cb5dcc3bc7
Brian Hatchl <brian.hatchl@digitalglobe.com>
2017-03-29 20:12:58 -0400

    hoot service modify changesets now increment element versions
    refs #1481

--- hoot-services/src/main/java/hoot/services/models/osm/Node.java
+++ hoot-services/src/main/java/hoot/services/models/osm/Node.java
@@ -5,7 +5,7 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -126,7 +126,7 @@ public class Node extends Element {
         // set these props at the very beginning, b/c they will be needed
         // regardless of whether following checks fail
         nodeRecord.setChangesetId(parseChangesetId(xmlAttributes));
-        nodeRecord.setVersion(parseVersion());
+        nodeRecord.setVersion(incrementVersion(xmlAttributes));
         nodeRecord.setTimestamp(parseTimestamp(xmlAttributes));
         nodeRecord.setVisible(true);
 
--- hoot-services/src/main/java/hoot/services/models/osm/Way.java
+++ hoot-services/src/main/java/hoot/services/models/osm/Way.java
@@ -5,7 +5,7 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -227,7 +227,7 @@ public class Way extends Element {
 
         CurrentWays wayRecord = (CurrentWays) super.record;
         wayRecord.setChangesetId(parseChangesetId(xmlAttributes));
-        wayRecord.setVersion(parseVersion());
+        wayRecord.setVersion(incrementVersion(xmlAttributes));
         wayRecord.setTimestamp(parseTimestamp(xmlAttributes));
         wayRecord.setVisible(true);
 
