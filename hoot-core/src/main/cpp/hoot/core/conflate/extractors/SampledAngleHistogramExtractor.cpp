7529399076404c0268f9c41b79a98799f892cbbf
Matt C. Jackson <matthew.jackson@digitalglobe.com>
2017-04-10 15:30:29 -0600

    Compiles and runs cleanly in Vagrant
    1. refs #1353

--- hoot-core/src/main/cpp/hoot/core/conflate/extractors/SampledAngleHistogramExtractor.cpp
+++ hoot-core/src/main/cpp/hoot/core/conflate/extractors/SampledAngleHistogramExtractor.cpp
@@ -40,6 +40,9 @@ using namespace geos::geom;
 #include <hoot/core/algorithms/WayHeading.h>
 #include <hoot/core/algorithms/linearreference/WayLocation.h>
 
+// Qt
+#include <qnumeric.h>
+
 namespace hoot
 {
 
@@ -84,7 +87,7 @@ public:
         const double distance = currentLoc.getCoordinate().distance(lastLoc.getCoordinate());
         //calculate the heading using some distance around the way
         const double theta = WayHeading::calculateHeading(currentLoc, _headingDelta);
-        if (!isnan(theta))
+        if (! ::qIsNaN(theta))
         {
           _angleHistogram.addAngle(theta, distance);
         }
