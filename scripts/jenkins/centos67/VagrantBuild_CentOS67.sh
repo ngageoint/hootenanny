f3b4da62642ca277c214c393fb3b93684549d4e7
sputnam <seth.putnam@digitalglobe.com>
2017-04-20 09:34:26 -0600

    Build script for CentOS67 with vagrant

---
+++ scripts/jenkins/centos67/VagrantBuild_CentOS67.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -e
+
+source ~/.bashrc
+cd hoot
+source ./SetupEnv.sh
+
+echo "### Configuring Hoot..."
+echo HOOT_HOME: $HOOT_HOME
+
+# Going to remove this so that it gets updated
+if [ -f missing ]; then
+  rm -f missing
+fi
+
+aclocal && autoconf && autoheader && automake --add-missing && ./configure --quiet --with-rnd --with-services
+
+echo "Building Hoot... "
+echo "Will take several extra minutes to build the training data the initial time Hootenanny is installed only."
+make -s clean && make -sj$(nproc)
+
+# Make sure that these directories exist. This is done by VagrantProvision.sh but will be wiped out if the user
+mkdir -p $HOOT_HOME/ingest/processed
+mkdir -p $HOOT_HOME/upload
+
+#make -sj$(nproc) docs &> /dev/null || true
+#make -sj$(nproc) docs
+
+hoot version
