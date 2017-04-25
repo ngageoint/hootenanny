323a7d01d94f9aaf63704bde4ebb9015eedff263
sputnam <seth.putnam@digitalglobe.com>
2017-04-25 16:42:16 -0600

    Remove redundant make and make copy/paste typo

--- scripts/jenkins/TestQuick.sh
+++ scripts/jenkins/TestQuick.sh
@@ -5,10 +5,8 @@ set -e
 
 cd $HOOT_HOME
 
-make -sj`nproc`
-
 hoot --version --debug
 
 export HOOT_TEST_DIFF=--diff
-make -sj`nproc` testmake -s -f Makefile.old services-test-all
+make -s -f Makefile.old services-test-all
 HootTest --quick
