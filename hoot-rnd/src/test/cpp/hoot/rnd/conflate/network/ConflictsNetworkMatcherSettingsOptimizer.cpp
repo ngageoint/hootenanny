2630e7b6c577e56a6c9789ffea960427414cacff
Brandon Witham <brandon.witham@digitalglobe.com>
2017-05-04 13:55:43 +0000

    turn off test again

--- hoot-rnd/src/test/cpp/hoot/rnd/conflate/network/ConflictsNetworkMatcherSettingsOptimizer.cpp
+++ hoot-rnd/src/test/cpp/hoot/rnd/conflate/network/ConflictsNetworkMatcherSettingsOptimizer.cpp
@@ -48,7 +48,7 @@ namespace hoot
 class ConflictsNetworkMatcherSettingsOptimizer : public CppUnit::TestFixture
 {
   CPPUNIT_TEST_SUITE(ConflictsNetworkMatcherSettingsOptimizer);
-  CPPUNIT_TEST(optimizeAgainstCaseDataTest);
+  //CPPUNIT_TEST(optimizeAgainstCaseDataTest);
   CPPUNIT_TEST_SUITE_END();
 
 public:
