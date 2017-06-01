b36975f39da0cfbeb3928172b7e94bf13635c69a
sputnam <seth.putnam@digitalglobe.com>
2017-06-01 17:03:01 -0600

    Add other exclude to remove error

--- scripts/jenkins/TestQuick.sh
+++ scripts/jenkins/TestQuick.sh
@@ -10,6 +10,7 @@ hoot --version --debug
 export HOOT_TEST_DIFF=--diff
 make -s -f Makefile.old services-test-all
 HootTest --exclude=.*ConflateAverageTest.sh \
+         --exclude=.*RubberSheetConflateTest.sh \
          --exclude=.*ExactMatchInputsTest.sh \
          --exclude=.*RafahConflateTest.sh \
          --exclude=.*HaitiDrConflateTest.sh \
