fcfe4bc962a3290755d0465a318f9a39ccadc7be
Brandon Witham <brandon.witham@digitalglobe.com>
2017-06-08 18:42:50 +0000

    remove accidental change
    1. refs #1093

--- scripts/TestConfigure.sh
+++ scripts/TestConfigure.sh
@@ -27,8 +27,53 @@ function build {
 echo "### 01. --without-cppunit"
 ./configure -q --without-cppunit                                                                    && build_notest
 
+echo "### 02. --without-stxxl"
+./configure -q                                                                      --without-stxxl && build
+
+echo "### 03. --without-newmat"
+./configure -q                   --without-newmat                                                   && build
+
+echo "### 04. --without-log4cxx"
+./configure -q                                    --without-log4cxx                                 && build
+
+echo "### 05. --with-services"
+./configure -q                                                      --with-services                 && build
+
+echo "### 06. --without-cppunit --without-newmat"
+./configure -q --without-cppunit --without-newmat                                                   && build_notest
+
+echo "### 07. --without-cppunit --without-newmat --without-log4cxx"
+./configure -q --without-cppunit --without-newmat --without-log4cxx                                 && build_notest
+
+echo "### 08. --without-cppunit --without-newmat --without-log4cxx --with-services"
+./configure -q --without-cppunit --without-newmat --without-log4cxx --with-services                 && build_notest
+
+echo "### 09. --without-newmat --with-services"
+./configure -q                   --without-newmat                   --with-services                 && build
+
+echo "### 10. --without-newmat --without-log4cxx"
+./configure -q                   --without-newmat --without-log4cxx                                 && build
+
+echo "### 11. --without-newmat --without-log4cxx --with-services"
+./configure -q                   --without-newmat --without-log4cxx --with-services                 && build
+
+echo "### 12. --without-log4cxx --with-services"
+./configure -q                                    --without-log4cxx --with-services                 && build
+
+echo "### 13. --without-log4cxx --with-services --without-stxxl"
+./configure -q                                    --without-log4cxx --with-services --without-stxxl && build
+
+echo "### 14. ./configure -q"
+./configure -q
+
 echo "### 15. --without-cppunit --without-newmat --without-log4cxx --with-services --without-stxxl --with-rnd"
 ./configure -q --without-cppunit --without-newmat --without-log4cxx --with-services --without-stxxl --with-rnd && build_notest
+
+echo "### 16. --without-newmat --without-log4cxx --with-services --without-stxxl --with-rnd"
+./configure -q                   --without-newmat --without-log4cxx --with-services --without-stxxl --with-rnd && build
+
+echo "### 17. --with-rnd"
+./configure -q                                                                                      --with-rnd && build
 
 echo "############"
 echo "# Success! #"
