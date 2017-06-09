7b439de03984807c47e3b38de04bc8b520d9c0b2
Brian Hatchl <brian.hatchl@digitalglobe.com>
2017-06-09 09:37:21 -0400

    using name chrome_timeout still caused issues, revert to selenium

--- test-files/ui/features/support/env.rb
+++ test-files/ui/features/support/env.rb
@@ -7,17 +7,14 @@ require 'capybara-screenshot/cucumber'
 require 'selenium-webdriver'
 
 # Chrome instance with timeout is set to 100 seconds
-Capybara.register_driver :chrome_timeout do |app|
+Capybara.register_driver :selenium do |app|
   client = Selenium::WebDriver::Remote::Http::Default.new
   client.timeout = 100
   Capybara::Selenium::Driver.new(app, :browser => :chrome, :http_client => client)
 end
 
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
-end
-Capybara.javascript_driver = :chrome_timeout
-Capybara.default_driver = :chrome_timeout
+Capybara.javascript_driver = :selenium
+Capybara.default_driver = :selenium
 
 Capybara.run_server = false
 Capybara.default_selector = :css
@@ -37,8 +34,5 @@ end
 
 World(Capybara::DSL, Helpers)
 
-Capybara::Screenshot.register_driver(:chrome_timeout) do |driver, path|
-#noop
-end
 # Keep only the screenshots generated from the last failing test suite
 Capybara::Screenshot.prune_strategy = :keep_last_run
