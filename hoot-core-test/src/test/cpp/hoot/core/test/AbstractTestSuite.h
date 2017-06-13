bb2304593a5d61ed7db4108c50b6e94d15c8e769
Brandon Witham <brandon.witham@digitalglobe.com>
2017-06-13 15:08:42 +0000

    fix copyrights
    1. refs #1093

--- hoot-core-test/src/test/cpp/hoot/core/test/AbstractTestSuite.h
+++ hoot-core-test/src/test/cpp/hoot/core/test/AbstractTestSuite.h
@@ -22,7 +22,7 @@
  * This will properly maintain the copyright information. DigitalGlobe
  * copyrights will be updated automatically.
  *
- * @copyright Copyright (C) 2015 DigitalGlobe (http://www.digitalglobe.com/)
+ * @copyright Copyright (C) 2017 DigitalGlobe (http://www.digitalglobe.com/)
  */
 #ifndef ABSTRACTTESTSUITE_H
 #define ABSTRACTTESTSUITE_H
