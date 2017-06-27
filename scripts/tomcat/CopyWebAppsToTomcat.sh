956644887a71ae6ad452f502a1abc8ae056c0502
Brian Hatchl <brian.hatchl@digitalglobe.com>
2017-06-27 13:06:48 -0400

    ensure hoot-ui dist files are made before deploying

--- scripts/tomcat/CopyWebAppsToTomcat.sh
+++ scripts/tomcat/CopyWebAppsToTomcat.sh
@@ -14,23 +14,12 @@ rm -rf $TOMCAT8_HOME/webapps/hoot-services/
 
 cp `ls -t hoot-services/target/hoot-services-*.war | sed -n 1p` $TOMCAT8_HOME/webapps/hoot-services.war
 
+cd hoot-ui
+make clean && make
 rm -rf $TOMCAT8_HOME/webapps/hootenanny-id/
+cp -R dist $TOMCAT8_HOME/webapps/hootenanny-id
 mkdir -p $TOMCAT8_HOME/webapps/hootenanny-id/data
 cp hoot-ui/data/osm-plus-taginfo.csv $TOMCAT8_HOME/webapps/hootenanny-id/data
 cp hoot-ui/data/tdsv61_field_values.json $TOMCAT8_HOME/webapps/hootenanny-id/data
-# cp -R hoot-ui $TOMCAT8_HOME/webapps/hootenanny-id
-
-# rm -rf $TOMCAT8_HOME/webapps/hootenanny-id/css/img
-# cp -R $TOMCAT8_HOME/webapps/hootenanny-id/dist/img $TOMCAT8_HOME/webapps/hootenanny-id/css/
-
-#rm -f $TOMCAT8_HOME/webapps/hoot-services.war
-#rm -rf $TOMCAT8_HOME/webapps/hoot-services/
-#cp $HOOT_HOME/hoot-services/target/hoot-services-*.war $TOMCAT8_HOME/webapps/hoot-services.war
-#unzip -q -d $TOMCAT8_HOME/webapps/hoot-services $TOMCAT8_HOME/webapps/hoot-services.war
-#cp $HOOT_HOME/hoot-services/src/main/resources/conf/local.conf $TOMCAT8_HOME/webapps/hoot-services/WEB-INF/classes/conf
-#rm -rf $TOMCAT8_HOME/webapps/hootenanny-id/
-#cp -R $HOOT_HOME/hoot-ui $TOMCAT8_HOME/webapps/hootenanny-id
-#rm -rf $TOMCAT8_HOME/webapps/hootenanny-id/css/img
-#cp -R $TOMCAT8_HOME/webapps/hootenanny-id/dist/img $TOMCAT8_HOME/webapps/hootenanny-id/css/
 
 echo Web application files deployed.
--- scripts/tomcat/vagrantDeployTomcat.sh
+++ scripts/tomcat/vagrantDeployTomcat.sh
@@ -2,13 +2,10 @@
 
 export TOMCAT8_HOME=/usr/share/tomcat8
 
-#rm -rf $TOMCAT8_HOME/webapps/hoot-services
-#rm -f $TOMCAT8_HOME/webapps/hoot-services.war
-#cp `ls -t hoot-services/target/hoot-services-*.war | sed -n 1p` $TOMCAT8_HOME/webapps/hoot-services.war
+cd hoot-ui
+make clean && make
 rm -rf $TOMCAT8_HOME/webapps/hootenanny-id/
-cp -R hoot-ui/dist $TOMCAT8_HOME/webapps/hootenanny-id
+cp -R dist $TOMCAT8_HOME/webapps/hootenanny-id
 mkdir -p $TOMCAT8_HOME/webapps/hootenanny-id/data
-cp hoot-ui/data/osm-plus-taginfo.csv $TOMCAT8_HOME/webapps/hootenanny-id/data
-cp hoot-ui/data/tdsv61_field_values.json $TOMCAT8_HOME/webapps/hootenanny-id/data
-# rm -rf $TOMCAT8_HOME/webapps/hootenanny-id/css/img
-# cp -R $TOMCAT8_HOME/webapps/hootenanny-id/dist/img $TOMCAT8_HOME/webapps/hootenanny-id/css/
+cp data/osm-plus-taginfo.csv $TOMCAT8_HOME/webapps/hootenanny-id/data
+cp data/tdsv61_field_values.json $TOMCAT8_HOME/webapps/hootenanny-id/data
