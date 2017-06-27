23ea8526ab4de6c5cc5ebf093f80ca02281c5b84
Brian Hatchl <brian.hatchl@digitalglobe.com>
2017-06-27 11:34:23 -0400

    use dist file for production deployment
    refs https://github.com/ngageoint/hootenanny-ui/issues/812

--- scripts/tomcat/CopyWebAppsToTomcat.sh
+++ scripts/tomcat/CopyWebAppsToTomcat.sh
@@ -15,10 +15,13 @@ rm -rf $TOMCAT8_HOME/webapps/hoot-services/
 cp `ls -t hoot-services/target/hoot-services-*.war | sed -n 1p` $TOMCAT8_HOME/webapps/hoot-services.war
 
 rm -rf $TOMCAT8_HOME/webapps/hootenanny-id/
-cp -R hoot-ui $TOMCAT8_HOME/webapps/hootenanny-id
+mkdir -p $TOMCAT8_HOME/webapps/hootenanny-id/data
+cp hoot-ui/data/osm-plus-taginfo.csv $TOMCAT8_HOME/webapps/hootenanny-id/data
+cp hoot-ui/data/tdsv61_field_values.json $TOMCAT8_HOME/webapps/hootenanny-id/data
+# cp -R hoot-ui $TOMCAT8_HOME/webapps/hootenanny-id
 
-rm -rf $TOMCAT8_HOME/webapps/hootenanny-id/css/img
-cp -R $TOMCAT8_HOME/webapps/hootenanny-id/dist/img $TOMCAT8_HOME/webapps/hootenanny-id/css/
+# rm -rf $TOMCAT8_HOME/webapps/hootenanny-id/css/img
+# cp -R $TOMCAT8_HOME/webapps/hootenanny-id/dist/img $TOMCAT8_HOME/webapps/hootenanny-id/css/
 
 #rm -f $TOMCAT8_HOME/webapps/hoot-services.war
 #rm -rf $TOMCAT8_HOME/webapps/hoot-services/
--- scripts/tomcat/vagrantDeployTomcat.sh
+++ scripts/tomcat/vagrantDeployTomcat.sh
@@ -2,10 +2,13 @@
 
 export TOMCAT8_HOME=/usr/share/tomcat8
 
-rm -rf $TOMCAT8_HOME/webapps/hoot-services
-rm -f $TOMCAT8_HOME/webapps/hoot-services.war
-cp `ls -t hoot-services/target/hoot-services-*.war | sed -n 1p` $TOMCAT8_HOME/webapps/hoot-services.war
+#rm -rf $TOMCAT8_HOME/webapps/hoot-services
+#rm -f $TOMCAT8_HOME/webapps/hoot-services.war
+#cp `ls -t hoot-services/target/hoot-services-*.war | sed -n 1p` $TOMCAT8_HOME/webapps/hoot-services.war
 rm -rf $TOMCAT8_HOME/webapps/hootenanny-id/
-cp -R hoot-ui $TOMCAT8_HOME/webapps/hootenanny-id
-rm -rf $TOMCAT8_HOME/webapps/hootenanny-id/css/img
-cp -R $TOMCAT8_HOME/webapps/hootenanny-id/dist/img $TOMCAT8_HOME/webapps/hootenanny-id/css/
+cp -R hoot-ui/dist $TOMCAT8_HOME/webapps/hootenanny-id
+mkdir -p $TOMCAT8_HOME/webapps/hootenanny-id/data
+cp hoot-ui/data/osm-plus-taginfo.csv $TOMCAT8_HOME/webapps/hootenanny-id/data
+cp hoot-ui/data/tdsv61_field_values.json $TOMCAT8_HOME/webapps/hootenanny-id/data
+# rm -rf $TOMCAT8_HOME/webapps/hootenanny-id/css/img
+# cp -R $TOMCAT8_HOME/webapps/hootenanny-id/dist/img $TOMCAT8_HOME/webapps/hootenanny-id/css/
