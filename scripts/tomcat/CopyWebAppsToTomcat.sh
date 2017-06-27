4171252ea9605ffbc40956a7d3829c03b2cf099c
Brian Hatchl <brian.hatchl@digitalglobe.com>
2017-06-27 16:27:24 -0400

    on second thought, don't build the iD dist in these scripts

--- scripts/tomcat/CopyWebAppsToTomcat.sh
+++ scripts/tomcat/CopyWebAppsToTomcat.sh
@@ -14,10 +14,8 @@ rm -rf $TOMCAT8_HOME/webapps/hoot-services/
 
 cp `ls -t hoot-services/target/hoot-services-*.war | sed -n 1p` $TOMCAT8_HOME/webapps/hoot-services.war
 
-cd hoot-ui
-make clean && make
 rm -rf $TOMCAT8_HOME/webapps/hootenanny-id/
-cp -R dist $TOMCAT8_HOME/webapps/hootenanny-id
+cp -R hoot-ui/dist $TOMCAT8_HOME/webapps/hootenanny-id
 mkdir -p $TOMCAT8_HOME/webapps/hootenanny-id/data
 cp hoot-ui/data/osm-plus-taginfo.csv $TOMCAT8_HOME/webapps/hootenanny-id/data
 cp hoot-ui/data/tdsv61_field_values.json $TOMCAT8_HOME/webapps/hootenanny-id/data
--- scripts/tomcat/vagrantDeployTomcat.sh
+++ scripts/tomcat/vagrantDeployTomcat.sh
@@ -2,10 +2,11 @@
 
 export TOMCAT8_HOME=/usr/share/tomcat8
 
-cd hoot-ui
-make clean && make
+rm -rf $TOMCAT8_HOME/webapps/hoot-services
+rm -f $TOMCAT8_HOME/webapps/hoot-services.war
+cp `ls -t hoot-services/target/hoot-services-*.war | sed -n 1p` $TOMCAT8_HOME/webapps/hoot-services.war
 rm -rf $TOMCAT8_HOME/webapps/hootenanny-id/
-cp -R dist $TOMCAT8_HOME/webapps/hootenanny-id
+cp -R hoot-ui/dist $TOMCAT8_HOME/webapps/hootenanny-id
 mkdir -p $TOMCAT8_HOME/webapps/hootenanny-id/data
-cp data/osm-plus-taginfo.csv $TOMCAT8_HOME/webapps/hootenanny-id/data
-cp data/tdsv61_field_values.json $TOMCAT8_HOME/webapps/hootenanny-id/data
+cp hoot-ui/data/osm-plus-taginfo.csv $TOMCAT8_HOME/webapps/hootenanny-id/data
+cp hoot-ui/data/tdsv61_field_values.json $TOMCAT8_HOME/webapps/hootenanny-id/data
