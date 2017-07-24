323523fd6e419b76d61469b1c1c6d7c1720a99b9
Brian Hatchl <brian.hatchl@digitalglobe.com>
2017-07-24 18:56:16 -0400

    Enable GGDMv3.0 as option for export and translation servers  (#1677)
    * Add GGDMv30 translation to node translation and export servers
    
    and add some mocha tests
    
    refs #1668
    
    * Fixes for missing GGDM30 metadata
    
    1. refs #1668
    
    * add a OSM to GGDM Raw test
    
    * Synced the to/from english GGDM30 scripts with TDSv61
    
    1. refs #1668
    
    * Some GGDM30 fixes
    
    1. ref #1668
    Added some fixes from TDSv61 to GGDM.

--- translations/OSM_to_englishGGDM30.js
+++ translations/OSM_to_englishGGDM30.js
@@ -61,6 +61,19 @@ function initialize()
 {
     // Turn off the TDS structure so we just get the raw feature
     hoot.Settings.set({"ogr.thematic.structure":"false"});
+
+        // Turn off the tds extra function
+    hoot.Settings.set({"ogr.note.extra":"none"});
+
+    // Turn off the ESRI FCSUBTYPE
+    hoot.Settings.set({"ogr.esri.fcsubtype":"false"});
+
+    // Throw errors instead of returning partial translations/o2s_X features
+    hoot.Settings.set({"ogr.throw.error":"true"});
+
+    // Set the schema type for the export
+    hoot.Settings.set({"osm.map.writer.schema":"GGDMv30"});
+
 }
 
 
