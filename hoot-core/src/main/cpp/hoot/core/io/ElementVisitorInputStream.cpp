75527ec9644b1e4863eb3122436266f65a4fcd84
Jason R. Surratt <jason.surratt@digitalglobe.com>
2017-08-10 13:33:12 -0600

    Update header copyrights
    1. refs #1715

--- hoot-core/src/main/cpp/hoot/core/io/ElementVisitorInputStream.cpp
+++ hoot-core/src/main/cpp/hoot/core/io/ElementVisitorInputStream.cpp
@@ -22,7 +22,7 @@
  * This will properly maintain the copyright information. DigitalGlobe
  * copyrights will be updated automatically.
  *
- * @copyright Copyright (C) 2017 DigitalGlobe (http://www.digitalglobe.com/)
+ * @copyright Copyright (C) 2015, 2017 DigitalGlobe (http://www.digitalglobe.com/)
  */
 #include <boost/shared_ptr.hpp>
 #include <hoot/core/io/ElementInputStream.h>
