b12b49f3b03fc293d8950d7f42d04d363f167935
Matt C. Jackson <matthew.jackson@digitalglobe.com>
2017-08-18 16:16:45 -0600

    Added ui-tests back into the config line
    refa #1042

--- VagrantBuild.sh
+++ VagrantBuild.sh
@@ -25,7 +25,7 @@ fi
 
 # Taking out ui-tests until we get Tomcat8 etc installed
 # aclocal && autoconf && autoheader && automake --add-missing --copy && ./configure --quiet --with-rnd --with-services --with-uitests
-aclocal && autoconf && autoheader && automake --add-missing --copy && ./configure --with-rnd --with-services
+aclocal && autoconf && autoheader && automake --add-missing --copy && ./configure --with-rnd --with-services --with-uitests
 
 if [ ! -f LocalConfig.pri ] && ! grep --quiet QMAKE_CXX LocalConfig.pri; then
     echo 'Customizing LocalConfig.pri...'
