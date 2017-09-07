ed745e7b3a28bb0becc222cab5fa4eb57b5d8856
Micah Schicker <micah.schicker@digitalglobe.com>
2017-09-07 14:31:58 -0600

    Add make command to TestPullRequest_ui.sh

--- scripts/jenkins/TestPullRequest_ui.sh
+++ scripts/jenkins/TestPullRequest_ui.sh
@@ -6,6 +6,19 @@ set -e
 
 cd $HOOT_HOME
 
+aclocal
+autoconf
+autoheader
+automake --add-missing
+./configure --with-rnd --with-services --with-uitests
+
+make -sj`nproc`
+
+cd $HOOT_HOME/docs
+make -sj`nproc`
+
+cd $HOOT_HOME
+
 hoot --version --debug
 
 # activate EGD plugin so it's available for cucumber tests
