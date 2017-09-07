cc9c68f4a493fbc49b62b64cacce5c71d1fb4e3e
Micah Schicker <micah.schicker@digitalglobe.com>
2017-09-07 14:51:14 -0600

    Undo changes in TestPullRequest_ui.sh

--- scripts/jenkins/TestPullRequest_ui.sh
+++ scripts/jenkins/TestPullRequest_ui.sh
@@ -6,19 +6,6 @@ set -e
 
 cd $HOOT_HOME
 
-aclocal
-autoconf
-autoheader
-automake --add-missing
-./configure --with-rnd --with-services --with-uitests
-
-make -sj`nproc`
-
-cd $HOOT_HOME/docs
-make -sj`nproc`
-
-cd $HOOT_HOME
-
 hoot --version --debug
 
 # activate EGD plugin so it's available for cucumber tests
