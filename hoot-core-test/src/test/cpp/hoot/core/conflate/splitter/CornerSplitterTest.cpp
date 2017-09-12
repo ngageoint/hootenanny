1f2f047439e2b42f2cd40d712468d07677d1e77f
Micah Schicker <mschicker@users.noreply.github.com>
2017-09-12 08:29:52 -0600

    Corner Splitter (#1786)
    * Add CornerSplitter files
    
    * Icremental commit
    
    * Corner Splitter Test
    
    * Update some comments on the CornerSplitter
    
    * Forgot corner splitter test file
    
    * Add userfiles dir structure creation to makefile
    
    * Add make command to TestPullRequest_ui.sh
    
    * Undo changes in TestPullRequest_ui.sh
    
    * Add -f option to halt to try to solve some issues we are seeing on Jenkins
    
    * Code review changes. Remove a non-defined function, remove an experimental flag
    
    * Fix typos, add a comment

---
+++ hoot-core-test/src/test/cpp/hoot/core/conflate/splitter/CornerSplitterTest.cpp
@@ -0,0 +1,88 @@
+/*
+ * This file is part of Hootenanny.
+ *
+ * Hootenanny is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * --------------------------------------------------------------------
+ *
+ * The following copyright notices are generated automatically. If you
+ * have a new notice to add, please use the format:
+ * " * @copyright Copyright ..."
+ * This will properly maintain the copyright information. DigitalGlobe
+ * copyrights will be updated automatically.
+ *
+ * @copyright Copyright (C) 2017 DigitalGlobe (http://www.digitalglobe.com/)
+ */
+
+// Hoot
+#include <hoot/core/OsmMap.h>
+#include <hoot/core/util/MapProjector.h>
+#include <hoot/core/conflate/splitter/CornerSplitter.h>
+#include <hoot/core/io/OsmXmlReader.h>
+#include <hoot/core/io/OsmXmlWriter.h>
+using namespace hoot;
+
+
+// Boost
+using namespace boost;
+
+// CPP Unit
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/TestAssert.h>
+#include <cppunit/TestFixture.h>
+
+// Qt
+#include <QDebug>
+#include <QDir>
+
+#include "../../TestUtils.h"
+
+class CornerSplitterTest : public CppUnit::TestFixture
+{
+    CPPUNIT_TEST_SUITE(CornerSplitterTest);
+    CPPUNIT_TEST(runTest);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+
+  void runTest()
+  {
+    QDir().mkpath("test-output/conflate/splitter");
+
+    OsmXmlReader reader;
+
+    OsmMap::resetCounters();
+
+    OsmMapPtr map(new OsmMap());
+    reader.setDefaultStatus(Status::Unknown1);
+    reader.read("test-files/conflate/splitter/CornerSplitter.osm", map);
+
+    MapProjector::projectToWgs84(map);
+
+    CornerSplitter::splitCorners(map);
+
+
+    OsmXmlWriter writer;
+    writer.setIncludeCompatibilityTags(false);
+    writer.write(map, "test-output/conflate/splitter/CornerSplitterOut.osm");
+
+    HOOT_FILE_EQUALS("test-output/conflate/splitter/CornerSplitterOut.osm",
+                     "test-files/conflate/splitter/CornerSplitterExpected.osm");
+  }
+
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(CornerSplitterTest);
+
