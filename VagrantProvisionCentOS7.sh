04de08778db3f3afc382c07ed25be0171ab1f127
Ben Marchant <ben_marchant@hotmail.com>
2017-10-19 11:15:07 -0500

    Updated NodeJs and v8 installs

--- VagrantProvisionCentOS7.sh
+++ VagrantProvisionCentOS7.sh
@@ -66,14 +66,11 @@ sudo yum -y install \
     glpk \
     glpk-devel \
     gnuplot \
-    libicu-devel \
     libpng-devel \
     libtool \
     m4 \
     maven \
     mlocate \
-    nodejs \
-    nodejs-devel \
     opencv \
     opencv-core \
     opencv-devel \
@@ -102,7 +99,6 @@ sudo yum -y install \
     swig \
     tex* \
     unzip \
-    v8-devel \
     vim \
     w3m \
     wget \
@@ -155,7 +151,43 @@ sudo alternatives --set javac /usr/java/jdk1.8.0_${JAVA_VERSION}/bin/javac
 
 echo "##### Temp installs #####"
 
+# ICU:
+cd ~
+wget http://download.icu-project.org/files/icu4c/59.1/icu4c-59_1-src.tgz
+tar xzf icu4c-59_1-src.tgz
+cd icu/source/
+./runConfigureICU Linux
+gmake -j4
+sudo gmake install
+
+# Google depot tools
+cd ~
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+export PATH=$PATH:~/depot_tools
+
+# V8
+cd ~
+fetch v8
+cd v8
+gclient sync
+tools/dev/v8gen.py x64.release
+make x64.release library=shared -j4
+isudo cp out/x64.release/lib.target/*.so /usr/lib64/
+sudo cp include/*.h /usr/include/
+sudo cp -r include/libplatform /usr/include
+
+# Node
+wget https://nodejs.org/dist/v6.11.4/node-v6.11.4-linux-x64.tar.xz
+tar xf node-v6.11.4-linux-x64.tar.xz 
+cd node-v6.11.4-linux-x64
+sudo cp -r bin /usr/
+sudo cp -r include /usr/
+sudo cp -r lib /usr/
+sudo cp -r share /usr/
+
+
 # Stxxl:
+cd ~
 git clone http://github.com/stxxl/stxxl.git stxxl
 cd stxxl
 git checkout -q tags/1.3.1
