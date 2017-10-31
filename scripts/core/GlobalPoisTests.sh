40defd52ffecbae64001738666601ff9ed3fbf23
Jason R. Surratt <jason.surratt@digitalglobe.com>
2017-10-31 12:25:02 -0600

    Remove false error

--- scripts/core/GlobalPoisTests.sh
+++ scripts/core/GlobalPoisTests.sh
@@ -2,9 +2,9 @@
 set -e
 
 # Disable the tests that aren't running on Ubuntu 16.04. They aren't relevant to global POIs.
-#hoot --version --debug; HootTest --slow --exclude=.*PrintNamesCmdTest.sh --exclude=.*ConflateCmdStatsGenericRiversTest.sh --exclude=.*PertyScoreCmdTest.sh --exclude=.*ScoreCmdTest.sh --exclude=.*StatsFileTest.sh --exclude=.*PertyMatchScorerTest.* --exclude=.*BigPertyOpTest.* --exclude=.*TDistributionTest.* --exclude=.*ExpectationIntersectionTest.* --exclude=.*PertyDuplicatePoiOpTest.* --exclude=hoot::PertyOpTest.* --parallel `nproc`
+# These failures are likely due to library changes.
+hoot --version --debug; HootTest --slow --exclude=.*PrintNamesCmdTest.sh --exclude=.*ConflateCmdStatsGenericRiversTest.sh --exclude=.*PertyScoreCmdTest.sh --exclude=.*ScoreCmdTest.sh --exclude=.*StatsFileTest.sh --exclude=.*PertyMatchScorerTest.* --exclude=.*BigPertyOpTest.* --exclude=.*TDistributionTest.* --exclude=.*ExpectationIntersectionTest.* --exclude=.*PertyDuplicatePoiOpTest.* --exclude=hoot::PertyOpTest.* --parallel `nproc`
 
+# This should be re-enabled when it is working as expected
 #make plugins-test
 
-echo false error
-false
