e017b2a803eeadb0d6bf41e9b9b521448ca99770
Brian Hatchl <brian.hatchl@digitalglobe.com>
2017-11-09 09:56:16 -0700

    Add node script to generate validation mappings json for JOSM (#1945)
    refs #1944

---
+++ plugins/validation_mocks.js
@@ -0,0 +1,13 @@
+mgcp = {},
+ggdm30 = {},
+tds = {},
+tds61 = {},
+schemaTools = {
+    simple: function(x, y) {
+        return null;
+    },
+    isA: function(x, y) {
+        return null;
+    }
+};
+
