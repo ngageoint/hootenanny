88ff6620e364a292ec215483de6bddfae9cd871c
Ben Marchant <ben_marchant@hotmail.com>
2017-11-09 20:19:00 -0600

    Set restriction relations to use GEOS_GEOMETRYCOLLECTION geometry type instead of failing to export (#1952)

--- hoot-core/src/main/cpp/hoot/core/util/MetadataTags.h
+++ hoot-core/src/main/cpp/hoot/core/util/MetadataTags.h
@@ -131,6 +131,7 @@ public:
   inline static const QString RelationMultiPolygon()    { return "multipolygon"; }
   inline static const QString RelationNetwork()         { return "network"; }
   inline static const QString RelationOuter()           { return "outer"; }
+  inline static const QString RelationRestriction()     { return "restriction"; }
   inline static const QString RelationReview()          { return "review"; }
   inline static const QString RelationRoute()           { return "route"; }
   inline static const QString RelationRouteMaster()     { return "route_master"; }
