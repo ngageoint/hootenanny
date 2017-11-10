bd7e4b308f32fa8b699325f0c56360fa3ef3c3fe
Brandon Witham <brandon.witham@digitalglobe.com>
2017-11-10 18:54:10 +0000

    enable implicit tags command line tests
    1. refs #1797

--- test-files/cmd/quick/DerivePoiImplicitTagRulesCmdTest.sh
+++ test-files/cmd/quick/DerivePoiImplicitTagRulesCmdTest.sh
@@ -7,7 +7,7 @@ set -e
 OPT_COMMAND=`hoot | grep derive-poi-implicit-tag-rules`
 #echo $OPT_COMMAND
 if [ -z "$OPT_COMMAND" ]; then
-  #echo "multiary-ingest command not present.  Exiting test without running it."
+  #echo "derive-poi-implicit-tag-rules command not present.  Exiting test without running it."
  exit 0
 fi
 
@@ -18,7 +18,7 @@ INPUT_DIR=test-files/io/PoiImplicitTagRulesDeriverTest
 rm -rf $OUTPUT_DIR
 mkdir -p $OUTPUT_DIR
 
-hoot derive-poi-implicit-tag-rules --warn "$INPUT_DIR/yemen-crop-2.osm.pbf;$INPUT_DIR/philippines-1.osm.pbf" "translations/OSM_Ingest.js;translations/OSM_Ingest.js" "$OUTPUT_DIR/DerivePoiImplicitTagRulesCmdTest-out.json;$OUTPUT_DIR/DerivePoiImplicitTagRulesCmdTest-out.tsv;$OUTPUT_DIR/DerivePoiImplicitTagRulesCmdTest-out.sqlite" "amenity;tourism;building" 4
+hoot derive-poi-implicit-tag-rules --warn $INPUT_DIR/PoiImplicitTagRulesDeriverTest-input.implicitTagRules "$OUTPUT_DIR/DerivePoiImplicitTagRulesCmdTest-out.json;$OUTPUT_DIR/DerivePoiImplicitTagRulesCmdTest-out.tsv;$OUTPUT_DIR/DerivePoiImplicitTagRulesCmdTest-out.sqlite"
 
 #only checking the json and tsv files here
 diff $REF_DIR/DerivePoiImplicitTagRulesCmdTest.json $OUTPUT_DIR/DerivePoiImplicitTagRulesCmdTest-out.json
