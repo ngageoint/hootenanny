2012710bd0616ec6f0f3da4bb88d84534d4b5ce0
Ben Marchant <benjamin.marchant@digitalglobe.com>
2017-12-18 14:55:28 -0600

    Updated ordering in o2sTranslationTest to work on CentOS 6.7 and enabled GGDM and TDS 4.0 test comparisons

--- test-files/cmd/slow/o2sTranslationTest.sh
+++ test-files/cmd/slow/o2sTranslationTest.sh
@@ -8,6 +8,8 @@ TRANS_MGCP=$HOOT_HOME/translations/MGCP_TRD4.js
 TRANS_GGDM=$HOOT_HOME/translations/GGDMv30.js
 
 inputFile=test-files/o2s_test.osm
+tds40File=test-files/o2s_tds40.osm
+ggdmFile=test-files/o2s_ggdm.osm
 
 outputDir=test-output/o2s
 mkdir -p $outputDir
@@ -25,22 +27,31 @@ HOOT_OPT="--info"
 # Export files
 echo "### TDSv40 ###"
 hoot osm2ogr $HOOT_OPT -D ogr.thematic.structure=false $TRANS_TDS40 $inputFile $outputDir/tds40.shp
-hoot ogr2osm $HOOT_OPT $TRANS_TDS40 $outputDir/tds40.osm  $outputDir/tds40/*.shp
+hoot ogr2osm $HOOT_OPT $TRANS_TDS40 $outputDir/tds40.osm \
+  $outputDir/tds40/HUT_P.shp \
+  $outputDir/tds40/o2s_*.shp \
+  $outputDir/tds40/ORCHARD_S.shp \
+  $outputDir/tds40/RIVER_C.shp
 
 # TDSv40 complains about the "source" tag. It maps to an attibute but it is not on each element
-#hoot is-match $outputDir/tds40.osm $inputFile || diff $outputDir/tds40.osm $inputFile
+hoot is-match $outputDir/tds40.osm $tds40File || diff $outputDir/tds40.osm $tds40File
 
 echo
 echo "### TDSv61 ###"
 hoot osm2ogr $HOOT_OPT -D ogr.thematic.structure=false $TRANS_TDS61 $inputFile $outputDir/tds61.shp
-hoot ogr2osm $HOOT_OPT $TRANS_TDS61 $outputDir/tds61.osm  $outputDir/tds61/*.shp
+hoot ogr2osm $HOOT_OPT $TRANS_TDS61 $outputDir/tds61.osm \
+  $outputDir/tds61/HUT_P.shp \
+  $outputDir/tds61/o2s_*.shp \
+  $outputDir/tds61/ORCHARD_S.shp \
+  $outputDir/tds61/RIVER_C.shp
+
 hoot is-match $outputDir/tds61.osm $inputFile || diff $outputDir/tds61.osm $inputFile
 
 echo
 echo "### MGCP ###"
 hoot osm2ogr $HOOT_OPT $TRANS_MGCP $inputFile $outputDir/mgcp.shp
 # hoot ogr2osm $HOOT_OPT $TRANS_MGCP $outputDir/mgcp.osm  $outputDir/mgcp/*.shp
-hoot ogr2osm $HOOT_OPT $TRANS_MGCP $outputDir/mgcp.osm  \
+hoot ogr2osm $HOOT_OPT $TRANS_MGCP $outputDir/mgcp.osm \
   $outputDir/mgcp/PAL099.shp \
   $outputDir/mgcp/o2s_*.shp \
   $outputDir/mgcp/AEA040.shp \
@@ -51,7 +62,11 @@ hoot is-match $outputDir/mgcp.osm $inputFile || diff $outputDir/mgcp.osm $inputF
 echo
 echo "### GGDM ###"
 hoot osm2ogr $HOOT_OPT -D ogr.thematic.structure=false $TRANS_GGDM $inputFile $outputDir/ggdm.shp
-hoot ogr2osm $HOOT_OPT $TRANS_GGDM $outputDir/ggdm.osm  $outputDir/ggdm/*.shp
+hoot ogr2osm $HOOT_OPT $TRANS_GGDM $outputDir/ggdm.osm \
+  $outputDir/ggdm/HUT_P.shp \
+  $outputDir/ggdm/o2s_*.shp \
+  $outputDir/ggdm/ORCHARD_S.shp \
+  $outputDir/ggdm/RIVER_C.shp
 
 # GGDMv30 complains about the "source" tag. It maps to an attibute but it is not on each element
-#hoot is-match $outputDir/ggdm.osm $inputFile || diff $outputDir/ggdm.osm $inputFile
+hoot is-match $outputDir/ggdm.osm $ggdmFile || diff $outputDir/ggdm.osm $ggdmFile
