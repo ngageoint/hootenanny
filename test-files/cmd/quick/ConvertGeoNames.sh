74c58971ab204224de637c99084606360bd20a02
Ben Marchant <benjamin.marchant@digitalglobe.com>
2017-12-22 15:00:26 -0600

    Update a couple of script tests to ensure directories exist and add all of the test scripts to the hoot-test project to make them easier to access and edit

--- test-files/cmd/quick/ConvertGeoNames.sh
+++ test-files/cmd/quick/ConvertGeoNames.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+set -e
 
 hoot convert -D osm.map.writer.factory.writer=hoot::OsmXmlWriter -D convert.ops=hoot::TranslationOp -D translation.script=$HOOT_HOME/translations/GeoNames.js test-files/cmd/quick/ConvertGeoNames.geonames /dev/stdout
--- test-files/cmd/quick/HelloWorld.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo Hello World.
-echo This is an error. 1>&2
