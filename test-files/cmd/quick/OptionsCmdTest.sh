b94ce8d97e0db7f85ce166d76b6b6a8c1b3f9caa
Brandon Witham <brandon.witham@digitalglobe.com>
2018-01-18 16:50:04 -0500

    fix test file path
    1. refs #1467

--- test-files/cmd/quick/OptionsCmdTest.sh
+++ test-files/cmd/quick/OptionsCmdTest.sh
@@ -4,10 +4,10 @@ set -e
 # use a static config file, so this is not susceptible to config file changes
 CONFIG_FILE=test-files/cmd/quick/OptionsCmdTest/ConfigOptions.asciidoc
 
-hoot options -D $CONFIG_FILE
+hoot options -D config.options.file=$CONFIG_FILE
 
-hoot options -D $CONFIG_FILE --detail
+hoot options -D config.options.file=$CONFIG_FILE --detail
 
-hoot options -D $CONFIG_FILE poi.polygon
+hoot options -D config.options.file=$CONFIG_FILE poi.polygon
 
-hoot options -D $CONFIG_FILE poi.polygon --detail
+hoot options -D config.options.file=$CONFIG_FILE poi.polygon --detail
