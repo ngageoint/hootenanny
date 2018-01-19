f51ad19060c766aee7479b4e2d479ff3ec1088f8
rmrice <rebecca.rice@digitalglobe.com>
2018-01-19 12:19:37 -0500

    add query to remove empty folders and disconnect

--- scripts/database/clean-datasets-node/DeleteOldData.js
+++ scripts/database/clean-datasets-node/DeleteOldData.js
@@ -1,7 +1,6 @@
 var pg = require('pg');
 var moment = require('moment');
-var now = moment();
-// config
+//config
 var pgclient = new pg.Client({
   host: 'localhost',
   database: 'hoot',
@@ -12,10 +11,13 @@ var pgclient = new pg.Client({
 
 // disconnect
 function disconnect() {
-  pgclient.end(function(err) {
-    console.log('client has disconnected');
-  });
-}
+pgclient.end(function(err){
+  console.log('disconnected');
+  if (err) {
+    console.log('error during disconnection', err.stack)
+  }
+});
+};
 
 pgclient.connect(function(err){
   if (err) {
@@ -24,23 +26,22 @@ pgclient.connect(function(err){
     // client has connected to pg
     console.log('connected');
     // get dataset ids, names, and lastAccessed dates from pg table
-    var query = pgclient.query("select id, display_name, (a).value from ( \
+    var getStaleData = pgclient.query("select id, display_name, (a).value from ( \
                                   select *, each(tags) as a \
                                   from maps \
                                 ) t;");
     var rows = [];
-    query.on('row', function(row, res) {
+    getStaleData.on('row', function(row, result) {
       rows.push(row);
     });
-    query.on('end', function(result){
+    getStaleData.on('end', function(result){
       if(rows[0] === undefined){
         // if no results are returned, disconnect
         console.log('no data returned');
-        disconnect();
       } else {
         // create new array with data id , last access date, and whether or not it's stale
         var data = rows.map(function(row){
-          var stale = moment().diff(moment(row.value), 'days') > 60;
+          var stale = moment().diff(moment(row.value), 'seconds') > 1;
           return {
             id: row.id,
             name: row.display_name,
@@ -48,16 +49,36 @@ pgclient.connect(function(err){
             stale: stale
           }
         });
-        console.log(data);
         // delete stale datasets
         data.forEach(function(d){
           if (d.stale) {
             pgclient.query("delete from maps where id=" + d.id);
             console.log("deleted dataset " + d.name);
           }
+        });
+      }
+    });
+
+    // get folders that don't have any datasets in them
+    var getEmptyFolders = pgclient.query("select id, display_name from folders \
+                                          where id not in \
+                                          (select folder_id from folder_map_mappings)");
+    var emptyFolders = [];
+    getEmptyFolders.on('row', function(row, result){
+      emptyFolders.push(row);
+    });
+    getEmptyFolders.on('end', function(result){
+      if(emptyFolders[0] === undefined) {
+        console.log('no empty folders');
+        disconnect();
+      } else {
+        // delete empty folders
+        emptyFolders.forEach(function(f){
+          pgclient.query("delete from folders where id="+f.id);
+          console.log("deleted folder " + f.display_name);
+          disconnect();
         })
       }
-      disconnect();
-    })
+    });
   }
 });
