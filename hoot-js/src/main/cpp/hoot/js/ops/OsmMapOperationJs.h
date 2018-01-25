a74c56fb98dd91aed17bff71364a7d96a6b1cc5c
Ben Marchant <ben_marchant@hotmail.com>
2018-01-25 14:54:21 -0600

    Copyright update

--- hoot-js/src/main/cpp/hoot/js/ops/OsmMapOperationJs.h
+++ hoot-js/src/main/cpp/hoot/js/ops/OsmMapOperationJs.h
@@ -22,7 +22,7 @@
  * This will properly maintain the copyright information. DigitalGlobe
  * copyrights will be updated automatically.
  *
- * @copyright Copyright (C) 2015, 2017 DigitalGlobe (http://www.digitalglobe.com/)
+ * @copyright Copyright (C) 2015, 2017, 2018 DigitalGlobe (http://www.digitalglobe.com/)
  */
 #ifndef OSMMAPOPERATIONJS_H
 #define OSMMAPOPERATIONJS_H
--- hoot-js/src/main/cpp/hoot/js/schema/TagDifferencerJs.h
+++ hoot-js/src/main/cpp/hoot/js/schema/TagDifferencerJs.h
@@ -22,7 +22,7 @@
  * This will properly maintain the copyright information. DigitalGlobe
  * copyrights will be updated automatically.
  *
- * @copyright Copyright (C) 2015, 2017 DigitalGlobe (http://www.digitalglobe.com/)
+ * @copyright Copyright (C) 2015, 2017, 2018 DigitalGlobe (http://www.digitalglobe.com/)
  */
 #ifndef __TAG_DIFFERENCER_JS_H__
 #define __TAG_DIFFERENCER_JS_H__
