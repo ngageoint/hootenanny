a7df068021645a88695700dc8833cc11678df5cd
Ben Marchant <ben_marchant@hotmail.com>
2018-02-15 15:16:03 -0600

    All of the 'hoot-core-test' files that haven't been updated in many moons.

--- hoot-core-test/src/test/cpp/hoot/core/algorithms/MultiLineStringSplitterTest.cpp
+++ hoot-core-test/src/test/cpp/hoot/core/algorithms/MultiLineStringSplitterTest.cpp
@@ -22,7 +22,7 @@
  * This will properly maintain the copyright information. DigitalGlobe
  * copyrights will be updated automatically.
  *
- * @copyright Copyright (C) 2014 DigitalGlobe (http://www.digitalglobe.com/)
+ * @copyright Copyright (C) 2014, 2015, 2016, 2017 DigitalGlobe (http://www.digitalglobe.com/)
  */
 // CPP Unit
 #include <cppunit/extensions/HelperMacros.h>
--- hoot-core-test/src/test/cpp/hoot/core/visitors/SumNumericTagsVisitorTest.cpp
+++ hoot-core-test/src/test/cpp/hoot/core/visitors/SumNumericTagsVisitorTest.cpp
@@ -22,7 +22,7 @@
  * This will properly maintain the copyright information. DigitalGlobe
  * copyrights will be updated automatically.
  *
- * @copyright Copyright (C) 2014 DigitalGlobe (http://www.digitalglobe.com/)
+ * @copyright Copyright (C) 2014, 2017 DigitalGlobe (http://www.digitalglobe.com/)
  */
 
 // hoot
