4037b02593c50af26ff4fa08b61bf8b1afb759c6
Justin Bronn <justin.bronn@radiantsolutions.com>
2018-03-01 14:16:34 -0600

    Use permissions of vagrant:tomcat when deploying the web application files.

--- scripts/tomcat/CopyWebAppsToTomcat.sh
+++ scripts/tomcat/CopyWebAppsToTomcat.sh
@@ -13,25 +13,18 @@ else # Centos
   source ~/.bash_profile
 fi
 
-TOMCAT_USER=tomcat8
-VMUSER=`id -u -n`
-if [ "$VMUSER" = 'vagrant' ]
-then
-  TOMCAT_USER=$VMUSER
-fi
-
-sudo -u $TOMCAT_USER rm -f $TOMCAT8_HOME/webapps/hoot-services.war
+sudo -u vagrant -g tomcat rm -f $TOMCAT8_HOME/webapps/hoot-services.war
 
 # Need to make sure we don't die due to nfs funkyness
-sudo -u $TOMCAT_USER rm -rf $TOMCAT8_HOME/webapps/hoot-services/
-sudo -u $TOMCAT_USER rm -rf $TOMCAT8_HOME/webapps/hoot-services/
+sudo -u vagrant -g tomcat rm -rf $TOMCAT8_HOME/webapps/hoot-services/
+sudo -u vagrant -g tomcat rm -rf $TOMCAT8_HOME/webapps/hoot-services/
 
-sudo -u $TOMCAT_USER cp `ls -t hoot-services/target/hoot-services-*.war | sed -n 1p` $TOMCAT8_HOME/webapps/hoot-services.war
+sudo -u vagrant -g tomcat cp `ls -t hoot-services/target/hoot-services-*.war | sed -n 1p` $TOMCAT8_HOME/webapps/hoot-services.war
 
-sudo -u $TOMCAT_USER rm -rf $TOMCAT8_HOME/webapps/hootenanny-id/
-sudo -u $TOMCAT_USER cp -R hoot-ui/dist $TOMCAT8_HOME/webapps/hootenanny-id
-sudo -u $TOMCAT_USER mkdir -p $TOMCAT8_HOME/webapps/hootenanny-id/data
-sudo -u $TOMCAT_USER cp hoot-ui/data/osm-plus-taginfo.csv $TOMCAT8_HOME/webapps/hootenanny-id/data
-sudo -u $TOMCAT_USER cp hoot-ui/data/tdsv61_field_values.json $TOMCAT8_HOME/webapps/hootenanny-id/data
+sudo -u vagrant -g tomcat rm -rf $TOMCAT8_HOME/webapps/hootenanny-id/
+sudo -u vagrant -g tomcat cp -R hoot-ui/dist $TOMCAT8_HOME/webapps/hootenanny-id
+sudo -u vagrant -g tomcat mkdir -p $TOMCAT8_HOME/webapps/hootenanny-id/data
+sudo -u vagrant -g tomcat cp hoot-ui/data/osm-plus-taginfo.csv $TOMCAT8_HOME/webapps/hootenanny-id/data
+sudo -u vagrant -g tomcat cp hoot-ui/data/tdsv61_field_values.json $TOMCAT8_HOME/webapps/hootenanny-id/data
 
 echo Web application files deployed.
