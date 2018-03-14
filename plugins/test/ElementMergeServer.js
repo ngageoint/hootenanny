27e8b69513b2d25022599685fd89e4ed2be6e353
Brandon Witham <brandon.witham@digitalglobe.com>
2018-03-14 11:13:54 -0400

    add mocha merge tests

--- plugins/test/ElementMergeServer.js
+++ plugins/test/ElementMergeServer.js
@@ -4,53 +4,96 @@ var assert = require('assert'),
 var httpMocks = require('node-mocks-http');
 var server = require('../ElementMergeServer.js');
 
-//For elements with constituents (way nodes for ways, relation members for relations), the service does not require that
-//the constituent elements are passed in.  If the constituent elements are not passed in, the caller is responsible for
-//deleting them manually after merging.
+var poiToPoiInput = 
+    "<?xml version='1.0' encoding='UTF-8'?>\
+     <osm version='0.6' upload='true' generator='hootenanny'>\
+       <node id='-3200079' visible='true' lat='48.0479399' lon='11.7012814'>\
+         <tag k='amenity' v='post_office' />\
+         <tag k='error:circular' v='15' />\
+         <tag k='hoot:status' v='1' />\
+         <tag k='hoot:merge:target' v='yes' />\
+       </node>\
+       <node id='-3200083' visible='true' lat='48.04854' lon='11.70194'>\
+         <tag k='amenity' v='post_office' />\
+         <tag k='error:circular' v='150' />\
+         <tag k='name' v='POST, JÄGER-VON-FALL-STRASSE' />\
+         <tag k='hoot:status' v='2' />\
+       </node>\
+     </osm>";
 
-var poiToPoiInput = "<?xml version='1.0' encoding='UTF-8'?>\
-    <osm version='0.6' upload='true' generator='hootenanny'>\
-    <node id='-3200079' visible='true' lat='48.0479399' lon='11.7012814'>\
-    <tag k='amenity' v='post_office' />\
-    <tag k='error:circular' v='15' />\
-    <tag k='hoot:status' v='1' />\
-    </node>\
-    <node id='-3200083' visible='true' lat='48.04854' lon='11.70194'>\
-    <tag k='amenity' v='post_office' />\
-    <tag k='error:circular' v='150' />\
-    <tag k='name' v='POST, JÄGER-VON-FALL-STRASSE' />\
-    <tag k='hoot:status' v='2' />\
-    </node>\
-    </osm>";
+var poiToPolyInput = 
+    "<?xml version='1.0' encoding='UTF-8'?>\
+     <osm version='0.6' upload='true' generator='hootenanny'>\
+     <node visible='true' id='-530' lat='-0.3635327329999427' lon='42.5442892910000410'>\
+         <tag k='building' v='yes'/>\
+         <tag k='amenity' v='prison'/>\
+         <tag k='note' v='poi1'/>\
+         <tag k='error:circular' v='100'/>\
+     </node>\
+     <way visible='true' id='-56'>\
+         <nd ref='-270'/>\
+         <nd ref='-269'/>\
+         <nd ref='-268'/>\
+         <nd ref='-267'/>\
+         <nd ref='-266'/>\
+         <nd ref='-265'/>\
+         <nd ref='-270'/>\
+         <tag k='building' v='yes'/>\
+         <tag k='amenity' v='prison'/>\
+         <tag k='note' v='building1'/>\
+         <tag k='error:circular' v='15'/>\
+     </way>\
+     </osm>";
 
-//Relations can also be passed in for polys, buildings, and areas, but we're testing that enough in ElementMergerJsTest
+var areaToAreaInput = 
+    "<?xml version='1.0' encoding='UTF-8'?>\
+     <osm version='0.6' generator='JOSM'>\
+     <bounds minlat='37.7081' minlon='-122.5118886' maxlat='37.8106' maxlon='-122.376' origin='hootenanny' />\
+       <relation visible='true' id='-37' timestamp='1970-01-01T00:00:00Z' version='1'>\
+         <member type='way' ref='-1991' role='outer'/>\
+         <member type='way' ref='-1992' role='inner'/>\
+         <tag k='area' v='yes'/>\
+         <tag k='name' v='Mt. Davidson Park'/>\
+         <tag k='type' v='multipolygon'/>\
+         <tag k='hoot:status' v='2'/>\
+         <tag k='error:circular' v='15'/>\
+         <tag k='hoot:merge:target' v='yes'/>\
+       </relation>\
+       <relation visible='true' id='-38' timestamp='1970-01-01T00:00:00Z' version='1'>\
+         <member type='way' ref='-1991' role='outer'/>\
+         <member type='way' ref='-1992' role='inner'/>\
+         <tag k='area' v='yes'/>\
+         <tag k='name' v='Mt. Davidson Park 2'/>\
+         <tag k='type' v='multipolygon'/>\
+         <tag k='hoot:status' v='1'/>\
+         <tag k='error:circular' v='15'/>\
+       </relation>\
+       </osm>";
 
-var poiToPolyInput = "<?xml version='1.0' encoding='UTF-8'?>\
-    <osm version='0.6' upload='true' generator='hootenanny'>\
-    <node visible='true' id='-530' lat='-0.3635327329999427' lon='42.5442892910000410'>\
-        <tag k='building' v='yes'/>\
-        <tag k='amenity' v='prison'/>\
-        <tag k='note' v='poi1'/>\
-        <tag k='error:circular' v='100'/>\
-    </node>\
-    <way visible='true' id='-56'>\
-        <nd ref='-270'/>\
-        <nd ref='-269'/>\
-        <nd ref='-268'/>\
-        <nd ref='-267'/>\
-        <nd ref='-266'/>\
-        <nd ref='-265'/>\
-        <nd ref='-270'/>\
-        <tag k='building' v='yes'/>\
-        <tag k='amenity' v='prison'/>\
-        <tag k='note' v='building1'/>\
-        <tag k='error:circular' v='15'/>\
-    </way>\
-</osm>";
-
-//TODO: finish
-var areaToAreaInput = "";
-var buildingToBuildingInput = "";
+var buildingToBuildingInput = 
+    "<?xml version='1.0' encoding='UTF-8'?>\
+     <osm version='0.6' generator='JOSM'>\
+       <way id='-222330' action='modify' visible='true'>\
+         <nd ref='-222258' />\
+         <nd ref='-222256' />\
+         <nd ref='-222254' />\
+         <nd ref='-222252' />\
+         <nd ref='-222264' />\
+         <nd ref='-222258' />\
+         <tag k='building' v='yes' />\
+         <tag k='name' v='building 1' />\
+         <tag k='hoot:merge:target' v='yes'/>\
+       </way>\
+       <way id='-222332' action='modify' visible='true'>\
+         <nd ref='-222264' />\
+         <nd ref='-222262' />\
+         <nd ref='-222260' />\
+         <nd ref='-222258' />\
+         <nd ref='-222264' />\
+         <tag k='building' v='yes' />\
+         <tag k='name' v='building 2' />\
+       </way>\
+     </osm>";
 
 describe('ElementMergeServer', function () {
     it('responds with HTTP 404 if url not found', function() {
@@ -72,8 +115,11 @@ describe('ElementMergeServer', function () {
         assert.equal(response.statusCode, '400');
     });
 
-    it('merges two osm poi', function() {
+    //Testing good inputs here only.  Bad inputs are thoroughly tested in the core.  Not checking 
+    //nodes in the polys here, but is ok since we're testing that in the core.
+    //TODO: need to check output element count
 
+    it('merges two pois', function() {
         var request  = httpMocks.createRequest({
             method: 'POST',
             url: '/elementmerge',
@@ -92,44 +138,32 @@ describe('ElementMergeServer', function () {
                 assert.equal(result.osm.node[0].$.lat, "48.0479399000000029");
                 assert.equal(result.osm.node[0].$.lon, "11.7012813999999992");
                 assert.equal(result.osm.node[0].tag[0].$.k, "hoot:status");
-                assert.equal(result.osm.node[0].tag[0].$.v, "1");
-                assert.equal(result.osm.node[0].tag[1].$.k, "name");
-                assert.equal(result.osm.node[0].tag[1].$.v, "POST, JÄGER-VON-FALL-STRASSE");
-                assert.equal(result.osm.node[0].tag[2].$.k, "amenity");
-                assert.equal(result.osm.node[0].tag[2].$.v, "post_office");
-            });
-        });
-    });
-
-    describe('handleInputs', function() {
-        it('merges two osm poi', function() {
-            var merged = server.handleInputs({
-                path: '/elementmerge',
-                method: 'POST',
-                osm: poiToPoiInput
-            });
-
-            xml2js.parseString(merged, function(err, result) {
-                if (err) console.error(err);
-                assert.equal(result.osm.node[0].$.lat, "48.0479399000000029");
-                assert.equal(result.osm.node[0].$.lon, "11.7012813999999992");
-                assert.equal(result.osm.node[0].tag[0].$.k, "hoot:status");
                 assert.equal(result.osm.node[0].tag[0].$.v, "3");
                 assert.equal(result.osm.node[0].tag[1].$.k, "name");
                 assert.equal(result.osm.node[0].tag[1].$.v, "POST, JÄGER-VON-FALL-STRASSE");
                 assert.equal(result.osm.node[0].tag[2].$.k, "amenity");
                 assert.equal(result.osm.node[0].tag[2].$.v, "post_office");
+                assert.equal(result.osm.node[0].tag[3].$.k, "error:circular");
+                assert.equal(result.osm.node[0].tag[3].$.v, "15");
             });
         });
+    });
 
-        it('merges an osm poi and a polygon', function() {
-            var merged = server.handleInputs({
-                path: '/elementmerge',
-                method: 'POST',
-                osm: poiToPolyWayInput
-            });
-
-            xml2js.parseString(merged, function(err, result) {
+    it('merges a poi and a polygon', function() {
+        var request  = httpMocks.createRequest({
+            method: 'POST',
+            url: '/elementmerge',
+            body: poiToPolyInput
+        });
+        var response = httpMocks.createResponse();
+        server.ElementMergeserver(request, response);
+        assert.equal(response.statusCode, '200');
+        var body = '';
+        response.on('data', function(chunk){
+            body += chunk;
+        })
+        response.on('end', function(body){
+            xml2js.parseString(body, function(err, result) {
                 if (err) console.error(err);
                 assert.equal(result.osm.way[0].tag[0].$.k, "note");
                 assert.equal(result.osm.way[0].tag[0].$.v, "poi1;building1");
@@ -138,12 +172,77 @@ describe('ElementMergeServer', function () {
                 assert.equal(result.osm.way[0].tag[2].$.k, "hoot:status");
                 assert.equal(result.osm.way[0].tag[2].$.v, "3");
                 assert.equal(result.osm.way[0].tag[3].$.k, "building");
-                assert.equal(result.osm.way[0].tag[3].$.v, "yes");
+               assert.equal(result.osm.way[0].tag[3].$.v, "yes");
                 assert.equal(result.osm.way[0].tag[4].$.k, "amenity");
                 assert.equal(result.osm.way[0].tag[4].$.v, "prison");
+                assert.equal(result.osm.way[0].tag[5].$.k, "error:circular");
+                assert.equal(result.osm.way[0].tag[5].$.v, "15");
             });
         });
+    });
 
+    it('merges two areas', function() {
+        var request  = httpMocks.createRequest({
+            method: 'POST',
+            url: '/elementmerge',
+            body: areaToAreaInput
+        });
+        var response = httpMocks.createResponse();
+        server.ElementMergeserver(request, response);
+        assert.equal(response.statusCode, '200');
+        var body = '';
+        response.on('data', function(chunk){
+            body += chunk;
+        })
+        response.on('end', function(body){
+            xml2js.parseString(body, function(err, result) {
+                if (err) console.error(err);
+                assert.equal(result.osm.way[0].tag[0].$.k, "hoot:status");
+                assert.equal(result.osm.way[0].tag[0].$.v, "3");
+                assert.equal(result.osm.way[0].tag[1].$.k, "leisure");
+                assert.equal(result.osm.way[0].tag[1].$.v, "pitch");
+                assert.equal(result.osm.way[0].tag[2].$.k, "alt_name");
+                assert.equal(result.osm.way[0].tag[2].$.v, "Margaret S.Hayward Playground");
+                assert.equal(result.osm.way[0].tag[3].$.k, "name");
+                assert.equal(result.osm.way[0].tag[3].$.v, "baseball field");
+                assert.equal(result.osm.way[0].tag[4].$.k, "area");
+                assert.equal(result.osm.way[0].tag[4].$.v, "yes");
+                assert.equal(result.osm.way[0].tag[5].$.k, "sport");
+                assert.equal(result.osm.way[0].tag[5].$.v, "baseball");
+            });
+        });
+    });
+
+    it('merges two buildings', function() {
+        var request  = httpMocks.createRequest({
+            method: 'POST',
+            url: '/elementmerge',
+            body: buildingToBuildingInput
+        });
+        var response = httpMocks.createResponse();
+        server.ElementMergeserver(request, response);
+        assert.equal(response.statusCode, '200');
+        var body = '';
+        response.on('data', function(chunk){
+            body += chunk;
+        })
+        response.on('end', function(body){
+            xml2js.parseString(body, function(err, result) {
+                if (err) console.error(err);
+                assert.equal(result.osm.way[0].tag[0].$.k, "hoot:status");
+                assert.equal(result.osm.way[0].tag[0].$.v, "3");
+                assert.equal(result.osm.way[0].tag[1].$.k, "alt_name");
+                assert.equal(result.osm.way[0].tag[1].$.v, "building 2");
+                assert.equal(result.osm.way[0].tag[2].$.k, "building");
+                assert.equal(result.osm.way[0].tag[2].$.v, "yes");
+                assert.equal(result.osm.way[0].tag[3].$.k, "name");
+                assert.equal(result.osm.way[0].tag[3].$.v, "building 1");
+            });
+        });
+    });
+
+    describe('handleInputs', function() {
+ 
         it('throws error if url not found', function() {
             assert.throws(function error() {
                 server.handleInputs({
