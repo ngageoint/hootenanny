7f4447c3b6fa70d04e418b43a33dd91d9041a9a7
mschicker <micah.schicker@radiantsolutions.com>
2018-03-14 22:50:35 -0600

    Add BuildingOnlyOp, and supporting ReplaceTagsVisitor

---
+++ hoot-core/src/main/cpp/hoot/core/ops/BuildingOnlyOp.cpp
@@ -0,0 +1,92 @@
+/*
+ * This file is part of Hootenanny.
+ *
+ * Hootenanny is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * --------------------------------------------------------------------
+ *
+ * The following copyright notices are generated automatically. If you
+ * have a new notice to add, please use the format:
+ * " * @copyright Copyright ..."
+ * This will properly maintain the copyright information. DigitalGlobe
+ * copyrights will be updated automatically.
+ *
+ * @copyright Copyright (C) 2015, 2017 DigitalGlobe (http://www.digitalglobe.com/)
+ */
+#include "BuildingOnlyOp.h"
+
+// geos
+#include <geos/geom/Geometry.h>
+#include <geos/geom/GeometryFactory.h>
+#include <geos/geom/Polygon.h>
+
+// hoot
+#include <hoot/core/util/Factory.h>
+#include <hoot/core/index/OsmMapIndex.h>
+#include <hoot/core/conflate/NodeToWayMap.h>
+#include <hoot/core/ops/RecursiveElementRemover.h>
+#include <hoot/core/ops/RemoveRelationOp.h>
+#include <hoot/core/visitors/RemoveTagVisitor.h>
+#include <hoot/core/visitors/ReplaceTagVisitor.h>
+#include <hoot/core/schema/OsmSchema.h>
+#include <hoot/core/util/GeometryConverter.h>
+#include <hoot/core/OsmMap.h>
+
+using namespace std;
+
+namespace hoot
+{
+
+HOOT_FACTORY_REGISTER(OsmMapOperation, BuildingOnlyOp)
+
+BuildingOnlyOp::BuildingOnlyOp()
+{
+}
+
+void BuildingOnlyOp::apply(boost::shared_ptr<OsmMap>& map)
+{
+  _map = map;
+
+  // Remove superfluous tags
+  RemoveTagVisitor removeVisitor("error:circular");
+  removeVisitor.addKey("OBJECTID");
+  removeVisitor.addKey("PAGENUMBER");
+  removeVisitor.addKey("SHAPE_AREA");
+  removeVisitor.addKey("SHAPE_LENG");
+  removeVisitor.addKey("hoot:layername");
+  map->visitRw(removeVisitor);
+
+  // Change uppercase "BUILDING" tag to lower
+  ReplaceTagVisitor replaceVisitor("BUILDING", "yes",
+                                   "building", "yes");
+  map->visitRw(replaceVisitor);
+
+  // Go through all the relations, find which ones to remove
+  std::vector<long> rIdsToRemove;
+  const RelationMap& relations = map->getRelations();
+  for (RelationMap::const_iterator it = relations.begin(); it != relations.end(); ++it)
+  {
+    const boost::shared_ptr<Relation>& r = it->second;
+
+    // Is it a building relation?
+    if (r->getType() == MetadataTags::RelationBuilding())
+      rIdsToRemove.push_back(r->getId());
+  }
+
+  // Now do the removing
+  for (size_t i = 0; i < rIdsToRemove.size(); i++)
+    RemoveRelationOp::removeRelation(map, rIdsToRemove[i]);
+}
+
+} // namespace hoot
---
+++ hoot-core/src/main/cpp/hoot/core/visitors/ReplaceTagVisitor.h
@@ -0,0 +1,106 @@
+/*
+ * This file is part of Hootenanny.
+ *
+ * Hootenanny is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * --------------------------------------------------------------------
+ *
+ * The following copyright notices are generated automatically. If you
+ * have a new notice to add, please use the format:
+ * " * @copyright Copyright ..."
+ * This will properly maintain the copyright information. DigitalGlobe
+ * copyrights will be updated automatically.
+ *
+ * @copyright Copyright (C) 2015, 2017 DigitalGlobe (http://www.digitalglobe.com/)
+ */
+#ifndef REPLACETAGVISITOR_H
+#define REPLACETAGVISITOR_H
+
+// hoot
+#include <hoot/core/visitors/ElementOsmMapVisitor.h>
+
+namespace hoot
+{
+
+/**
+ *
+ */
+
+/**
+ * @brief The ReplaceTagVisitor class replaces matching tags with the
+ * specified replacement tag.
+ *
+ * This was written for the building outline project: the input data had tag
+ * BUILDING=yes, but the output data needed to have tag building=yes
+ *
+ * Feel free to extend this class. Maybe add a flag for case-insensitive
+ * matching. Or support for wildcards.
+ */
+class ReplaceTagVisitor : public ElementOsmMapVisitor
+{
+public:
+
+  /**
+   * @brief className - get class name string
+   * @return The namespace qualified class name
+   */
+  static std::string className() { return "hoot::ReplaceTagVisitor"; }
+
+  /**
+   * @brief ReplaceTagVisitor - default constructor
+   */
+  ReplaceTagVisitor();
+
+  /**
+   * @brief ReplaceTagVisitor - This constructor lets you specify the tag
+   * (k=v) to match, and the tag (k=v) to replace it with.
+   * @param matchKey - key to match
+   * @param matchValue - value to match
+   * @param replaceKey - key to use as a replacement
+   * @param replaceValue - value to use as a replacement
+   */
+  ReplaceTagVisitor(QString matchKey, QString matchValue,
+                    QString replaceKey, QString replaceValue);
+
+  /**
+   * @brief setMatchTag - Set the tag to match
+   * @param k - tag key
+   * @param v - tag value
+   */
+  void setMatchTag(QString k, QString v);
+
+  /**
+   * @brief setReplaceTage -Set the tag to use as a replacement
+   * @param k - tag key
+   * @param v - tag value
+   */
+  void setReplaceTage(QString k, QString v);
+
+  /**
+   * @brief visit - visit an element & perform check and replace
+   * @param e - element to visit
+   */
+  virtual void visit(const boost::shared_ptr<Element>& e);
+
+private:
+
+  QString _matchKey;
+  QString _matchValue;
+  QString _replaceKey;
+  QString _replaceValue;
+};
+
+}
+
+#endif // REPLACETAGVISITOR_H
