098ee668aae50b39c098946b6d159cf575099389
Brandon Witham <brandon.witham@digitalglobe.com>
2018-03-22 17:28:48 +0000

    fix test

--- test-files/cmd/quick/AttributeCoOccurrenceCmdTest.sh
+++ test-files/cmd/quick/AttributeCoOccurrenceCmdTest.sh
@@ -5,5 +5,5 @@ set -e
 # for correctitude
 inputfile1=$HOOT_HOME/test-files/cmd/quick/AttributeCoOccurrenceTest/input.osm
 inputfile2=$HOOT_HOME/test-files/cmd/quick/AttributeCoOccurrenceTest/input.osm
-hoot attr-co-occurence $inputfile1 $inputfile2
+hoot attribute-co-occurrence $inputfile1 $inputfile2
 
