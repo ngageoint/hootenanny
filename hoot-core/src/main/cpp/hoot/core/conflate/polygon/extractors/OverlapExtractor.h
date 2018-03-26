d20ad29d8029794764ebc210d83701bd96e5166b
Brandon Witham <brandon.witham@digitalglobe.com>
2018-03-26 23:13:33 +0000

    copyright header updates

--- hoot-core/src/main/cpp/hoot/core/conflate/polygon/extractors/OverlapExtractor.h
+++ hoot-core/src/main/cpp/hoot/core/conflate/polygon/extractors/OverlapExtractor.h
@@ -23,7 +23,7 @@
  * copyrights will be updated automatically.
  *
  * @copyright Copyright (C) 2005 VividSolutions (http://www.vividsolutions.com/)
- * @copyright Copyright (C) 2015, 2017 DigitalGlobe (http://www.digitalglobe.com/)
+ * @copyright Copyright (C) 2015, 2017, 2018 DigitalGlobe (http://www.digitalglobe.com/)
  */
 #ifndef OVERLAPEXTRACTOR_H
 #define OVERLAPEXTRACTOR_H
--- hoot-core/src/main/cpp/hoot/core/io/OsmGeoJsonWriter.h
+++ hoot-core/src/main/cpp/hoot/core/io/OsmGeoJsonWriter.h
@@ -22,7 +22,7 @@
  * This will properly maintain the copyright information. DigitalGlobe
  * copyrights will be updated automatically.
  *
- * @copyright Copyright (C) 2017 DigitalGlobe (http://www.digitalglobe.com/)
+ * @copyright Copyright (C) 2017, 2018 DigitalGlobe (http://www.digitalglobe.com/)
  */
 #ifndef OSM_GEOJSON_WRITER_H
 #define OSM_GEOJSON_WRITER_H
