642f7062844cd9121445a36103002bf9e6c2e9ad
Brandon Witham <bwitham@users.noreply.github.com>
2018-03-27 23:07:33 -0400

    Cleanup on the newly added commands (#2275)
    * formats
    * operators

--- test-files/cmd/slow/InlineOperatorsCmdTest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Since the operators will constantly be changing, just going to grep out some that are unlikely to change any time soon.
-
-hoot inline-operators | grep "Map Cleaner"
-hoot inline-operators | grep "Implicit POI Tagger"
-hoot inline-operators | grep "Remove Empty Areas"
-hoot inline-operators | grep "Non-Building Area"
