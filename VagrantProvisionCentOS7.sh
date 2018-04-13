80d01ee98543901365ca5a1d5aaf31c0b84eb312
Ben Marchant <ben_marchant@hotmail.com>
2018-04-13 11:16:25 -0500

    Added c++11 to the standard compile config (#2306)
    * Move all changes from 1500 to 1500_1.
    
    * Must change directory to the hoot directory inorder to locate the Gemfile

--- VagrantProvisionCentOS7.sh
+++ VagrantProvisionCentOS7.sh
@@ -150,12 +150,7 @@ if ! hash qmake >/dev/null 2>&1 ; then
     fi
 fi
 
-#####
-# Temp change until we get the C++11 support into develop
 cd $HOOT_HOME
-cp LocalConfig.pri.orig LocalConfig.pri
-echo "QMAKE_CXXFLAGS += -std=c++11" >> LocalConfig.pri
-#####
 
 echo "### Configuring environment..."
 
