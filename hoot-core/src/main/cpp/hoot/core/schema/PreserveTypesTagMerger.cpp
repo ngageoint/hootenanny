6852b761a567b112b0609abf10726561e0dbc33f
Brandon Witham <brandon.witham@radiantsolutions.com>
2018-04-24 18:28:00 -0400

    fix missed merge conflict

--- hoot-core/src/main/cpp/hoot/core/schema/PreserveTypesTagMerger.cpp
+++ hoot-core/src/main/cpp/hoot/core/schema/PreserveTypesTagMerger.cpp
@@ -84,12 +84,8 @@ Tags PreserveTypesTagMerger::mergeTags(const Tags& t1, const Tags& t2, ElementTy
       {
         //TODO: This is actually going to bring in any tags into alt_types and not just schema type
         //tags.  Probably need to either rename alt_types or have a more strict check on which tags
-<<<<<<< HEAD
         //can be merged into it.  Something similar to PoiPolygonTypeScoreExtractor::hasType but
         //looser could be used...or pass in a type def function to this class.
-=======
-        //can be merged into it.
->>>>>>> develop
 
         //arbitrarily use first one and add second to an alt_types field
         LOG_TRACE(
