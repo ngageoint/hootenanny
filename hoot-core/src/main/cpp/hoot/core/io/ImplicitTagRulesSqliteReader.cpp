65bc974780a574d411aebc3528c52c07b38bcc47
Ben Marchant <ben_marchant@hotmail.com>
2018-05-10 12:06:24 -0500

    Fix up some lingering copyright issues (#2360)
    * Found quite a few copyright problems in the code.  Also deleted PertyComparator.* because they were empy files.
    
    * Update copyright missmatch.

--- hoot-core/src/main/cpp/hoot/core/io/ImplicitTagRulesSqliteReader.cpp
+++ hoot-core/src/main/cpp/hoot/core/io/ImplicitTagRulesSqliteReader.cpp
@@ -22,8 +22,7 @@
  * This will properly maintain the copyright information. DigitalGlobe
  * copyrights will be updated automatically.
  *
- * @copyright Copyright (C) 2017 DigitalGlobe (http://www.digitalglobe.com/)
- * @copyright Copyright (C) 2015, 2018 DigitalGlobe (http://www.digitalglobe.com/)
+ * @copyright Copyright (C) 2015, 2017, 2018 DigitalGlobe (http://www.digitalglobe.com/)
  */
 
 #include "ImplicitTagRulesSqliteReader.h"
