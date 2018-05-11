9c235370ade9a66a19dce3377ac875569d7a5174
Seth Putnam <seth.putnam@outlook.com>
2018-05-11 13:49:42 -0600

    Add initial support for sonar scanning hoot C++ code (#2366)
    * Add sonar install and Jenkinsfile for sonar scanning of C++ code
    
    * Put build and scan steps into script file
    
    * Fix permissions of script file
    
    * Update scripts to support parameter
    
    * Exclude autogenerated protocall buffer files
    
    * Exclude protocall buffer header files
    
    * Exclude sql files

---
+++ scripts/sonar/sonar-build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+# Configure the make
+aclocal && autoconf && autoheader && automake --add-missing --copy
+./configure --quiet --with-rnd --with-services --with-uitests
+
+# Make with the sonar build watcher
+build-wrapper-linux-x86-64 --out-dir bw-output make -sj$(nproc)
+
+# Perform scan and upload to sonar scanner
+sonar-scanner \
+    -Dsonar.projectKey=hoot \
+    -Dsonar.sources=./hoot-cmd,./hoot-core,./hoot-core-test,./hoot-js,./hoot-rnd,./hoot-test,./tbs,./tgs \
+    -Dsonar.cfamily.build-wrapper-output=bw-output \
+    -Dsonar.host.url=https://sonarcloud.io \
+    -Dsonar.organization=hootenanny \
+    -Dsonar.login=$1 \
+    -Dsonar.cfamily.threads=4 \
+    -Dsonar.exclusions=**/*.pb.cc,**/*.pb.h,**/*.sql
---
+++ scripts/sonar/sonar-install.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+set -e
+
+# Main sonar scanner
+SONAR_VERSION=3.1.0.1141
+SONAR_CLI=sonar-scanner-cli-$SONAR_VERSION-linux
+SONAR_PKG=sonar-scanner-$SONAR_VERSION-linux
+SONAR_ZIP=$SONAR_CLI.zip
+SONAR_URL=https://sonarsource.bintray.com/Distribution/sonar-scanner-cli
+SONAR_PATH=/opt/sonar
+
+# Sonar build wrapper (required to scan C/C++)
+SONAR_BLD_PKG=build-wrapper-linux-x86
+SONAR_BLD_ZIP=$SONAR_BLD_PKG.zip
+SONAR_BLD_URL=https://sonarcloud.io/static/cpp
+
+# Download sonar scanner
+if [ ! -f $SONAR_ZIP ]; then
+    echo "### Downloading Sonar Scanner..."
+    wget --quiet $SONAR_URL/$SONAR_ZIP
+fi
+
+# Install sonar scanner
+if [ ! -f $SONAR_PATH ]; then
+    echo "### Installing Sonar Scanner..."
+    if [ ! -f $SONAR_PKG ]; then
+        unzip -qq -o $SONAR_ZIP
+    fi
+    sudo mv $SONAR_PKG $SONAR_PATH
+fi
+echo "export PATH=\$PATH:$SONAR_PATH/bin" >> ~/.bash_profile
+source ~/.bash_profile
+
+# Download sonar build wrapper
+if [ ! -f $SONAR_BLD_ZIP ]; then
+    echo "### Downloading Sonar Build Wrapper..."
+    wget --quiet $SONAR_BLD_URL/$SONAR_BLD_ZIP
+fi
+
+#Install sonar build wrapper
+if [ ! -f $SONAR_PATH/bin/$SONAR_BLD_PKG-64 ]; then
+    echo "### Installing Sonar Build Wrapper..."
+    if [ ! -f $SONAR_BLD_PKG ]; then
+        unzip -qq -o $SONAR_BLD_ZIP
+    fi
+    sudo mv $SONAR_BLD_PKG/$SONAR_BLD_PKG-64 $SONAR_PATH/bin/
+    sudo mv $SONAR_BLD_PKG/libinterceptor-x86_64.so $SONAR_PATH/bin/
+    sudo mv $SONAR_BLD_PKG/libinterceptor-i686.so $SONAR_PATH/bin/
+    rm -rf $SONAR_BLD_PKG
+fi
