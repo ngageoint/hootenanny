a42a0b6b9925ef44eb9b443d5619f275021bd30e
sputnam <seth.putnam@digitalglobe.com>
2018-05-11 10:14:21 -0600

    Add core coverage

--- scripts/sonar/sonar-build.sh
+++ scripts/sonar/sonar-build.sh
@@ -6,7 +6,7 @@ aclocal && autoconf && autoheader && automake --add-missing --copy
 ./configure --quiet --with-rnd --with-services --with-uitests
 
 # Make with the sonar build watcher
-build-wrapper-linux-x86-64 --out-dir bw-output make -sj$(nproc)
+build-wrapper-linux-x86-64 --out-dir bw-output make -sj$(nproc) core-coverage
 
 # Perform scan and upload to sonar scanner
 sonar-scanner \
