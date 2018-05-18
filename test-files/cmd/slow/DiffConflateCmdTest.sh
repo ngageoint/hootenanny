dcb440ff047a37e01402e66ac9cc6b775e6404a4
mschicker <micah.schicker@radiantsolutions.com>
2018-05-18 15:25:32 -0600

    Add changeset output to Differential Conflation And a unit test

--- test-files/cmd/slow/DiffConflateCmdTest.sh
+++ test-files/cmd/slow/DiffConflateCmdTest.sh
@@ -3,9 +3,20 @@ set -e
 
 mkdir -p $HOOT_HOME/tmp/
 mkdir -p test-output/cmd/DiffConflateCmdTest
-hoot conflate-differential -C Diff.conf test-files/conflate/diff/input1.osm test-files/conflate/diff/input2.osm test-output/cmd/DiffConflateCmdTest/output.osm
 
+# Check differential conflation
+hoot conflate-differential -C Diff.conf test-files/conflate/diff/input1.osm test-files/conflate/diff/input2.osm test-output/cmd/DiffConflateCmdTest/output.osm
 hoot map-diff test-output/cmd/DiffConflateCmdTest/output.osm test-files/cmd/slow/DiffConflateCmdTest/output.osm || diff test-output/cmd/DiffConflateCmdTest/output.osm test-files/cmd/slow/DiffConflateCmdTest/output.osm
+
+# Check changeset derivation
+hoot conflate-differential -C Diff.conf test-files/conflate/diff/input1.osm test-files/conflate/diff/input2.osm test-output/cmd/DiffConflateCmdTest/output.osc
+
+if diff test-output/cmd/DiffConflateCmdTest/output.osc test-files/cmd/slow/DiffConflateCmdTest/output.osc >/dev/null ; then
+  echo "Changeset Files Match"
+else
+  echo "Changeset Files Don't Match"
+  diff test-output/cmd/DiffConflateCmdTest/output.osc test-files/cmd/slow/DiffConflateCmdTest/output.osc
+fi
 
 # Check to make sure we don't bomb out on empty files
 hoot conflate --warn test-files/Empty.osm test-files/Empty.osm tmp/dum.osm
