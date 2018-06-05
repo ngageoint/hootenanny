7c3f88748fdfae6278c90ec0f3c58a400ef48403
sputnam <seth.putnam@digitalglobe.com>
2018-06-05 16:47:11 -0600

    Better handling of coverage paths, only run core tests

--- scripts/cover/gcovHoot.sh
+++ scripts/cover/gcovHoot.sh
@@ -10,27 +10,24 @@ fi
 cd $HOOT_HOME
 
 # hoot-core coverage
-mkdir gcov-hoot-core
-cd gcov-hoot-core
-gcov ../hoot-core/tmp/debug/*.gcda
+cd hoot-core
+gcov tmp/debug/*.gcda
 # fix path
 sed -i 's/src/hoot-core\/src/g' *.gcov
 mv *.gcov ../
 cd ..
 
 #tbs coverage
-mkdir gcov-tbs
-cd gcov-tbs
-gcov ../tbs/tmp/debug/*.gcda
+cd tbs
+gcov tmp/debug/*.gcda
 # fix path
 sed -i 's/src/tbs\/src/g' *.gcov
 mv *.gcov ../
 cd ..
 
 # hoot-rnd coverage
-mkdir gcov-hoot-rnd
-cd gcov-hoot-rnd
-gcov ../hoot-rnd/tmp/debug/*.gcda
+cd hoot-rnd
+gcov tmp/debug/*.gcda
 # fix path
 sed -i 's/src/hoot-rnd\/src/g' *.gcov
 mv *.gcov ../
