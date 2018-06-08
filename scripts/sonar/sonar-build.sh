75b63753ea2e114be0024ab22694ae6e3b21e108
Justin Bronn <justin.bronn@radiantsolutions.com>
2018-06-08 10:23:52 -0500

    * Remove UI tests from sonar build configuration. * Minor shell style fixes and quoting.

--- scripts/sonar/sonar-build.sh
+++ scripts/sonar/sonar-build.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
-set -e
+#!/bin/bash
+set -euo pipefail
 
-# Configure the make
+# Setup hoot's autoconf files.
 aclocal && autoconf && autoheader && automake --add-missing --copy
-./configure --quiet --with-rnd --with-services --with-uitests
+./configure --quiet --with-rnd --with-services
 
 # Make with the sonar build watcher
-build-wrapper-linux-x86-64 --out-dir bw-output make -sj$(nproc)
+build-wrapper-linux-x86-64 --out-dir bw-output make -sj"$(nproc)"
