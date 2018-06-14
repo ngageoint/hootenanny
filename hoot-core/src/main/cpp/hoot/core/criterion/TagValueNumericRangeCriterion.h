68a9e01e6af9605d6767dd692947156afa4e7ca9
Brandon Witham <brandon.witham@radiantsolutions.com>
2018-06-14 17:47:32 -0400

    add TagValueNumericRangeCriterion

---
+++ hoot-core/src/main/cpp/hoot/core/criterion/TagValueNumericRangeCriterion.h
@@ -0,0 +1,71 @@
+/*
+ * This file is part of Hootenanny.
+ *
+ * Hootenanny is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * --------------------------------------------------------------------
+ *
+ * The following copyright notices are generated automatically. If you
+ * have a new notice to add, please use the format:
+ * " * @copyright Copyright ..."
+ * This will properly maintain the copyright information. DigitalGlobe
+ * copyrights will be updated automatically.
+ *
+ * @copyright Copyright (C) 2016, 2017, 2018 DigitalGlobe (http://www.digitalglobe.com/)
+ */
+#ifndef TAGVALUENUMERICRANGECRITERION_H
+#define TAGVALUENUMERICRANGECRITERION_H
+
+// hoot
+#include <hoot/core/util/Configurable.h>
+#include "ElementCriterion.h"
+
+namespace hoot
+{
+
+class Element;
+
+/**
+ * Finds numeric tag values falling within a range
+ */
+class TagValueNumericRangeCriterion : public ElementCriterion, public Configurable
+{
+public:
+
+  static std::string className() { return "hoot::TagValueNumericRangeCriterion"; }
+
+  TagValueNumericRangeCriterion();
+  TagValueNumericRangeCriterion(const QStringList tagKeys, const long rangeMin,
+                                const long rangeMax);
+
+  bool isSatisfied(const boost::shared_ptr<const Element> &e) const;
+
+  virtual ElementCriterionPtr clone()
+  { return ElementCriterionPtr(new TagValueNumericRangeCriterion()); }
+
+  void setConfiguration(const Settings& conf);
+
+  virtual QString getDescription() const
+  { return "Identifies numeric tag values falling within a range"; }
+
+private:
+
+  QStringList _tagKeys;
+  long _rangeMin;
+  long _rangeMax;
+};
+
+}
+
+#endif // TAGVALUENUMERICRANGECRITERION_H
