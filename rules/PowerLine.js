42d1a3e6a326a07b566877c7f6faf5ed8e9862d4
Brandon Witham <brandon.witham@radiantsolutions.com>
2018-06-28 10:26:18 -0400

    up way max angle

--- rules/PowerLine.js
+++ rules/PowerLine.js
@@ -14,7 +14,7 @@ exports.reviewThreshold = parseFloat(hoot.get("conflate.review.threshold"));
 
 var sublineMatcher =
   new hoot.MaximalSublineStringMatcher(
-    { "way.matcher.max.angle": hoot.get("way.matcher.max.angle"),
+    { "way.matcher.max.angle": hoot.get("power.line.matcher.max.angle"),
       "way.subline.matcher": hoot.get("power.line.subline.matcher") });
 
 var distanceWeightCoeff = parseFloat(hoot.get("power.line.distance.weight.coefficient")) * -1.0;
@@ -149,7 +149,7 @@ exports.matchScore = function(map, e1, e2)
         weightedShapeDistanceExtractor7Val < 0.49*/)
     {
       //So far, voltage and location (underground vs overhead) seem to be the most consistent tags to disambiguate matches.  We'll 
-      //review when features match and they disagree, and we'll ignore them if the values for them are unpopulated.
+      //review when features match and those tags disagree, and we'll ignore them if the values for either of them are unpopulated.
 
       var voltageStr1 = String(e1.getTags().get("voltage")).trim();
       var voltageStr2 = String(e2.getTags().get("voltage")).trim();
