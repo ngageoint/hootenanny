a01d3a961a21cbe00db370ea7ad5c634e1e89a81
Brandon Witham <brandon.witham@radiantsolutions.com>
2018-07-19 15:10:41 -0400

    cleanup

--- rules/PowerLine.js
+++ rules/PowerLine.js
@@ -14,30 +14,19 @@ var sublineMatcher =
     { "way.matcher.max.angle": hoot.get("power.line.matcher.max.angle"),
       "way.subline.matcher": hoot.get("power.line.subline.matcher") });
 
-var sublineMatcher2 =
-  new hoot.MaximalSublineStringMatcher(
-    { "way.matcher.max.angle": 60,
-      "way.subline.matcher": "hoot::MaximalNearestSublineMatcher" });
+var centroidDistanceExtractor = new hoot.CentroidDistanceExtractor();
+var edgeDistanceExtractor1 = new hoot.EdgeDistanceExtractor(new hoot.MeanAggregator());
+var weightedMetricDistanceExtractor1 = new hoot.WeightedMetricDistanceExtractor(new hoot.MeanAggregator());
+var weightedShapeDistanceExtractor7 = new hoot.WeightedShapeDistanceExtractor(new hoot.SigmaAggregator());
 
+//for distance weighting
 var distanceWeightCoeff = parseFloat(hoot.get("power.line.matcher.distance.weight.coefficient")) * -1.0;
 var distanceScoreExtractor = new hoot.DistanceScoreExtractor();
 
-var distanceScoreExtractor7 = new hoot.DistanceScoreExtractor(new hoot.SigmaAggregator());
-
-var angleHistogramExtractor5 = new hoot.AngleHistogramExtractor({"angle.histogram.extractor.smoothing": 1.57});
-
-var centroidDistanceExtractor = new hoot.CentroidDistanceExtractor();
-
-var edgeDistanceExtractor1 = new hoot.EdgeDistanceExtractor(new hoot.MeanAggregator());
-
-var nameExtractor = new hoot.NameExtractor();
-
-var parallelScoreExtractor = new hoot.ParallelScoreExtractor();
-
-var weightedMetricDistanceExtractor1 = new hoot.WeightedMetricDistanceExtractor(new hoot.MeanAggregator());
-
-var weightedShapeDistanceExtractor1 = new hoot.WeightedShapeDistanceExtractor(new hoot.MeanAggregator());
-var weightedShapeDistanceExtractor7 = new hoot.WeightedShapeDistanceExtractor(new hoot.SigmaAggregator());
+//var angleHistogramExtractor5 = new hoot.AngleHistogramExtractor({"angle.histogram.extractor.smoothing": 1.57});
+//var nameExtractor = new hoot.NameExtractor();
+//var parallelScoreExtractor = new hoot.ParallelScoreExtractor();
+//var weightedShapeDistanceExtractor1 = new hoot.WeightedShapeDistanceExtractor(new hoot.MeanAggregator());
 
 /**
  * Runs before match creation occurs and provides an opportunity to perform custom initialization.
@@ -112,66 +101,25 @@ exports.matchScore = function(map, e1, e2)
     return result;
   }
 
-  var maximalUsed = false;
-
   var sublines = sublineMatcher.extractMatchingSublines(map, e1, e2);
-
-  //This is an attempt to use maximal subline matching in situations when Frechet doesn't find the match.  Maximal finds more matches
-  //with some data, although for the most part, we want anything it finds that Frechet didn't to become a review rather than a match.
-  if (!sublines)
-  {
-    sublines = sublineMatcher2.extractMatchingSublines(map, e1, e2);
-    maximalUsed = true;
-  }
-  hoot.trace("maximalUsed");
-
   if (sublines)
   {
     var m = sublines.map;
     var m1 = sublines.match1;
     var m2 = sublines.match2;
-
-    var centroidDistanceExtractorVal;
-    var edgeDistanceExtractor1Val;
-    var weightedMetricDistanceExtractor1Val;
-
-    var angleHistogramExtractor5Val;
-    var distanceScoreExtractor7Val;
-    var nameExtractorVal;
-    var parallelScoreExtractorVal;
-    var weightedShapeDistanceExtractor1Val;
-
+    
+    var centroidDistanceExtractorVal = centroidDistanceExtractor.extract(m, m1, m2);
+    var edgeDistanceExtractor1Val = edgeDistanceExtractor1.extract(m, m1, m2);
+    var weightedMetricDistanceExtractor1Val = weightedMetricDistanceExtractor1.extract(m, m1, m2);
     var weightedShapeDistanceExtractor7Val = weightedShapeDistanceExtractor7.extract(m, m1, m2);
 
-    if (!maximalUsed)
-    {
-      centroidDistanceExtractorVal = centroidDistanceExtractor.extract(m, m1, m2);
-      edgeDistanceExtractor1Val = edgeDistanceExtractor1.extract(m, m1, m2);
-      weightedMetricDistanceExtractor1Val = weightedMetricDistanceExtractor1.extract(m, m1, m2);
-    }
-    else
-    {
-      angleHistogramExtractor5Val = angleHistogramExtractor5.extract(m, m1, m2);
-      distanceScoreExtractor7Val = distanceScoreExtractor7.extract(m, m1, m2);
-      nameExtractorVal = nameExtractor.extract(m, m1, m2);
-      parallelScoreExtractorVal = parallelScoreExtractor.extract(m, m1, m2);
-      weightedShapeDistanceExtractor1Val = weightedShapeDistanceExtractor1.extract(m, m1, m2);
-    }
-
     /*hoot.trace("centroidDistanceExtractorVal: " + centroidDistanceExtractorVal);
-    hoot.trace("angleHistogramExtractor5Val: " + angleHistogramExtractor5Val);
-    hoot.trace("distanceScoreExtractor7Val: " + distanceScoreExtractor7Val);
     hoot.trace("edgeDistanceExtractor1Val: " + edgeDistanceExtractor1Val);
-    hoot.trace("parallelScoreExtractorVal: " + parallelScoreExtractorVal);
     hoot.trace("weightedMetricDistanceExtractor1Val: " + weightedMetricDistanceExtractor1Val);
-    hoot.trace("weightedShapeDistanceExtractor1Val: " + weightedShapeDistanceExtractor1Val);
     hoot.trace("weightedShapeDistanceExtractor7Val: " + weightedShapeDistanceExtractor7Val);*/
 
-    if ((maximalUsed && angleHistogramExtractor5Val > 0.95 && distanceScoreExtractor7Val < 0.575 && parallelScoreExtractorVal > 0.118 &&
-         weightedShapeDistanceExtractor1Val > 0.118 && weightedShapeDistanceExtractor7Val < 0.13 && nameExtractorVal < 0.451 &&
-         weightedMetricDistanceExtractor1Val < 2.38/*angleHistogramExtractor5Val > 0.95 && weightedMetricDistanceExtractor1Val < 1.93*/) ||
-        (!maximalUsed && (centroidDistanceExtractorVal > 0.61 && weightedMetricDistanceExtractor1Val < 1.4) || 
-         (edgeDistanceExtractor1Val > 0.997 && weightedShapeDistanceExtractor7Val == 0.0)))
+    if ((centroidDistanceExtractorVal > 0.61 && weightedMetricDistanceExtractor1Val < 1.4) || 
+         (edgeDistanceExtractor1Val > 0.997 && weightedShapeDistanceExtractor7Val == 0.0))
     {
       //So far, voltage and location (underground vs overhead) seem to be the only tags useful to disambiguate power line matches.  We'll 
       //review when two features match and those tags disagree, and we'll ignore those tags completely if the values for either of them 
@@ -261,8 +209,8 @@ exports.getMatchFeatureDetails = function(map, e1, e2)
     var m1 = sublines.match1;
     var m2 = sublines.match2;
 
-    featureDetails["centroidDistanceExtractorVal"] = centroidDistanceExtractor.extract(m, m1, m2);
     featureDetails["angleHistogramExtractor5"] = angleHistogramExtractor5.extract(m, m1, m2);
+    featureDetails["centroidDistanceExtractorVal"] = centroidDistanceExtractor.extract(m, m1, m2);
     featureDetails["edgeDistanceExtractor1Val"] = edgeDistanceExtractor1.extract(m, m1, m2);
     featureDetails["nameExtractorVal"] = nameExtractor.extract(m, m1, m2);
     featureDetails["parallelScoreExtractorVal"] = parallelScoreExtractor.extract(m, m1, m2);
