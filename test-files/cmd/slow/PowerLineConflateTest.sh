bbfe40e080a15bcb77faa5f48df6b9386f255882
Brandon Witham <brandon.witham@radiantsolutions.com>
2018-07-23 20:40:45 +0000

    add power line conflate test

---
+++ test-files/cmd/slow/PowerLineConflateTest.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+mkdir -p $HOOT_HOME/tmp/
+mkdir -p test-output/cmd/PowerLineConflateTest
+
+hoot conflate --warn -D uuid.helper.repeatable=true -D match.creators="hoot::ScriptMatchCreator,PowerLine.js" -D merger.creators="hoot::ScriptMergerCreator" test-files/cmd/slow/PowerLineConflateTest/power-line-1.osm test-files/cmd/slow/PowerLineConflateTest/power-line-2.osm test-output/cmd/PowerLineConflateTest/output.osm
+hoot map-diff test-output/cmd/PowerLineConflateTest/output.osm test-files/cmd/slow/PowerLineConflateTest/output.osm || diff test-output/cmd/PowerLineConflateTest/output.osm test-files/cmd/slow/PowerLineConflateTest/output.osm
