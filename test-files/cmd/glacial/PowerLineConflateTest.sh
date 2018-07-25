eecc4ff01a5ca2e556a2d3ceba733ec4d0c1a891
Brandon Witham <brandon.witham@radiantsolutions.com>
2018-07-25 16:05:20 -0400

    fix some tests

--- test-files/cmd/glacial/PowerLineConflateTest.sh
+++ test-files/cmd/glacial/PowerLineConflateTest.sh
@@ -5,4 +5,4 @@ mkdir -p $HOOT_HOME/tmp/
 mkdir -p test-output/cmd/PowerLineConflateTest
 
 hoot conflate --warn -D uuid.helper.repeatable=true -D match.creators="hoot::ScriptMatchCreator,PowerLine.js" -D merger.creators="hoot::ScriptMergerCreator" test-files/cmd/glacial/PowerLineConflateTest/power-line-1.osm test-files/cmd/glacial/PowerLineConflateTest/power-line-2.osm test-output/cmd/PowerLineConflateTest/output.osm
-hoot map-diff test-output/cmd/PowerLineConflateTest/output.osm test-files/cmd/glacial/PowerLineConflateTest/output.osm || diff test-output/cmd/PowerLineConflateTest/output.osm test-files/cmd/glacial/PowerLineConflateTest/output.osm
+hoot diff test-output/cmd/PowerLineConflateTest/output.osm test-files/cmd/glacial/PowerLineConflateTest/output.osm || diff test-output/cmd/PowerLineConflateTest/output.osm test-files/cmd/glacial/PowerLineConflateTest/output.osm
--- test-files/cmd/slow/RndNodeDensityTilesCmdTest.sh
+++ test-files/cmd/slow/RndNodeDensityTilesCmdTest.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-OUTPUT_DIR=test-output/cmd/slow/RndCalculateTilesCmdTest
-GOLD_FILES_DIR=test-files/cmd/slow/RndCalculateTilesCmdTest
+OUTPUT_DIR=test-output/cmd/slow/RndNodeDensityTilesCmdTest
+GOLD_FILES_DIR=test-files/cmd/slow/RndNodeDensityTilesCmdTest
 mkdir -p $OUTPUT_DIR 
 
-hoot calculate-tiles --warn "test-files/DcGisRoads.osm;test-files/DcTigerRoads.osm" $OUTPUT_DIR/output.geojson 1000 0.001
+hoot node-density-tiles --warn "test-files/DcGisRoads.osm;test-files/DcTigerRoads.osm" $OUTPUT_DIR/output.geojson 1000 0.001
 diff $GOLD_FILES_DIR/output.geojson $OUTPUT_DIR/output.geojson
 
-hoot calculate-tiles --error "test-files/DcGisRoads.osm;test-files/DcTigerRoads.osm" $OUTPUT_DIR/output-random.geojson 1000 0.001 1 --random
+hoot node-density-tiles --error "test-files/DcGisRoads.osm;test-files/DcTigerRoads.osm" $OUTPUT_DIR/output-random.geojson 1000 0.001 1 --random
 diff $GOLD_FILES_DIR/output-random.geojson $OUTPUT_DIR/output-random.geojson
 
