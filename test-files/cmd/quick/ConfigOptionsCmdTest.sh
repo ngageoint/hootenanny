ac04cdf86af701b26ad4fd5df2b7cc4e6fc09bd6
Brandon Witham <brandon.witham@radiantsolutions.com>
2018-07-25 18:09:23 -0400

    fix test

--- test-files/cmd/quick/ConfigOptionsCmdTest.sh
+++ test-files/cmd/quick/ConfigOptionsCmdTest.sh
@@ -6,19 +6,19 @@ CONFIG_FILE=test-files/cmd/quick/OptionsCmdTest/ConfigOptions.asciidoc
 
 echo "Listing all option names..."
 echo ""
-hoot options -D config.options.file=$CONFIG_FILE --option-names
+hoot config-options -D config.options.file=$CONFIG_FILE --option-names
 echo ""
 
 echo "Listing all option names and descriptions..."
 echo ""
-hoot options -D config.options.file=$CONFIG_FILE --option-details
+hoot config-options -D config.options.file=$CONFIG_FILE --option-details
 echo ""
 
 echo "Listing all option names containing 'poi.polygon'..."
 echo ""
-hoot options -D config.options.file=$CONFIG_FILE poi.polygon --option-names
+hoot config-options -D config.options.file=$CONFIG_FILE poi.polygon --option-names
 echo ""
 
 echo "Listing all option names containing 'poi.polygon' and their descriptions..."
 echo ""
-hoot options -D config.options.file=$CONFIG_FILE poi.polygon --option-details
+hoot config-options -D config.options.file=$CONFIG_FILE poi.polygon --option-details
