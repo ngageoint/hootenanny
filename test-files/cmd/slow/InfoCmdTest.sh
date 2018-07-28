73aaf54529ffb57faa77a5d1bd6ea508844e3ca4
Brandon Witham <brandon.witham@radiantsolutions.com>
2018-07-28 15:43:41 +0000

    fix test; copyright headers

--- test-files/cmd/slow/InfoCmdTest.sh
+++ test-files/cmd/slow/InfoCmdTest.sh
@@ -7,46 +7,81 @@ CONFIG_FILE=test-files/cmd/quick/ConfigOptionsCmdTest/ConfigOptions.asciidoc
 
 echo "Listing all option names..."
 echo ""
-hoot info --config-options --option-names -D config.options.file=$CONFIG_FILE
+hoot info -D config.options.file=$CONFIG_FILE --config-options --option-names
 echo ""
 
 echo "Listing all option names and descriptions..."
 echo ""
-hoot info --config-options --option-details -D config.options.file=$CONFIG_FILE
+hoot info -D config.options.file=$CONFIG_FILE --config-options --option-details
 echo ""
 
 echo "Listing all option names containing 'poi.polygon'..."
 echo ""
-hoot info --config-options --option-names -D config.options.file=$CONFIG_FILE poi.polygon
+hoot info -D config.options.file=$CONFIG_FILE --config-options --option-names poi.polygon
 echo ""
 
 echo "Listing all option names containing 'poi.polygon' and their descriptions..."
 echo ""
-hoot info --config-options --option-details -D config.options.file=$CONFIG_FILE poi.polygon
+hoot info -D config.options.file=$CONFIG_FILE --config-options --option-details poi.polygon
 
 # Since the list of a lot of these will constantly be changing, just going to grep out ones that are unlikely to change any time soon.
 
 # FEATURE EXTRACTORS
+echo "Listing all option names..."
+echo ""
 hoot info --feature-extractors | grep "SampledAngleHistogramExtractor"
+echo ""
 
 # FORMATS
+echo "Listing a format that is both input and output..."
+echo ""
 hoot info --formats | grep "hootapidb://"
+echo ""
+echo "Listing an input format..."
+echo ""
 hoot info --formats --input | grep ".osm.bz2"
+echo ""
+echo "Listing an output format..."
+echo ""
 hoot info --formats --output | grep ".shp"
+echo ""
 
 # MATCHERS
+echo "Listing a match creator..."
+echo ""
 hoot info --matchers | grep "HighwayMatchCreator"
-hoot info --matchers | grep "ScriptMatchCreator"
+echo ""
+echo "Listing a script match creator..."
+echo ""
+hoot info --matchers | grep "ScriptMatchCreator,PoiGeneric.js"
+echo ""
 
 # MERGERS
+echo "Listing a merger..."
+echo ""
 hoot info --mergers | grep "PoiPolygonMergerCreator"
+echo ""
+echo "Listing a script merger..."
+echo ""
 hoot info --mergers | grep "ScriptMergerCreator"
+echo ""
 
 # OPERATORS
+echo "Listing an op..."
+echo ""
 hoot info --operators | grep "MapCleaner"
-hoot info --operators | grep "ImplicitPoiTypeTagger"
+echo ""
+echo "Listing a visitor..."
+echo ""
 hoot info --operators | grep "RemoveEmptyAreasVisitor"
+echo ""
+echo "Listing a criterion..."
+echo ""
 hoot info --operators | grep "NonBuildingAreaCriterion"
+echo ""
 
 # TAG MERGERS
+echo "Listing a tag merger..."
+echo ""
 hoot info --tag-mergers | grep "AverageTagMerger"
+echo ""
