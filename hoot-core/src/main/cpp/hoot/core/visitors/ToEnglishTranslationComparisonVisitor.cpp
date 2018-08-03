acfe6c110fac549be205053e70f04e77cb85f4ee
Brandon Witham <brandon.witham@radiantsolutions.com>
2018-08-03 12:54:53 -0400

    encoding fixes

--- hoot-core/src/main/cpp/hoot/core/visitors/ToEnglishTranslationComparisonVisitor.cpp
+++ hoot-core/src/main/cpp/hoot/core/visitors/ToEnglishTranslationComparisonVisitor.cpp
@@ -9,6 +9,8 @@
 
 // Qt
 #include <QProcess>
+#include <QTextCodec>
+#include <QLocale>
 
 namespace hoot
 {
@@ -36,42 +38,55 @@ void ToEnglishTranslationComparisonVisitor::_translateName(Tags& tags,
 {
   if ((tags.contains(toTranslateNameKey) && tags.contains(preTranslatedNameKey)))
   {
-    //TODO: fix encoding
-    QString name = tags.get(toTranslateNameKey)/*.toUtf8()*/.trimmed();
+    //TODO: these replacements are to appease the bash command; if we switch to a network call
+    //with qt, shouldn't be needed (or maybe there's a better way to encode these inside the
+    //bash command)
+    QString name = tags.get(toTranslateNameKey).trimmed();
     name = name.replace("&amp;", "and");
     name = name.replace("&", "and");
     name = name.replace("'", "");
     name = name.replace("(", "");
     name = name.replace(")", "");
-    QString preTranslatedName = tags.get(preTranslatedNameKey)/*.toUtf8()*/.trimmed();
+    LOG_VARD(name);
+    QString preTranslatedName = tags.get(preTranslatedNameKey).trimmed();
     preTranslatedName = preTranslatedName.replace("&amp;", "and");
     preTranslatedName = preTranslatedName.replace("&", "and");
     preTranslatedName = preTranslatedName.replace("'", "");
     preTranslatedName = preTranslatedName.replace("(", "");
     preTranslatedName = preTranslatedName.replace(")", "");
+    LOG_VARD(preTranslatedName);
     //TODO: add english common word lookup
     if (!name.isEmpty() && name != preTranslatedName)
     {
       //TODO: replace with net call
       QProcess process;
-      process.start("bash -c \"echo " + name + " | nc localhost 5674");
-      process.waitForFinished(-1); // will wait forever until finished
+      const QString cmd = "bash -c \"echo " + name + " | nc localhost 5674\"";
+      LOG_VARD(cmd);
+      process.start(cmd);
+      process.waitForFinished(-1);
 
       const QString error = process.readAllStandardError();
       if (!error.trimmed().isEmpty())
       {
         throw HootException("Unable to translate name: " + name + "; error: " + error);
       }
-      QString translatedName = process.readAllStandardOutput();
-      //translatedName = translatedName.toUtf8();
-      if (translatedName.compare(name, Qt::CaseInsensitive) != 0)
-      //if (translatedName != name)
+
+      const QString translatedName =
+        QTextCodec::codecForMib(106)->toUnicode(process.readAllStandardOutput()).trimmed();
+      LOG_VARD(translatedName);
+      //not sure if this locale stuff is needed yet
+      //QLocale::setDefault(QLocale(QLocale::German, QLocale::Germany));
+      //QLocale::setDefault(QLocale(QLocale::German));
+      //const int nameComparison = translatedName.localeAwareCompare(name);
+      const int nameComparison = translatedName.compare(name, Qt::CaseInsensitive);
+      LOG_VARD(nameComparison);
+      if (nameComparison != 0)
       {
         tags.appendValue("joshua:translated:" + toTranslateNameKey, translatedName);
-        //LOG_ERROR(translatedName);
+        const double similarityScore = _levDist.score(preTranslatedName, translatedName);
+        LOG_VARD(similarityScore);
         tags.appendValue(
-          "joshua:similarityscore:" + toTranslateNameKey,
-          QString::number(_levDist.score(preTranslatedName, translatedName)));
+          "joshua:similarityscore:" + toTranslateNameKey, QString::number(similarityScore));
       }
     }
   }
