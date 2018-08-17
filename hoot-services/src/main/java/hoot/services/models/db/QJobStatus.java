b3df7823203575664a1256dea480107b70ddcc59
Brian Hatchl <brian.hatchl@radiantsolutions.com>
2018-08-17 13:04:07 -0400

    update copyright headers
    refs #2565

--- hoot-services/src/main/java/hoot/services/models/db/QJobStatus.java
+++ hoot-services/src/main/java/hoot/services/models/db/QJobStatus.java
@@ -5,7 +5,7 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -22,7 +22,7 @@
  * This will properly maintain the copyright information. DigitalGlobe
  * copyrights will be updated automatically.
  *
- * @copyright Copyright (C) 2016 DigitalGlobe (http://www.digitalglobe.com/)
+ * @copyright Copyright (C) 2016, 2017, 2018 DigitalGlobe (http://www.digitalglobe.com/)
  */
 package hoot.services.models.db;
 
@@ -80,7 +80,7 @@ public class QJobStatus extends com.querydsl.sql.RelationalPathBase<JobStatus> {
         super(path.getType(), path.getMetadata(), "public", "job_status");
         addMetadata();
     }
-    
+
     public QJobStatus(PathMetadata metadata) {
         super(JobStatus.class, metadata, "public", "job_status");
         addMetadata();
