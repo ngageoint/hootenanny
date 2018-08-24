7d797cd9953adc2f04f5471359acafe1900b0ef5
Arni Sumarlidason <sumarlidason@gmail.com>
2018-08-24 10:57:06 -0400

    Work-in-progress implementation of OAuth1.0
    refs #2566
    
    * implement two rest end-points to be used by XHR from hoot-ui to authenticate user w/ hoot-services

---
+++ hoot-services/src/main/java/hoot/services/HootAccessDeniedHandler.java
@@ -0,0 +1,20 @@
+package hoot.services;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+public class HootAccessDeniedHandler implements AccessDeniedHandler {
+
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response,
+			AccessDeniedException accessDeniedException) throws IOException, ServletException {
+		response.sendError(401);
+	}
+
+}
