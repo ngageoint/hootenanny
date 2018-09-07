0617979844e0db2c393f495a1881106b6fee0cbd
Arni Sumarlidason <sumarlidason@gmail.com>
2018-09-07 16:30:42 -0400

    Work-in-progress implementation of OAuth1.0
    refs #2566
    
    * WIP: Update UserResource unit tests

--- hoot-services/src/test/java/hoot/services/utils/MapUtils.java
+++ hoot-services/src/test/java/hoot/services/utils/MapUtils.java
@@ -48,165 +48,173 @@ import hoot.services.models.db.QCurrentWays;
 
 public final class MapUtils {
 
-    private MapUtils() {}
+	private MapUtils() {}
 
-    /**
-     *
-     *
-     * @param mapId
+	/**
+	 *
+	 *
+	 * @param mapId
 
-     *             //TODO: This code needs to be changed to dynamically read in
-     *             the data types from querydsl. If I make a change to the
-     *             schema in liquibase, it will never be picked up unless this
-     *             static code is also changed. See r6777
-     */
-    public static void createMap(long mapId) {
-        try {
-            Connection connection = createQuery().getConnection();
+	 *             //TODO: This code needs to be changed to dynamically read in
+	 *             the data types from querydsl. If I make a change to the
+	 *             schema in liquibase, it will never be picked up unless this
+	 *             static code is also changed. See r6777
+	 */
+	public static void createMap(long mapId) {
+		try {
+			Connection connection = createQuery().getConnection();
 
-            // changesets
-            String createTblSql = "CREATE TABLE changesets_" + mapId + "(id bigserial NOT NULL, "
-                    + " user_id bigint NOT NULL, " + " created_at timestamp without time zone NOT NULL, "
-                    + " min_lat double precision NOT NULL, " + " max_lat double precision NOT NULL, "
-                    + " min_lon double precision NOT NULL, " + " max_lon double precision NOT NULL, "
-                    + " closed_at timestamp without time zone NOT NULL, " + " num_changes integer NOT NULL DEFAULT 0, "
-                    + " tags hstore, " + " CONSTRAINT pk_changesets_" + mapId + " PRIMARY KEY (id ), "
-                    + " CONSTRAINT changesets_" + mapId + "_user_id_fkey FOREIGN KEY (user_id) "
-                    + " REFERENCES users (id) MATCH SIMPLE " + " ON UPDATE NO ACTION ON DELETE NO ACTION "
-                    + " ) WITH ( OIDS=FALSE );";
+			// changesets
+			String createTblSql = "CREATE TABLE changesets_" + mapId + "(id bigserial NOT NULL, "
+					+ " user_id bigint NOT NULL, " + " created_at timestamp without time zone NOT NULL, "
+					+ " min_lat double precision NOT NULL, " + " max_lat double precision NOT NULL, "
+					+ " min_lon double precision NOT NULL, " + " max_lon double precision NOT NULL, "
+					+ " closed_at timestamp without time zone NOT NULL, " + " num_changes integer NOT NULL DEFAULT 0, "
+					+ " tags hstore, " + " CONSTRAINT pk_changesets_" + mapId + " PRIMARY KEY (id ), "
+					+ " CONSTRAINT changesets_" + mapId + "_user_id_fkey FOREIGN KEY (user_id) "
+					+ " REFERENCES users (id) MATCH SIMPLE " + " ON UPDATE NO ACTION ON DELETE NO ACTION "
+					+ " ) WITH ( OIDS=FALSE );";
 
-            createTable(createTblSql, connection);
+			createTable(createTblSql, connection);
 
-            // current_nodes
-            createTblSql = "CREATE TABLE current_nodes_" + mapId + "(id bigserial NOT NULL, "
-                    + " latitude double precision NOT NULL, " + " longitude double precision NOT NULL, "
-                    + " changeset_id bigint NOT NULL, " + " visible boolean NOT NULL DEFAULT true, "
-                    + " \"timestamp\" timestamp without time zone NOT NULL DEFAULT now(), " + " tile bigint NOT NULL, "
-                    + " version bigint NOT NULL DEFAULT 1, " + " tags hstore, " + " CONSTRAINT current_nodes_" + mapId
-                    + "_pkey" + " PRIMARY KEY (id ), " + " CONSTRAINT current_nodes_" + mapId
-                    + "_changeset_id_fkey FOREIGN KEY (changeset_id) " + " REFERENCES changesets_" + mapId
-                    + " (id) MATCH SIMPLE " + " ON UPDATE NO ACTION ON DELETE NO ACTION " + " ) WITH ( OIDS=FALSE );";
+			// current_nodes
+			createTblSql = "CREATE TABLE current_nodes_" + mapId + "(id bigserial NOT NULL, "
+					+ " latitude double precision NOT NULL, " + " longitude double precision NOT NULL, "
+					+ " changeset_id bigint NOT NULL, " + " visible boolean NOT NULL DEFAULT true, "
+					+ " \"timestamp\" timestamp without time zone NOT NULL DEFAULT now(), " + " tile bigint NOT NULL, "
+					+ " version bigint NOT NULL DEFAULT 1, " + " tags hstore, " + " CONSTRAINT current_nodes_" + mapId
+					+ "_pkey" + " PRIMARY KEY (id ), " + " CONSTRAINT current_nodes_" + mapId
+					+ "_changeset_id_fkey FOREIGN KEY (changeset_id) " + " REFERENCES changesets_" + mapId
+					+ " (id) MATCH SIMPLE " + " ON UPDATE NO ACTION ON DELETE NO ACTION " + " ) WITH ( OIDS=FALSE );";
 
-            createTable(createTblSql, connection);
+			createTable(createTblSql, connection);
 
-            // current_relation_members
-            createTblSql = "CREATE TABLE current_relation_members_" + mapId + "(relation_id bigint NOT NULL, "
-                    + " member_type nwr_enum NOT NULL, " + " member_id bigint NOT NULL, "
-                    + " member_role character varying(255) NOT NULL, " + " sequence_id integer NOT NULL DEFAULT 0, "
-                    + " CONSTRAINT current_relation_members_" + mapId
-                    + "_pkey PRIMARY KEY (relation_id , member_type , member_id , member_role , sequence_id ) "
-                    + " ) WITH ( OIDS=FALSE );";
+			// current_relation_members
+			createTblSql = "CREATE TABLE current_relation_members_" + mapId + "(relation_id bigint NOT NULL, "
+					+ " member_type nwr_enum NOT NULL, " + " member_id bigint NOT NULL, "
+					+ " member_role character varying(255) NOT NULL, " + " sequence_id integer NOT NULL DEFAULT 0, "
+					+ " CONSTRAINT current_relation_members_" + mapId
+					+ "_pkey PRIMARY KEY (relation_id , member_type , member_id , member_role , sequence_id ) "
+					+ " ) WITH ( OIDS=FALSE );";
 
-            createTable(createTblSql, connection);
+			createTable(createTblSql, connection);
 
-            // current_relations
-            createTblSql = "CREATE TABLE current_relations_" + mapId + "(" + "  id bigserial NOT NULL,"
-                    + "  changeset_id bigint NOT NULL,"
-                    + "  \"timestamp\" timestamp without time zone NOT NULL DEFAULT now(),"
-                    + "  visible boolean NOT NULL DEFAULT true," + "  version bigint NOT NULL DEFAULT 1,"
-                    + "  tags hstore," + "  CONSTRAINT current_relations_" + mapId + "_pkey PRIMARY KEY (id ),"
-                    + "  CONSTRAINT current_relations_" + mapId + "_changeset_id_fkey FOREIGN KEY (changeset_id)"
-                    + "      REFERENCES changesets_" + mapId + " (id) MATCH SIMPLE"
-                    + "      ON UPDATE NO ACTION ON DELETE NO ACTION" + ")" + "WITH (" + "  OIDS=FALSE" + ");";
+			// current_relations
+			createTblSql = "CREATE TABLE current_relations_" + mapId + "(" + "  id bigserial NOT NULL,"
+					+ "  changeset_id bigint NOT NULL,"
+					+ "  \"timestamp\" timestamp without time zone NOT NULL DEFAULT now(),"
+					+ "  visible boolean NOT NULL DEFAULT true," + "  version bigint NOT NULL DEFAULT 1,"
+					+ "  tags hstore," + "  CONSTRAINT current_relations_" + mapId + "_pkey PRIMARY KEY (id ),"
+					+ "  CONSTRAINT current_relations_" + mapId + "_changeset_id_fkey FOREIGN KEY (changeset_id)"
+					+ "      REFERENCES changesets_" + mapId + " (id) MATCH SIMPLE"
+					+ "      ON UPDATE NO ACTION ON DELETE NO ACTION" + ")" + "WITH (" + "  OIDS=FALSE" + ");";
 
-            createTable(createTblSql, connection);
+			createTable(createTblSql, connection);
 
-            // current_way_nodes
-            createTblSql = "CREATE TABLE current_way_nodes_" + mapId + "(" + "  way_id bigint NOT NULL,"
-                    + "  node_id bigint NOT NULL," + "  sequence_id bigint NOT NULL," + "  CONSTRAINT current_way_"
-                    + mapId + "_nodes_pkey PRIMARY KEY (way_id , sequence_id )" + ")" + "WITH (" + "  OIDS=FALSE"
-                    + ");";
+			// current_way_nodes
+			createTblSql = "CREATE TABLE current_way_nodes_" + mapId + "(" + "  way_id bigint NOT NULL,"
+					+ "  node_id bigint NOT NULL," + "  sequence_id bigint NOT NULL," + "  CONSTRAINT current_way_"
+					+ mapId + "_nodes_pkey PRIMARY KEY (way_id , sequence_id )" + ")" + "WITH (" + "  OIDS=FALSE"
+					+ ");";
 
-            createTable(createTblSql, connection);
+			createTable(createTblSql, connection);
 
-            // current_ways
-            createTblSql = "CREATE TABLE current_ways_" + mapId + "(" + "  id bigserial NOT NULL,"
-                    + "  changeset_id bigint NOT NULL,"
-                    + "  \"timestamp\" timestamp without time zone NOT NULL DEFAULT now(),"
-                    + "  visible boolean NOT NULL DEFAULT true," + "  version bigint NOT NULL DEFAULT 1,"
-                    + "  tags hstore," + "  CONSTRAINT current_ways_" + mapId + "_pkey PRIMARY KEY (id ),"
-                    + "  CONSTRAINT current_ways_" + mapId + "_changeset_id_fkey FOREIGN KEY (changeset_id)"
-                    + "      REFERENCES changesets_" + mapId + " (id) MATCH SIMPLE"
-                    + "      ON UPDATE NO ACTION ON DELETE NO ACTION" + ")" + "WITH (" + "  OIDS=FALSE" + ");";
+			// current_ways
+			createTblSql = "CREATE TABLE current_ways_" + mapId + "(" + "  id bigserial NOT NULL,"
+					+ "  changeset_id bigint NOT NULL,"
+					+ "  \"timestamp\" timestamp without time zone NOT NULL DEFAULT now(),"
+					+ "  visible boolean NOT NULL DEFAULT true," + "  version bigint NOT NULL DEFAULT 1,"
+					+ "  tags hstore," + "  CONSTRAINT current_ways_" + mapId + "_pkey PRIMARY KEY (id ),"
+					+ "  CONSTRAINT current_ways_" + mapId + "_changeset_id_fkey FOREIGN KEY (changeset_id)"
+					+ "      REFERENCES changesets_" + mapId + " (id) MATCH SIMPLE"
+					+ "      ON UPDATE NO ACTION ON DELETE NO ACTION" + ")" + "WITH (" + "  OIDS=FALSE" + ");";
 
-            createTable(createTblSql, connection);
-        }
-        catch (SQLException e) {
-            throw new RuntimeException("Error creating map with id = " + mapId, e);
-        }
-    }
+			createTable(createTblSql, connection);
+		}
+		catch (SQLException e) {
+			throw new RuntimeException("Error creating map with id = " + mapId, e);
+		}
+	}
 
-    /**
-     * Determines whether any OSM element records exist in the services database
-     *
-     * @return true if any OSM element records exist in the services database;
-     *         false otherwise
-     */
-    public static boolean elementDataExistsInServicesDb() {
-        long recordCount = 0;
+	/**
+	 * Determines whether any OSM element records exist in the services database
+	 *
+	 * @return true if any OSM element records exist in the services database;
+	 *         false otherwise
+	 */
+	public static boolean elementDataExistsInServicesDb() {
+		long recordCount = 0;
 
-        SQLQuery query = createQuery().query();
-        recordCount += query.from(QCurrentNodes.currentNodes).fetchCount();
-        recordCount += query.from(QCurrentWayNodes.currentWayNodes).fetchCount();
-        recordCount += query.from(QCurrentWays.currentWays).fetchCount();
-        recordCount += query.from(QCurrentRelationMembers.currentRelationMembers).fetchCount();
-        recordCount += query.from(QCurrentRelations.currentRelations).fetchCount();
+		SQLQuery query = createQuery().query();
+		recordCount += query.from(QCurrentNodes.currentNodes).fetchCount();
+		recordCount += query.from(QCurrentWayNodes.currentWayNodes).fetchCount();
+		recordCount += query.from(QCurrentWays.currentWays).fetchCount();
+		recordCount += query.from(QCurrentRelationMembers.currentRelationMembers).fetchCount();
+		recordCount += query.from(QCurrentRelations.currentRelations).fetchCount();
 
-        return (recordCount > 0);
-    }
+		return (recordCount > 0);
+	}
 
-    public static long insertMap(long userId) {
-        Long newId = createQuery()
-                .select(Expressions.numberTemplate(Long.class, "nextval('maps_id_seq')"))
-                .from()
-                .fetchOne();
+	public static long insertMap(long userId) {
+		Long newId = createQuery()
+				.select(Expressions.numberTemplate(Long.class, "nextval('maps_id_seq')"))
+				.from()
+				.fetchOne();
 
-        if (newId != null) {
-            Timestamp now = new Timestamp(Calendar.getInstance().getTimeInMillis());
+		if (newId != null) {
+			Timestamp now = new Timestamp(Calendar.getInstance().getTimeInMillis());
 
-            createQuery().insert(maps)
-                    .columns(maps.id, maps.createdAt, maps.displayName, maps.publicCol, maps.userId)
-                    .values(newId, now, "map-with-id-" + newId, true, userId)
-                    .execute();
-        }
+			createQuery().insert(maps)
+			.columns(maps.id, maps.createdAt, maps.displayName, maps.publicCol, maps.userId)
+			.values(newId, now, "map-with-id-" + newId, true, userId)
+			.execute();
+		}
 
-        createMap(newId);
+		createMap(newId);
 
-        return newId;
-    }
+		return newId;
+	}
 
-    public static long insertUser() {
-        Long newId = createQuery()
-                .select(Expressions.numberTemplate(Long.class, "nextval('users_id_seq')"))
-                .from()
-                .fetchOne();
+	public static long insertUser() {
+		Long newId = createQuery()
+				.select(Expressions.numberTemplate(Long.class, "nextval('users_id_seq')"))
+				.from()
+				.fetchOne();
 
-        if (newId != null) {
-            createQuery().insert(users)
-                    .columns(users.id, users.displayName, users.email)
-                    .values(newId, "user-with-id-" + newId, "user-with-id-" + newId)
-                    .execute();
-        }
+		assert newId != null : "failed to generate new test user id";
 
-        return newId;
-    }
+		// don't collide with someone's actual osm user id
+		newId *= -1;
+		long rowsAffected = createQuery().insert(users)
+				.columns(users.id, users.displayName, users.email, users.provider_access_key,
+						users.provider_access_token, users.session_id, users.hootservices_created_at,
+						users.hootservices_last_authorize, users.provider_created_at)
+				.values(newId, newId + "::MapUtils::insertUser()", newId + "@hootenanny.test", "provider_access_key",
+						"provider_access_token", "session_id_0000000000000000000000000",
+						Expressions.currentTimestamp(), Expressions.currentTimestamp(), Expressions.currentTimestamp())
+				.execute();
 
-    static void deleteUser(long userId) {
-        createQuery().delete(users).where(users.id.eq(userId)).execute();
-    }
+		assert rowsAffected == 1 : "failed to insert test user";
 
-    /**
-     *
-     * @param mapId
-     */
-    public static void deleteOSMRecord(Long mapId) {
-        DbUtils.deleteMapRelatedTablesByMapId(mapId);
-        createQuery().delete(maps).where(maps.id.eq(mapId)).execute();
-    }
+		return newId;
+	}
 
-    private static void createTable(String createTblSql, Connection conn) throws SQLException {
-        try (PreparedStatement stmt = conn.prepareStatement(createTblSql)) {
-            stmt.executeUpdate();
-        }
-    }
+	static void deleteUser(long userId) {
+		createQuery().delete(users).where(users.id.eq(userId)).execute();
+	}
+
+	/**
+	 *
+	 * @param mapId
+	 */
+	public static void deleteOSMRecord(Long mapId) {
+		DbUtils.deleteMapRelatedTablesByMapId(mapId);
+		createQuery().delete(maps).where(maps.id.eq(mapId)).execute();
+	}
+
+	private static void createTable(String createTblSql, Connection conn) throws SQLException {
+		try (PreparedStatement stmt = conn.prepareStatement(createTblSql)) {
+			stmt.executeUpdate();
+		}
+	}
 }
