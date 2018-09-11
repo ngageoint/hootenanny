1da784b847d05138f389aac9b27a1a5a9d217e13
Arni Sumarlidason <sumarlidason@gmail.com>
2018-09-11 09:49:52 -0400

    Work-in-progress implementation of OAuth1.0
    refs #2566
    
    * push forward hoot-core db version

--- hoot-core/src/main/cpp/hoot/core/io/ApiDb.h
+++ hoot-core/src/main/cpp/hoot/core/io/ApiDb.h
@@ -103,7 +103,7 @@ public:
   /**
    * This value should be updated after the DB is upgraded and all tests run successfully.
    */
-  static QString expectedHootDbVersion() { return "18:dmitriy.mylov"; }
+  static QString expectedHootDbVersion() { return "20:arni.sumarlidason"; }
   static int maximumChangeSetEdits() { return 50000; }
 
   static const Status DEFAULT_ELEMENT_STATUS;
