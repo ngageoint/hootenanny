2cec50c73bb2c48494ec9bb71b26d112d0afd343
Ben Marchant <ben_marchant@hotmail.com>
2018-09-25 21:06:57 -0500

    Renamed Makefile.old to Makefile.hoot and updated all occurances of the 'old' name.

--- scripts/jenkins/TestQuick.sh
+++ scripts/jenkins/TestQuick.sh
@@ -8,5 +8,5 @@ cd $HOOT_HOME
 hoot version --debug
 
 export HOOT_TEST_DIFF=--diff
-make -s -f Makefile.old services-test-all
+make -s -f Makefile.hoot services-test-all
 HootTest --quick --parallel $(nproc)
