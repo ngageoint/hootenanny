42cbb99bcccb690854099023bb042d82abb79ab9
Brandon Witham <brandon.witham@radiantsolutions.com>
2018-10-05 18:38:59 -0400

    remove inadvertant changes

--- hoot-services/src/main/java/hoot/services/controllers/auth/OAuth1Resource.java
+++ hoot-services/src/main/java/hoot/services/controllers/auth/OAuth1Resource.java
@@ -56,8 +56,6 @@ import org.xml.sax.SAXException;
 import hoot.services.HootProperties;
 import hoot.services.models.db.Users;
 
-import java.util.Arrays;
-
 @Controller
 @Path("")
 @Transactional
@@ -75,51 +73,26 @@ public class OAuth1Resource {
     @Path("/oauth1/verify")
     @Produces(MediaType.APPLICATION_JSON)
     public Response verify(@QueryParam("oauth_token") String oauth_token, @QueryParam("oauth_verifier") String oauth_verifier) {
-        logger.error("oauth_token: " + oauth_token);
-        logger.error("oauth_verifier: " + oauth_verifier);
         OAuthConsumerSupport s = oauthRestTemplate.getSupport();
         ProtectedResourceDetails r = oauthRestTemplate.getResource();
-        logger.error("ProtectedResourceDetails:");
-        logger.error("id: " + r.getId());
-        logger.error("access token url: " + r.getAccessTokenURL());
-        logger.error("additional params: " + r.getAdditionalParameters());
-        logger.error("additional request headers: " + r.getAdditionalRequestHeaders());
-        logger.error("auth header realm: " + r.getAuthorizationHeaderRealm());
-        logger.error("consumer key: " + r.getConsumerKey());
-        logger.error("request token url: " + r.getRequestTokenURL());
-        logger.error("user auth url: " + r.getUserAuthorizationURL());
         HttpSession sess = getSession();
         if (sess == null) {
-            return Response.status(401).entity("No session specified.").build();
+            return Response.status(401).build();
         }
         sess.setMaxInactiveInterval(31536000);
-        //logger.error("Session: " + Arrays.toString(sess.getValueNames()));
-        for (String valName : sess.getValueNames())
-        {
-          logger.error("key: " + valName + ", val: " + sess.getValue(valName));
-        }
         OAuthConsumerToken requestToken = tokenServices.getToken(r.getId());
         if (requestToken == null) {
-            logger.error("requestToken null");
-            return Response.status(401).entity("Unable to retrieve request token.").build();
+            return Response.status(401).build();
         }
         OAuthConsumerToken accessToken = null;
         try {
             accessToken = s.getAccessToken(r /* <== Specifying this was important [!] */, requestToken, oauth_verifier);
         } catch (Exception e) {
             // Pass.
-            logger.error(e.getMessage());
         }
         if (accessToken == null) {
-            logger.error("accessToken null");
-            return Response.status(401).entity("Unable to retrieve access token.").build();
+            return Response.status(401).build();
         }
-        logger.error("accessToken:");
-        logger.error("resource id: " + accessToken.getResourceId());
-        logger.error("secret: " + accessToken.getSecret());
-        logger.error("value: " + accessToken.getValue());
-        logger.error("isAccessToken: " + accessToken.isAccessToken());
-        logger.error("additional params: " + accessToken.getAdditionalParameters());
         // Save this access token to our HTTP Session based token services:
         tokenServices.storeToken(r.getId(), accessToken);
         // Save this access token to our security context, used by
@@ -127,29 +100,19 @@ public class OAuth1Resource {
         // outbound requests.
         OAuthSecurityContext context = OAuthSecurityContextHolder.getContext();
         context.getAccessTokens().put(r.getId(), accessToken);
-        logger.error("security context:");
-        logger.error("access tokens: " + context.getAccessTokens());
-        logger.error("details: " + context.getDetails());
 
         String response = oauthRestTemplate
                 .getForObject(HootProperties.OAUTH_PROVIDERURL + HootProperties.OAUTH_PATHS_USER, String.class);
-        logger.error("response: " + response);
         Users user;
         try {
             user = userManager.upsert(response, accessToken, sess.getId());
         } catch (InvalidUserProfileException | SAXException | IOException | ParserConfigurationException e) {
             logger.error("Failed to read user profile from oauth provider", e);
-            return Response.status(502).entity("Failed to read user profile from oauth provider.").build();
+            return Response.status(502).build();
         } catch (Exception e) {
             logger.error("Failed to store user during oauth verification", e);
-            return Response.status(500).entity("Failed to store user during oauth verification.").build();
+            return Response.status(500).build();
         }
-        logger.error("user:");
-        logger.error("email: " + user.getEmail());
-        logger.error("display name: " + user.getDisplayName());
-        logger.error("id: " + user.getId());
-        logger.error("provider access key: " + user.getProviderAccessKey());
-        logger.error("provider access token: " + user.getProviderAccessToken());
 
         return Response.status(200).entity(user).type(MediaType.APPLICATION_JSON).build();
     }
@@ -160,34 +123,18 @@ public class OAuth1Resource {
     public Response request() {
         OAuthConsumerSupport s = oauthRestTemplate.getSupport();
         ProtectedResourceDetails r = oauthRestTemplate.getResource();
-        logger.error("ProtectedResourceDetails:");
-        logger.error("id: " + r.getId());
-        logger.error("access token url: " + r.getAccessTokenURL());
-        logger.error("additional params: " + r.getAdditionalParameters());
-        logger.error("additional request headers: " + r.getAdditionalRequestHeaders());
-        logger.error("auth header realm: " + r.getAuthorizationHeaderRealm());
-        logger.error("consumer key: " + r.getConsumerKey());
-        logger.error("request token url: " + r.getRequestTokenURL());
-        logger.error("user auth url: " + r.getUserAuthorizationURL());
         OAuthConsumerToken requestToken = null;
         try {
             requestToken = s.getUnauthorizedRequestToken(r, HootProperties.OAUTH_REDIRECTURL);
         } catch (OAuthRequestFailedException e) {
             logger.error("Failed to obtain request token", e);
-            return Response.status(502).entity("Failed to obtain request token.").build();
+            return Response.status(502).build();
         } catch (Exception e) {
             logger.error("Failed to execute request for token.", e);
-            return Response.status(500).entity("Failed to execute request for token.").build();
+            return Response.status(500).build();
         }
-        logger.error("requestToken:");
-        logger.error("resource id: " + requestToken.getResourceId());
-        logger.error("secret: " + requestToken.getSecret());
-        logger.error("value: " + requestToken.getValue());
-        logger.error("isAccessToken: " + requestToken.isAccessToken());
-        logger.error("additional params: " + requestToken.getAdditionalParameters());
 
         tokenServices.storeToken(r.getId(), requestToken);
-        logger.error("Response: " + r.getUserAuthorizationURL() + "?oauth_token=" + requestToken.getValue());
         return Response.ok().entity(r.getUserAuthorizationURL() + "?oauth_token=" + requestToken.getValue()).type(MediaType.TEXT_PLAIN).build();
     }
     @GET
@@ -195,7 +142,6 @@ public class OAuth1Resource {
     @Produces(MediaType.TEXT_PLAIN)
     public Response logout() {
 
-        logger.error("logout");
         ProtectedResourceDetails r = oauthRestTemplate.getResource();
         tokenServices.removeToken(r.getId());
 
