a2ca567a8e0261f5eccfcb2c4e4b634b032355f0
Brandon Witham <brandon.witham@radiantsolutions.com>
2018-11-01 16:54:55 -0400

    remove support pbf temp files with external element sorting, as they do not work

--- test-files/cmd/slow/SortCmdTest.sh
+++ test-files/cmd/slow/SortCmdTest.sh
@@ -40,50 +40,26 @@ echo ""
 # The element not found warnings are expected here, as during the external sorting the temp files may have partial maps in them.  We just
 # need to be sure the final output matches what is expected
 
-echo "Sorting geonames on disk with xml temp format..."
+echo "Sorting geonames on disk..."
 echo ""
 hoot is-sorted $INPUT_DIR/input.geonames
-hoot sort --warn -D element.sorter.element.buffer.size=$BUFFER_SIZE -D element.sorter.external.temp.format=osm $INPUT_DIR/input.geonames $OUTPUT_DIR/geonames-output-2.osm
+hoot sort --warn -D element.sorter.element.buffer.size=$BUFFER_SIZE $INPUT_DIR/input.geonames $OUTPUT_DIR/geonames-output-2.osm
 hoot is-sorted $OUTPUT_DIR/geonames-output-2.osm
 hoot diff --warn $INPUT_DIR/geonames-output.osm $OUTPUT_DIR/geonames-output-2.osm
 echo ""
 
-echo "Sorting xml on disk with xml temp format..."
+echo "Sorting xml on disk..."
 echo ""
 hoot is-sorted $INPUT_DIR/input.osm
-hoot sort --warn -D element.sorter.element.buffer.size=$BUFFER_SIZE -D element.sorter.external.temp.format=osm $INPUT_DIR/input.osm $OUTPUT_DIR/osm-output-2.osm
+hoot sort --warn -D element.sorter.element.buffer.size=$BUFFER_SIZE $INPUT_DIR/input.osm $OUTPUT_DIR/osm-output-2.osm
 hoot is-sorted $OUTPUT_DIR/osm-output-2.osm
 hoot diff --warn $INPUT_DIR/osm-output.osm $OUTPUT_DIR/osm-output-2.osm
 echo ""
 
-echo "Sorting pbf on disk with xml temp format..."
+echo "Sorting pbf on disk..."
 echo ""
 hoot is-sorted $INPUT_DIR/input.osm.pbf
-hoot sort --warn -D element.sorter.element.buffer.size=$BUFFER_SIZE -D element.sorter.external.temp.format=osm $INPUT_DIR/input.osm.pbf $OUTPUT_DIR/pbf-output-2.osm.pbf
+hoot sort --warn -D element.sorter.element.buffer.size=$BUFFER_SIZE $INPUT_DIR/input.osm.pbf $OUTPUT_DIR/pbf-output-2.osm.pbf
 hoot is-sorted $OUTPUT_DIR/pbf-output-2.osm.pbf
 hoot diff --warn $INPUT_DIR/pbf-output.osm.pbf $OUTPUT_DIR/pbf-output-2.osm.pbf
-echo ""
-
-echo "Sorting geonames on disk with pbf temp format..."
-echo ""
-hoot is-sorted $INPUT_DIR/input.geonames
-hoot sort --warn -D element.sorter.element.buffer.size=$BUFFER_SIZE -D element.sorter.external.temp.format=pbf $INPUT_DIR/input.geonames $OUTPUT_DIR/geonames-output-3.osm
-hoot is-sorted $OUTPUT_DIR/geonames-output-3.osm
-hoot diff --warn $INPUT_DIR/geonames-output.osm $OUTPUT_DIR/geonames-output-3.osm
-echo ""
-
-echo "Sorting xml on disk with pbf temp format..."
-echo ""
-hoot is-sorted $INPUT_DIR/input.osm
-hoot sort --warn -D element.sorter.element.buffer.size=$BUFFER_SIZE -D element.sorter.external.temp.format=pbf $INPUT_DIR/input.osm $OUTPUT_DIR/osm-output-3.osm
-hoot is-sorted $OUTPUT_DIR/osm-output-3.osm
-hoot diff --warn $INPUT_DIR/osm-output.osm $OUTPUT_DIR/osm-output-3.osm
-echo ""
-
-echo "Sorting pbf on disk with pbf temp format..."
-echo ""
-hoot is-sorted $INPUT_DIR/input.osm.pbf
-hoot sort --warn -D element.sorter.element.buffer.size=$BUFFER_SIZE -D element.sorter.external.temp.format=pbf $INPUT_DIR/input.osm.pbf $OUTPUT_DIR/pbf-output-3.osm.pbf
-hoot is-sorted $OUTPUT_DIR/pbf-output-3.osm.pbf
-hoot diff --warn $INPUT_DIR/pbf-output.osm.pbf $OUTPUT_DIR/pbf-output-3.osm.pbf
 echo ""
