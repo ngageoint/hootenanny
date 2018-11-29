434cd5f5551bd7dfff8406fe1c0765c48a06f36d
Brandon Witham <brandon.witham@radiantsolutions.com>
2018-11-29 17:47:39 -0500

    Add option to make rubber sheeting more fault tolerant (#2759)
    * Added option to allow the NodeMatcher to skip over situations where it is trying to calculate an angle at a way node that is neither at the beginning or end of the way; option is enabled by default

--- hoot-core/src/main/cpp/hoot/core/conflate/matching/NodeMatcher.cpp
+++ hoot-core/src/main/cpp/hoot/core/conflate/matching/NodeMatcher.cpp
@@ -63,18 +63,21 @@ vector<Radians> NodeMatcher::calculateAngles(const OsmMap* map, long nid,
   vector<Radians> result;
   result.reserve(wids.size());
 
-  int badSpots = 0;
-
   LOG_VART(nid);
+  QSet<long> badWayIds;
   for (set<long>::const_iterator it = wids.begin(); it != wids.end(); ++it)
   {
     const ConstWayPtr& w = map->getWay(*it);
+    LOG_VART(w->getId());
+    LOG_VART(w->getLastNodeId());
+    LOG_VART(w->getNodeId(0));
 
     if (OsmSchema::getInstance().isLinearHighway(w->getTags(), w->getElementType()) == false &&
         OsmSchema::getInstance().isLinearWaterway(*w) == false &&
-        OsmSchema::getInstance().isPowerLine(*w) == false)
+        OsmSchema::getInstance().isPowerLine(*w) == false &&
+        OsmSchema::getInstance().isRailway(*w) == false)
     {
-      // if this isn't a highway, waterway, or power line then don't consider it.
+      // if this isn't a feature from a specific list, then don't consider it.
       LOG_TRACE("calculateAngles skipping feature...");
     }
     else if (w->getNodeId(0) == nid)
@@ -104,28 +107,37 @@ vector<Radians> NodeMatcher::calculateAngles(const OsmMap* map, long nid,
     }
     else
     {
-      LOG_TRACE("Bad spot");
-      LOG_VART(w->getNodeId(0));
-      LOG_VART(w->getLastNodeId());
       // count this as a bad spot. If we find some valid spots and some bad spots then that is an
       // error condition
-      badSpots++;
+      badWayIds.insert(w->getId());
     }
   }
 
-  if (result.size() > 0 && badSpots > 0)
+  LOG_VART(badWayIds.size());
+  LOG_VART(result.size());
+  if (result.size() > 0 && badWayIds.size() > 0)
   {
-    LOG_TRACE("Found bad spots in NodeMatcher when calculating angles:")
-    LOG_TRACE("nid: " << nid);
-    LOG_VART(map->getNode(nid)->toString());
-    LOG_TRACE("wids: " << wids);
-    for (set<long>::const_iterator it = wids.begin(); it != wids.end(); ++it)
+    LOG_TRACE(
+      "Found " << badWayIds.size() << " bad spot(s) in NodeMatcher when calculating angles " <<
+      "with node: " << nid);
+    LOG_TRACE("wids: " << badWayIds);
+    for (QSet<long>::const_iterator it = badWayIds.begin(); it != badWayIds.end(); ++it)
     {
-      LOG_VART(map->getWay(*it)->toString());
+      const ConstWayPtr& w = map->getWay(*it);
+      LOG_VART(w->getId());
+      LOG_VART(w->getTags().get("REF1"));
+      LOG_VART(w->getTags().get("REF2"));
+      LOG_VART(w->getNodeIndex(nid));
+      LOG_VART(w->getNodeId(0));
+      LOG_VART(w->getLastNodeId());
     }
 
-    throw HootException(
-      "NodeMatcher::calculateAngles was called with a node that was not a start or end node on the specified way.");
+    if (ConfigOptions().getNodeMatcherFailOnBadAngleSpots())
+    {
+      throw HootException(
+        QString("NodeMatcher::calculateAngles was called with a node that was not a start or ") +
+        QString("end node on the specified way."));
+    }
   }
 
   return result;
