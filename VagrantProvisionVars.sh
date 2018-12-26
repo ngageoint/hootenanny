7ef5f5d04e51cad029a3d1de4519d34d380a1365
Justin Bronn <justin.bronn@radiantsolutions.com>
2018-12-26 14:39:37 -0600

    Lock versions of additional libraries: geos, glpk, hoot-gdal, libphonenumber, liboauthcpp and stxxl.

--- VagrantProvisionVars.sh
+++ VagrantProvisionVars.sh
@@ -7,14 +7,19 @@ export JDK_URL=http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165b
 export JDK_TAR=jdk-8u161-linux-x64.tar.gz
 export JDK_MD5=99051574a0d90871ed24a91a5d321ed2
 
+# Dependency library versions.
 export GDAL_VERSION=2.1.4
+export GEOS_VERSION=3.6.2
+export GLPK_VERSION=4.64
+export LIBOAUTHCPP_VERSION=0.1.0
+export LIBPHONENUMBER_VERSION=8.9.16
+export NODE_VERSION=8.9.3
+export STXXL_VERSION=1.3.1
 
 # FGDB 1.5 is required to compile using g++ >= 5.1
 # https://trac.osgeo.org/gdal/wiki/FileGDB#HowtodealwithGCC5.1C11ABIonLinux
 export FGDB_VERSION=1.5.1
 export FGDB_URL=https://github.com/Esri/file-geodatabase-api/raw/master/FileGDB_API_${FGDB_VERSION}/
-
-export NODE_VERSION=8.9.3
 
 # Ruby versions and locations.
 export BUNDLER_VERSION=1.16.1
@@ -25,5 +30,3 @@ export RVM_BASE_URL=https://github.com/rvm/rvm
 export RVM_BINARIES_URL="${RUBY_BASE_URL}/binaries"
 export RVM_HOME="${RVM_HOME:-${HOME}/.rvm}"
 export RVM_VERSION=1.29.4
-
-export GLPK_VERSION=4.64
