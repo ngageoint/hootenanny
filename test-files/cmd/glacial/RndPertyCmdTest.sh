0209add4ee7ff724b325b9ebb90852770443c4ea
Brandon Witham <brandon.witham@radiantsolutions.com>
2019-01-23 10:57:11 -0600

    Move rnd tests to glacial (#2879)

--- test-files/cmd/glacial/RndPertyCmdTest.sh
+++ test-files/cmd/glacial/RndPertyCmdTest.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-mkdir -p $HOOT_HOME/test-output/cmd/quick/PertyCmdTest
+mkdir -p $HOOT_HOME/test-output/cmd/glacial/RndPertyCmdTest
 
 # Compare to known-good
 inputfile=$HOOT_HOME/test-files/DcGisRoads.osm
-testfile=$HOOT_HOME/test-files/cmd/quick/PertyCmdTest/p_out.osm
-outputfile=$HOOT_HOME/test-output/cmd/quick/PertyCmdTest/p_out.osm
+testfile=$HOOT_HOME/test-files/cmd/glacial/RndPertyCmdTest/p_out.osm
+outputfile=$HOOT_HOME/test-output/cmd/glacial/RndPertyCmdTest/p_out.osm
 
 # Run the command. Note that we sanitize output to remove 
 # number of nodes reprojected, because this is variable
@@ -18,5 +18,5 @@ hoot perty -D perty.search.distance=20 \
 
 
 # Checking for a match is not a good idea, because perturbation is random
-# TODO: use the random seeing feature in perty, then we can check for the match
+# TODO: use the random seeding feature in perty, then we can check for the match
 # hoot diff $testfile $outputfile || diff $testfile $outputfile
