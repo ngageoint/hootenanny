77ce0dfd98b184c70d7df481099c223a74430196
Brian Hatchl <brian.hatchl@radiantsolutions.com>
2019-02-14 13:43:19 -0500

    Add user id of owner to folder and map layer responses (#2960)
    * add user id to folder and map layers resp
    
    * don't generate buildInfo for hoot2x
    
    * update copyright header

--- scripts/GenerateBuildInfoFiles.sh
+++ scripts/GenerateBuildInfoFiles.sh
@@ -8,10 +8,8 @@ SERVICES_BUILD_INFO_FILE=./src/main/resources/build.info
 echo "name=Hootenanny Web Services" > $SERVICES_BUILD_INFO_FILE
 echo "version="$HOOT_VERSION >> $SERVICES_BUILD_INFO_FILE
 echo "user="$USER >> $SERVICES_BUILD_INFO_FILE
-  
+
 # assume that if we're building services, we're also building iD, since iD doesn't have its
 # own makefile
 UI_BUILD_INFO_FILE=../hoot-ui/data/buildInfo.json
-UI2X_BUILD_INFO_FILE=../hoot-ui-2x/modules/Hoot/config/buildInfo.json
 echo "{ \"name\": \"Hootenanny iD Editor\", \"version\": \"$HOOT_VERSION\", \"user\": \"$USER\" }" > $UI_BUILD_INFO_FILE
-echo "{ \"name\": \"Hootenanny iD Editor\", \"version\": \"$HOOT_VERSION\", \"user\": \"$USER\" }" > $UI2X_BUILD_INFO_FILE
