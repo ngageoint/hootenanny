b538e2c862c544cb65693ad165466a94fd022257
Brandon Witham <brandon.witham@radiantsolutions.com>
2019-02-27 15:41:44 -0500

    cleanup

--- hoot-core/src/main/cpp/hoot/core/criterion/ReviewScoreCriterion.cpp
+++ hoot-core/src/main/cpp/hoot/core/criterion/ReviewScoreCriterion.cpp
@@ -63,41 +63,14 @@ bool ReviewScoreCriterion::isSatisfied(const ConstElementPtr& e) const
     bool ok = false;
     const double score = e->getTags().get(MetadataTags::HootReviewScore()).toDouble(&ok);
     LOG_VART(score);
+
     if (!ok || score > 1.0 || score < 0.0)
     {
       LOG_TRACE("Skipping invalid score: " << score << " for: " << e->getElementId() << "...");
-//      if (_invertThresholding)
-//      {
-//        LOG_DEBUG("Passed filter.");
-//        return true;
-//      }
-//      else
-//      {
-//        LOG_DEBUG("Did not pass filter.");
-//        return false;
-//      }
       return _invertThresholding ? true : false;
     }
+
     LOG_TRACE("Filtering review: " << e->getElementId() << "...");
-//    if (_invertThresholding &&
-//        ((_minScoreThreshold == 0.0 && _maxScoreThreshold == 1.0) ||
-//         (_minScoreThreshold == 0.0 && score == 0.0) ||
-//         (_maxScoreThreshold == 1.0 && score == 1.0) ||
-//         score < _minScoreThreshold || score > _maxScoreThreshold))
-//    {
-//      LOG_DEBUG("Passed filter.");
-//      return true;
-//    }
-//    else if (!_invertThresholding && (score >= _minScoreThreshold && score <= _maxScoreThreshold))
-//    {
-//      LOG_DEBUG("Passed filter.");
-//      return true;
-//    }
-//    else
-//    {
-//      LOG_DEBUG("Did not pass filter.");
-//      return false;
-//    }
     return
       _invertThresholding ?
         // inverted; handle boundary conditions
