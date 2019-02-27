f206db2cffba1844288c19adc348db4c98378060
Ben Marchant <13385275+bmarchant@users.noreply.github.com>
2019-02-27 16:59:07 -0600

    Create conflation .conf files (#2977)
    * Adding Attribute, Horizontal, and Reference conflation configuration files, just like Network and Diff.
    * Changed names and contents of config files.  Updated tests and documentation.
    * Added warning to test output that has changed.

--- test-files/cmd/glacial/NonContiguousRoadReviewsTest.sh
+++ test-files/cmd/glacial/NonContiguousRoadReviewsTest.sh
@@ -6,5 +6,12 @@ set -e
 mkdir -p $HOOT_HOME/tmp/
 mkdir -p test-output/cmd/NonContiguousRoadReviewsTest
 
-hoot conflate --warn -C Network.conf test-files/Congo_MGCP_Roads_Bridges_subset.osm test-files/Congo_OSM_Roads_Bridges_subset.osm test-output/cmd/NonContiguousRoadReviewsTest/output.osm
-hoot diff test-output/cmd/NonContiguousRoadReviewsTest/output.osm test-files/cmd/glacial/NonContiguousRoadReviewsTest/output.osm || diff test-output/cmd/NonContiguousRoadReviewsTest/output.osm test-files/cmd/glacial/NonContiguousRoadReviewsTest/output.osm 
+hoot conflate --warn -C ReferenceConflation.conf -C NetworkAlgorithm.conf \
+ test-files/Congo_MGCP_Roads_Bridges_subset.osm \
+ test-files/Congo_OSM_Roads_Bridges_subset.osm \
+ test-output/cmd/NonContiguousRoadReviewsTest/output.osm
+
+hoot diff test-output/cmd/NonContiguousRoadReviewsTest/output.osm \
+          test-files/cmd/glacial/NonContiguousRoadReviewsTest/output.osm || \
+     diff test-output/cmd/NonContiguousRoadReviewsTest/output.osm \
+          test-files/cmd/glacial/NonContiguousRoadReviewsTest/output.osm
