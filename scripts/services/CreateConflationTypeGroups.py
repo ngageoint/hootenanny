25e9a9b2639eb8b126cb2a33de54db6e1c648aac
Max Grossman <m.grossman1813@gmail.com>
2019-03-07 14:29:52 -0500

    update script to add input property, get template defaults, and allow for injecting non-option ui objects
    ref #3028

--- scripts/services/CreateConflationTypeGroups.py
+++ scripts/services/CreateConflationTypeGroups.py
@@ -7,18 +7,35 @@ Creates a JSON file of configuration options used by UI
 import sys
 import json
 import os
+import re
 
-if (len(sys.argv) != 4):
+if (len(sys.argv) != 5):
     print "Usage:"
     print sys.argv[0] + " (config file) (output file)"
     sys.exit(-1)
 
 fn = sys.argv[1]
 parent = sys.argv[2]
-headerFn = sys.argv[3]
+defaults = sys.argv[3]
+headerFn = sys.argv[4]
 
 uiGroups = json.loads("".join(open(fn).readlines()))
 configOptions = json.loads("".join(open(parent).readlines()))
+groupDefaults = json.loads("".join(open(defaults).readlines()))
+
+optionTypesMap = {
+    'text': ['double', 'int', 'string', 'long'],
+    'combo': ['list'],
+    'check': ['bool']
+}
+
+def toInput(optionType):
+    matchingInput = ''
+    for inputType, options in optionTypesMap.items():
+        if optionType in options:
+            matchingInput = inputType
+            break;
+    return matchingInput
 
  # merges matches between ConfigOptions.json and conflationTypesMap.json
  # on command key back into conflationTypesMap. Outputs as ConflationTypesGroups.json
@@ -27,20 +44,32 @@ def createUiJSON(groups, options):
         configMembers = [{'key': key, 'member': member} for key, member in options.items()]
         for conflateType, conflateConfig in groups.items():
             members = []
-            for index, member in enumerate(conflateConfig['members']):
-                match = next((m['key'] for m in configMembers if m['member']['key'] == member['key']), None)
+            typeMembers = conflateConfig['members']
+            for flag in typeMembers.keys():
+                match = next(
+                    (m['key'] for m in configMembers if m['member']['key'] == flag), 
+                    None
+                )
                 if match != None:
-                    member.update(configOptions[match])
-                    member['id'] = match
-                    del member['key']
-                    # add logic to get default for token if like ${}
-                    # handle edge case for road engine
-                    # add some logic to get input from type...
-                    members.append(member)
+                    memberConfig = { 'label': typeMembers[flag], 'id': flag }
+                    memberConfig.update(configOptions[match])
+                    del memberConfig['key']
+                    templateDefault = re.findall(r'(?<=\${).*(?=})', memberConfig['default'])
+                    if len(templateDefault) == 1:
+                        defaultKey = templateDefault[0]
+                        memberConfig['default'] = next(
+                            (m['member']['default'] for m in configMembers if m['member']['key'] == defaultKey),
+                            ''
+                        )
+                    memberConfig['input'] = toInput(memberConfig['type'])
+                    members.append(memberConfig)
+
+            if (conflateType == 'Roads'):
+                members = groupDefaults[conflateType] + members
 
             groups[conflateType] = members
     except:
-        print('failed to load %s' % fn2)
+        print 'failed to load %s' % fn2
     finally:
         return json.dumps(groups)
 
