e15d364bc31f7c7835022f65e72094dc2edf10f6
Matthias Schill <matthias1231@protonmail.com>
2019-03-11 12:15:20 -0600

    Test file conf reference fix, missed earlier

--- test-files/cmd/slow/RubberSheetCmdTest.sh
+++ test-files/cmd/slow/RubberSheetCmdTest.sh
@@ -9,7 +9,7 @@ outputfile=$HOOT_HOME/test-output/cmd/slow/RubberSheetCmdTest/out.osm
 comparefile=$HOOT_HOME/test-files/cmd/slow/RubberSheetCmdTest/out.osm
 
 # Run the command.
-hoot rubber-sheet -D hash.seed.zero=true $input1 $input2 $outputfile | \
+hoot rubber-sheet -C Testing.conf $input1 $input2 $outputfile | \
            sed "s/RMSE: [0-9]\+\.[0-9]\+//g"
 
 # Check output
