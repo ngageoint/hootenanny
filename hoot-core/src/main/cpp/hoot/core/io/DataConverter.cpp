97c3907aea520b7f7cd514fb155cd9b0258572a9
Brandon Witham <brandon.witham@radiantsolutions.com>
2019-03-15 15:50:28 -0400

    3062 to develop (#3063)
    * In ogr reader skip polys that cant close their exterior rings with a warning

--- hoot-core/src/main/cpp/hoot/core/io/DataConverter.cpp
+++ hoot-core/src/main/cpp/hoot/core/io/DataConverter.cpp
@@ -486,13 +486,13 @@ void DataConverter::_convertFromOgr(const QStringList inputs, const QString outp
     // read each layer's data
     for (int i = 0; i < layers.size(); i++)
     {
-      PROGRESS_INFO("Read layer " << i + 1 << " of " << layers.size());
+      PROGRESS_INFO("Finished reading layer " << i + 1 << " of " << layers.size());
       LOG_VART(input);
       LOG_VART(layers[i]);
       progress.setTaskWeight(progressWeights[i]);
       reader.read(input, layers[i], map, progress);
     }
-    LOG_INFO("Read layer " << layers.size() << " of " << layers.size());
+    LOG_INFO("Finished reading layer " << layers.size() << " of " << layers.size());
   }
 
   if (map->getNodes().size() == 0)
