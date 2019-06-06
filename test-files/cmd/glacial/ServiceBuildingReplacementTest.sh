7a4f04e5517a0004dab1cbc144568604051be928
Brandon Witham <brandon.witham@radiantsolutions.com>
2019-06-06 16:59:04 -0400

    fix changeset application

--- test-files/cmd/glacial/ServiceBuildingReplacementTest.sh
+++ test-files/cmd/glacial/ServiceBuildingReplacementTest.sh
@@ -22,20 +22,6 @@ REF_LAYER=$OSM_API_DB_URL
 SEC_LAYER_FILE=test-files/BostonSubsetRoadBuilding_FromOsm.osm
 SEC_LAYER="$HOOT_DB_URL/$TEST_NAME-sec"
 
-# Additional settings that may end up being useful for: 
-
-# debugging:
-#-D writer.include.debug.tags=true
-
-# tweaking tag reading/writing behavior:
-#-D reader.preserve.all.tags=true -D reader.use.file.status=true -D reader.keep.status.tag=true
-
-# tweaking cookie cutting behavior:
-#-D cookie.cutter.alpha=1000 -D cookie.cutter.alpha.shape.buffer=0.0
-
-# tweaking changeset derivation behavior:
-#-D changeset.allow.deleting.reference.features=true -D changeset.buffer=0.0
-
 # CONFIG OPTS
 
 # opts to apply to all the commands; changeset.xml.writer.add.timestamp=false and reader.add.source.datetime=false are for testing 
@@ -45,11 +31,21 @@ GENERAL_OPTS="--warn -D uuid.helper.repeatable=true -D changeset.xml.writer.add.
 # opts to apply to commands involved in db i/o
 DB_OPTS="-D api.db.email=OsmApiDbHootApiDbConflate@hoottestcpp.org -D hootapi.db.writer.create.user=true -D hootapi.db.writer.overwrite.map=true"
 # perturbation opts; We just want a small amount of noticeable shift here and none of the other non-shift destructive perty ops.
-PERTY_OPTS="-D perty.seed=1 -D perty.systematic.error.x=10 -D perty.systematic.error.y=10 -D perty.ops="
+PERTY_OPTS="-D perty.seed=1 -D perty.systematic.error.x=15 -D perty.systematic.error.y=15 -D perty.ops="
 # changeset derivation opts; I've chosen to leave conflation (hoot::UnifyingConflator) out as the last op in convert.ops, since it hasn't 
 # been needed yet to make the building output look good...it may be needed at some point, though, and would likely be needed with features 
 # like roads. The RemoveElementsVisitor is set up to keep only buildings.
 CHANGESET_DERIVE_OPTS="-D changeset.user.id=1 -D convert.bounding.box=-71.4698,42.4866,-71.4657,42.4902 -D convert.ops=hoot::RemoveElementsVisitor;hoot::CookieCutterOp -D remove.elements.visitor.element.criteria=hoot::BuildingCriterion -D remove.elements.visitor.recursive=true -D element.criterion.negate=true"
+
+# Additional config opts that may end up being useful for: 
+# debugging:
+#-D writer.include.debug.tags=true
+# tweaking tag reading/writing behavior:
+#-D reader.preserve.all.tags=true -D reader.use.file.status=true -D reader.keep.status.tag=true
+# tweaking cookie cutting behavior:
+#-D cookie.cutter.alpha=1000 -D cookie.cutter.alpha.shape.buffer=0.0
+# tweaking changeset derivation behavior:
+#-D changeset.allow.deleting.reference.features=true -D changeset.buffer=0.0
 
 # DATA PREP
 
@@ -63,7 +59,7 @@ CHANGESET_DERIVE_OPTS="-D changeset.user.id=1 -D convert.bounding.box=-71.4698,4
 echo ""
 echo "Writing the reference dataset to an osm api db (contains features to be replaced)..."
 echo ""
-hoot convert $GENERAL_OPTS $PERTY_OPTS -D reader.use.data.source.ids=false -D convert.ops=hoot::PertyOp $SEC_LAYER_FILE $REF_LAYER_FILE 
+hoot convert $GENERAL_OPTS $PERTY_OPTS -D reader.use.data.source.ids=false -D id.generator=hoot::PositiveIdGenerator -D convert.ops=hoot::PertyOp $SEC_LAYER_FILE $REF_LAYER_FILE 
 scripts/database/CleanAndInitializeOsmApiDb.sh 
 hoot convert $GENERAL_OPTS $DB_OPTS -D changeset.user.id=1 -D reader.use.data.source.ids=true $REF_LAYER_FILE $REF_LAYER
 # Uncomment to see what the ref layer looks like in file form:
@@ -77,7 +73,7 @@ hoot convert $GENERAL_OPTS $DB_OPTS -D changeset.user.id=1 -D reader.use.data.so
 echo ""
 echo "Writing the secondary dataset to a hoot api db (contains features to replace with)..."
 echo ""
-hoot convert $GENERAL_OPTS $DB_OPTS -D reader.use.data.source.ids=false $SEC_LAYER_FILE $SEC_LAYER
+hoot convert $GENERAL_OPTS $DB_OPTS -D reader.use.data.source.ids=true -D id.generator=hoot::PositiveIdGenerator $SEC_LAYER_FILE $SEC_LAYER
 # TODO: For some strange reason this file output data is corrupted unless which layers IDs get element IDs preserved gets swapped
 # as described in the comments. So swap the values of reader.use.data.source.ids in the two convert commands that load db data if you want 
 # to see this output..will figure out what's going on  later.
@@ -102,26 +98,25 @@ echo ""
 hoot changeset-derive $GENERAL_OPTS $DB_OPTS $CHANGESET_DERIVE_OPTS $REF_LAYER $SEC_LAYER $OUT_DIR/$TEST_NAME-changeset-2.osc
 diff $IN_DIR/$TEST_NAME-changeset-2.osc $OUT_DIR/$TEST_NAME-changeset-2.osc
 # Go ahead and spit out a sql changeset in the same manner the XML changeset was generated...we'll need it for the next step.
-#echo ""
-#echo $CHANGESET_DERIVATION_MSG " (hoot api db secondary source; sql changeset out)..."
-#echo ""
-#hoot changeset-derive $GENERAL_OPTS $DB_OPTS $CHANGESET_DERIVE_OPTS $REF_LAYER $SEC_LAYER $OUT_DIR/$TEST_NAME-changeset-2.osc.sql $REF_LAYER
-# TODO add diff check
+echo ""
+echo $CHANGESET_DERIVATION_MSG " (hoot api db secondary source; sql changeset out)..."
+echo ""
+hoot changeset-derive $GENERAL_OPTS $DB_OPTS $CHANGESET_DERIVE_OPTS $REF_LAYER $SEC_LAYER $OUT_DIR/$TEST_NAME-changeset-2.osc.sql $REF_LAYER
+diff $IN_DIR/$TEST_NAME-changeset-2.osc.sql $OUT_DIR/$TEST_NAME-changeset-2.osc.sql
 
 # CHANGESET APPLICATION
 
 # Write the SQL changeset back to the ref db. We're using the SQL changeset here instead of the XML b/c that's the only kind we can write 
 # w/o a Rails Port in the hoot stack (which, if added, would only overly complicate hoot testing)...the effect is the same.
-#echo ""
-#echo "Applying the changeset to the reference data..."
-#echo ""
-#hoot changeset-apply $GENERAL_OPTS $DB_OPTS $CHANGESET_DERIVE_OPTS $OUT_DIR/$TEST_NAME-changeset-2.osc.sql $OSM_API_DB_URL
-#echo ""
-#echo "Reading the entire reference dataset out for verification..."
-#echo ""
-#hoot convert $GENERAL_OPTS $DB_OPTS $OSM_API_DB_URL $OUT_DIR/$TEST_NAME-conflated.osm
-# TODO:
-#hoot diff $GENERAL_OPTS $IN_DIR/$TEST_NAME-conflated.osm $OUT_DIR/$TEST_NAME-conflated.osm
+echo ""
+echo "Applying the changeset to the reference data..."
+echo ""
+hoot changeset-apply $GENERAL_OPTS $DB_OPTS $CHANGESET_DERIVE_OPTS $OUT_DIR/$TEST_NAME-changeset-2.osc.sql $OSM_API_DB_URL
+echo ""
+echo "Reading the entire reference dataset out for verification..."
+echo ""
+hoot convert $GENERAL_OPTS $DB_OPTS $OSM_API_DB_URL $OUT_DIR/$TEST_NAME-replaced.osm
+hoot diff $GENERAL_OPTS $IN_DIR/$TEST_NAME-replaced.osm $OUT_DIR/$TEST_NAME-replaced.osm
 
 # cleanup
 scripts/database/CleanOsmApiDB.sh 
