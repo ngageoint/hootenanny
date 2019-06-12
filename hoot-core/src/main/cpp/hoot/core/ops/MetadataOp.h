8c0fd81769ab5ad1eda7fc04acf6ab4515e92690
Matthias Schill <matthias1231@protonmail.com>
2019-06-12 09:51:54 -0600

    Framework for metadata ops

---
+++ hoot-core/src/main/cpp/hoot/core/ops/MetadataOp.h
@@ -0,0 +1,99 @@
+/*
+ * This file is part of Hootenanny.
+ *
+ * Hootenanny is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * --------------------------------------------------------------------
+ *
+ * The following copyright notices are generated automatically. If you
+ * have a new notice to add, please use the format:
+ * " * @copyright Copyright ..."
+ * This will properly maintain the copyright information. DigitalGlobe
+ * copyrights will be updated automatically.
+ *
+ * @copyright Copyright (C) 2019 DigitalGlobe (http://www.digitalglobe.com/)
+ */
+
+#ifndef METADATAOP_H
+#define METADATAOP_H
+
+// Hoot
+#include <hoot/core/info/OperationStatusInfo.h>
+#include <hoot/core/ops/OsmMapOperation.h>
+#include <hoot/core/util/ConfigOptions.h>
+#include <hoot/core/util/Configurable.h>
+
+// Qt
+#include <QString>
+#include <QPair>
+
+namespace hoot
+{
+
+class MetadataOp : public OsmMapOperation, public OperationStatusInfo, public Configurable
+{
+public:
+
+  static std::string className() { return "hoot::MetadataOp"; }
+  MetadataOp() : _pConf(&conf()) {}
+
+  // OsmMapOperation (partial)
+  virtual void apply(std::shared_ptr<OsmMap>& map) override
+  {
+    _map = map;
+    _configure();
+    _process();
+  }
+
+  // OperationStatusInfo
+  virtual QString getInitStatusMessage() const { return "Processing metadata..."; }
+  virtual QString getCompletedStatusMessage() const { return "Modified " + QString::number(_numAffected) + " elements"; }
+
+  // Configurable
+  virtual void setConfiguration(const Settings& conf) {
+    LOG_INFO( "MetadataOp setConfiguration");_pConf = &conf; }
+
+private:
+
+  const Settings* _pConf;
+  std::shared_ptr<OsmMap> _map;
+  QPair<QString,QString> _datasetIndicator;
+  QHash<QString,QString> _tags;
+
+  virtual void _process() = 0;
+
+  void _configure()
+  {
+    ConfigOptions opts = ConfigOptions(*_pConf);
+    QStringList indicator = opts.getMetadataDatasetIndicatorTag();
+    QStringList tags = opts.getMetadataTags();
+
+    if (indicator.length() > 1)
+    {
+      _datasetIndicator.first = indicator[0];
+      _datasetIndicator.second = indicator[1];
+    }
+
+    for (int i = 0; i < tags.length(); i+=2)
+    {
+      QString key = tags[i];
+      QString value = (i < tags.length()-1) ? tags[i+1] : "";
+      _tags[key] = value;
+    }
+  }
+};
+
+}
+
+#endif // METADATAOP_H
