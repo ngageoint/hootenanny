f91d916a3477ca6bca40667f4858f4a219690419
Matthias Schill <33227411+matthias1231@users.noreply.github.com>
2019-06-21 10:17:23 -0600

    Metadata import and export (#3299)
    * Framework for metadata ops
    
    * Metadata import, poly merging, work in progress
    
    * Import, wip
    
    * Refactored import code, assignment of metadata to nodes
    
    * Import for ways and relations
    
    * Refactoring/simplification
    
    * Finished import, added trace log and class commentary
    
    * Metadata export grid, some refactoring
    
    * Export metadata tags to dataset grid, fixes, stats
    
    * Metadata export work
    
    - Moved cell size to config
    - allow for 0 cell size to indicate using source data bounds for dataset
    - split source tags containing multiple entries and collect all possible tag values in metadata tag
    
    * Remove dataset elements after import
    
    * Cleanup and commentary
    
    * On export remove grid datasets that contain no data
    
    * Unit tests for MetadataOps

---
+++ hoot-core/src/main/cpp/hoot/core/ops/MetadataImport.h
@@ -0,0 +1,98 @@
+/*
+ * This file is part of Hootenanny.
+ *
+ * Hootenanny is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * --------------------------------------------------------------------
+ *
+ * The following copyright notices are generated automatically. If you
+ * have a new notice to add, please use the format:
+ * " * @copyright Copyright ..."
+ * This will properly maintain the copyright information. DigitalGlobe
+ * copyrights will be updated automatically.
+ *
+ * @copyright Copyright (C) 2019 DigitalGlobe (http://www.digitalglobe.com/)
+ */
+
+#ifndef METADATAIMPORT_H
+#define METADATAIMPORT_H
+
+// Hoot
+#include <hoot/core/ops/MetadataOp.h>
+
+// geos
+
+
+namespace hoot
+{
+
+/*
+ * MetadataImport imports a specific set of tags from one as 'dataset' specified way to all
+ * elements containd in it.
+ *
+ * A dataset is defined by the tag/value pair defined in the 'metadata.dataset.indicator.tag'
+ * configuration variable. The key/value pair defines the tag and value expected from the
+ * element that is to be considered the dataset (called DatasetTag).
+ *
+ * The specific set of tags and their default values are defined in the 'metadata.tags'
+ * configuration variable. The list of key/value pairs specifies the tags that are to be
+ * considered for import from the dataset element and their default values (called MetadataTags).
+ *
+ * Process:
+ * - Find all ways marked as datasets (meaning they contain the matching DatasetTag) and convert
+ *   them to geos::geom::Polygons.
+ * - Merge all resulting Polygons whose MetadataTags values are identical into one or more
+ *   DatasetPolygons.
+ * - Find all (non-dataset) way and node elements which have one or more non-debug tags and check
+ *   if they are contained in the DatasetPolygon. If an element is overlapping more than one
+ *   DatasetPolygon assign it to the one that contains the greatest number of its nodes. This way
+ *   processing is basically the same for all element types.
+ * - Copy all tags and values defined in the MetadataTags list from the DatasetPolygon to the
+ *   element. If one or more MetadataTags are not specified in the DatasetPolygon assign it using
+ *   its default value as specified in the ConfigOptions.
+ */
+class MetadataImport : public MetadataOp
+{
+public:
+
+  static std::string className() { return "hoot::MetadataImport"; }
+
+  // OsmMapOperation
+  virtual QString getDescription() const override { return "Imports metadata"; }
+
+private:
+
+  // MetadataOp
+  virtual void _apply();
+
+  // process sequence functions
+
+  // finds all way polygons that are marked as datasets
+  void _findDatasetWays();
+
+  // merge all geos geometries/polygons of datasets which have the exact same Metadata tags
+  // and values
+  void _mergePolygonsWithMatchingMetadata();
+
+  // copies all metadata tags from the dataset to the elements
+  void _importMetadataToElements();
+
+  // helper functions
+  bool _areMetadataTagsEqual(ElementPtr p1, ElementPtr p2);
+  bool _applyToElement(ElementPtr pElement);
+};
+
+}
+
+#endif // METADATAIMPORT_H
