56fc6953f2cd638fbf5232acab925201f831002a
Brandon Witham <brandon.witham@maxar.com>
2019-08-26 11:32:31 -0400

    fix scripts

---
+++ test-files/cmd/glacial/serial/ServiceChangesetReplacementAdditionalFiltersChainedTest.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+
+test-files/cmd/glacial/serial/ServiceChangesetReplacement.sh.off "ServiceChangesetReplacementAdditionalFiltersChainedTest" "test-files/cmd/glacial/PoiPolygonConflateStandaloneTest/PoiPolygon1.osm" "test-files/cmd/glacial/PoiPolygonConflateStandaloneTest/PoiPolygon2.osm" "-122.43204,37.7628,-122.4303457,37.76437" "false" "hoot::PoiCriterion" "hoot::TagContainsCriterion;hoot::TagCriterion" "true" '-D tag.criterion.kvps="amenity=cafe" -D tag.contains.criterion.kvps="name=Java"' "xml"
---
+++ test-files/cmd/glacial/serial/ServiceChangesetReplacementAdditionalFiltersTest.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+
+test-files/cmd/glacial/serial/ServiceChangesetReplacement.sh.off "ServiceChangesetReplacementAdditionalFiltersTest" "test-files/cmd/glacial/PoiPolygonConflateStandaloneTest/PoiPolygon1.osm" "test-files/cmd/glacial/PoiPolygonConflateStandaloneTest/PoiPolygon2.osm" "-122.43204,37.7628,-122.4303457,37.76437" "false" "hoot::PoiCriterion" "hoot::TagCriterion;hoot::TagValueNumericRangeCriterion" "false" '-D tag.criterion.kvps="name=Sweet Inspiration Bakery" -D tag.value.numeric.range.criterion.keys=addr:housenumber tag.value.numeric.range.criterion.min=2257 -D tag.value.numeric.range.criterion.max=2257' "xml"
