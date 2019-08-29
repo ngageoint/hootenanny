5384a83de60f9362b7b547c2b38dfead4cff76ef
Brandon Witham <brandon.witham@maxar.com>
2019-08-29 11:15:15 -0400

    fix test

--- test-files/cmd/glacial/serial/ServiceChangesetReplacementInputFiltersChainedTest.sh
+++ test-files/cmd/glacial/serial/ServiceChangesetReplacementInputFiltersChainedTest.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
-# replace POIs in the reference dataset that overlap with schools having "Preschool" in the name from the secondary dataset (AND the filter 
-# criterion together)
+# Replace POIs in the reference dataset that overlap with schools having "Preschool" in the name from the secondary dataset (AND the filter 
+# criterion together).
+#
+# - Should see TODO
+
 test-files/cmd/glacial/serial/ServiceChangesetReplacement.sh.off "ServiceChangesetReplacementInputFiltersChainedTest" "test-files/cmd/glacial/PoiPolygonConflateStandaloneTest/PoiPolygon1.osm" "test-files/cmd/glacial/PoiPolygonConflateStandaloneTest/PoiPolygon2.osm" "-122.43204,37.7628,-122.4303457,37.76437" "true" "hoot::PoiCriterion" "hoot::TagCriterion;hoot::TagContainsCriterion" "true" "tag.criterion.kvps='amenity=school';tag.contains.criterion.kvps='name=Preschool'" "xml"
--- test-files/cmd/glacial/serial/ServiceChangesetReplacementInputFiltersTest.sh
+++ test-files/cmd/glacial/serial/ServiceChangesetReplacementInputFiltersTest.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
 
-# replace POIs in the reference dataset that overlap with schools and POIs having "LIBRARY" in the name from the secondary dataset (OR the filter criterion together)
+# Replace POIs in the reference dataset that overlap with schools and POIs having "LIBRARY" in the name from the secondary dataset (OR the 
+# filter criterion together). 
+#
+# - Should see "Sanchez Elementary School" in the reference data completely replaced by "Sanchez Elementary School" and "Sanchez Preschool" in 
+# the secondary data (not conflated), since "Sanchez Elementary School" in the ref data overlaps with those two schools in the sec data. 
+# - Should see "Eureka Valley-Harvey Milk Memorial Branch Library" in the reference data be conflated with "EUREKA VALLEY BRANCH LIBRARY" since
+# the ref feature lies just outside of the the secondary dataset alpha shape.
+
 test-files/cmd/glacial/serial/ServiceChangesetReplacement.sh.off "ServiceChangesetReplacementInputFiltersTest" "test-files/cmd/glacial/PoiPolygonConflateStandaloneTest/PoiPolygon1.osm" "test-files/cmd/glacial/PoiPolygonConflateStandaloneTest/PoiPolygon2.osm" "-122.43204,37.7628,-122.4303457,37.76437" "true" "hoot::PoiCriterion" "hoot::TagCriterion;hoot::TagContainsCriterion" "false" "tag.criterion.kvps='amenity=school';tag.contains.criterion.kvps='name=LIBRARY'" "xml"
