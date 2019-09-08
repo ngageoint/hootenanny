32ac65544678780d90af890ff5c9bef0743c6bec
Brandon Witham <303724+bwitham@users.noreply.github.com>
2019-09-08 09:47:12 -0400

    Reduce input data sizes for changeset replacement tests (#3454)
    cuts test runtime in half

--- test-files/cmd/glacial/serial/ServiceChangesetReplacementAdditionalFiltersTest.sh
+++ test-files/cmd/glacial/serial/ServiceChangesetReplacementAdditionalFiltersTest.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+# This tests replacement changeset generation when filters are imposed on the data being used to replace that are not chained together.
+#
 # Replace schools and POIs with "Library" in the name in the reference dataset with schools and POIs having "LIBRARY" in the name from the 
 # secondary dataset (no chaining for filters: OR the replacement filter criteria together; OR the retainment filter criteria together). 
 #
@@ -10,4 +12,4 @@ set -e
 #   since the two overlap with each other.
 # - Nothing else in the reference dataset should be modified or removed. Nothing else from the secondary dataset should have been added.
 
-test-files/cmd/glacial/serial/ServiceChangesetReplacement.sh.off "ServiceChangesetReplacementAdditionalFiltersTest" "test-files/cmd/glacial/PoiPolygonConflateStandaloneTest/PoiPolygon1.osm" "test-files/cmd/glacial/PoiPolygonConflateStandaloneTest/PoiPolygon2.osm" "-122.43204,37.7628,-122.4303457,37.76437" "false" "true" "hoot::PoiCriterion" "hoot::TagCriterion;hoot::TagContainsCriterion" "false" "tag.criterion.kvps='amenity=school';tag.contains.criterion.kvps='name=LIBRARY'" "hoot::TagCriterion;hoot::TagContainsCriterion" "false" "tag.criterion.kvps='amenity=school';tag.contains.criterion.kvps='name=School'" "xml"
+test-files/cmd/glacial/serial/ServiceChangesetReplacement.sh.off "ServiceChangesetReplacementAdditionalFiltersTest" "test-files/cmd/glacial/PoiPolygonConflateStandaloneTest/PoiPolygon1.osm" "test-files/cmd/glacial/PoiPolygonConflateStandaloneTest/PoiPolygon2.osm" "-122.43204,37.7628,-122.4303457,37.76437" "-122.43384,37.76069,-122.42742,37.76869" "false" "true" "hoot::PoiCriterion" "hoot::TagCriterion;hoot::TagContainsCriterion" "false" "tag.criterion.kvps='amenity=school';tag.contains.criterion.kvps='name=LIBRARY'" "hoot::TagCriterion;hoot::TagContainsCriterion" "false" "tag.criterion.kvps='amenity=school';tag.contains.criterion.kvps='name=School'" "xml"
