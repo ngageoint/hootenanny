d14fe7df570809681d377212c0db308dae2473b3
Ben Marchant <13385275+bmarchant@users.noreply.github.com>
2019-11-04 14:46:29 -0600

    Split OsmMap objects into smaller maps using tiles (#3602)
    * Updated elements to use virtual envelope functions
    * Created split-map command

---
+++ hoot-core/src/main/cpp/hoot/core/cmd/SplitMapCmd.cpp
@@ -0,0 +1,83 @@
+/*
+ * This file is part of Hootenanny.
+ *
+ * Hootenanny is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * --------------------------------------------------------------------
+ *
+ * The following copyright notices are generated automatically. If you
+ * have a new notice to add, please use the format:
+ * " * @copyright Copyright ..."
+ * This will properly maintain the copyright information. DigitalGlobe
+ * copyrights will be updated automatically.
+ *
+ * @copyright Copyright (C) 2019 DigitalGlobe (http://www.digitalglobe.com/)
+ */
+
+//  Hoot
+#include <hoot/core/algorithms/splitter/OsmMapSplitter.h>
+#include <hoot/core/cmd/BaseCommand.h>
+#include <hoot/core/io/IoUtils.h>
+#include <hoot/core/util/Factory.h>
+
+using namespace std;
+
+namespace hoot
+{
+
+class SplitMapCmd : public BaseCommand
+{
+public:
+
+  static string className() { return "hoot::SplitMapCmd"; }
+
+  SplitMapCmd() { }
+
+  virtual QString getName() const override { return "split-map"; }
+
+  virtual QString getDescription() const override
+  { return "Split a map geospatially into tiled maps."; }
+
+  virtual int runSimple(QStringList& args) override
+  {
+    if (args.size() != 3)
+    {
+      LOG_VARD(args);
+      cout << getHelp() << endl << endl;
+      throw HootException(QString("%1 takes three parameters.").
+                          arg(getName()));
+    }
+    //  Load the tile map ignoring the file IDs
+    OsmMapPtr tile_map(new OsmMap());
+    IoUtils::loadMap(tile_map, args[0], false);
+    //  Don't introduce source:datetime or source:ingest:datetime
+    conf().set(ConfigOptions::getReaderAddSourceDatetimeKey(), false);
+    //  Load the actual map and use the file IDs
+    OsmMapPtr map(new OsmMap());
+    IoUtils::loadMap(map, args[1], true, Status::Unknown1);
+    //  Split the map up into smaller maps
+    OsmMapSplitter mapSplitter(map, tile_map);
+    mapSplitter.apply();
+    //  Don't include error:circular
+    conf().set(ConfigOptions::getWriterIncludeCircularErrorTagsKey(), false);
+    //  Write the maps out to disk
+    mapSplitter.writeMaps(args[2]);
+
+    return 0;
+  }
+};
+
+HOOT_FACTORY_REGISTER(Command, SplitMapCmd)
+
+}
