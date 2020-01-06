17d5becad8a2084410881e264950eff3ef48cc08
Brandon Witham <brandon.witham@maxar.com>
2020-01-06 11:30:06 -0500

    remove test

--- test-files/cmd/slow/PoiPolygonKeepClosestMatchesOnlyTest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-INPUT_DIR=test-files/cmd/slow/ChangesetDeriveCmdTest
-OUTPUT_DIR=test-output/cmd/slow/ChangesetDeriveCmdTest
-rm -rf $OUTPUT_DIR
-mkdir -p $OUTPUT_DIR
-
-# TODO: finish
