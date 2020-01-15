6b74cd411449698aed0973498108dc3b4ac0aba8
Brian Hatchl <brian.hatchl@radiantsolutions.com>
2020-01-15 16:00:13 -0500

    Handle changeset conflicts (#3696)
    * mark upload changeset jobs if they have conflicts
    
    * mark upload changeset job as CONFLICTS if diff-error.osc file present
    
    split grail refresh layer from upload changeset job
    
    * whoops, whacked check for file before marking conflicted
    
    * remove full replacement param, make alpha shape buffer adaptive
    
    * copyright update
    
    * remove commented out code
    
    * comments
    
    * small code simplify
    
    Co-authored-by: Bobby Simic <slobodan.simic@radiantsolutions.com>

--- hoot-services/src/main/java/hoot/services/controllers/grail/DeriveChangesetReplacementCommand.java
+++ hoot-services/src/main/java/hoot/services/controllers/grail/DeriveChangesetReplacementCommand.java
@@ -22,14 +22,17 @@
  * This will properly maintain the copyright information. DigitalGlobe
  * copyrights will be updated automatically.
  *
- * @copyright Copyright (C) 2019 DigitalGlobe (http://www.digitalglobe.com/)
+ * @copyright Copyright (C) 2019, 2020 DigitalGlobe (http://www.digitalglobe.com/)
  */
 package hoot.services.controllers.grail;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 /**
@@ -43,6 +46,12 @@ class DeriveChangesetReplacementCommand extends GrailCommand {
         List<String> options = new LinkedList<>();
         options.add("api.db.email=" + params.getUser().getEmail());
 
+        List<String> bounds = Arrays.asList(params.getBounds().split(","));
+        Double width = Double.valueOf(bounds.get(2)) - Double.valueOf(bounds.get(0));
+        Double height = Double.valueOf(bounds.get(1)) - Double.valueOf(bounds.get(3));
+        Double delta = Math.min(width, height);
+        Integer alpha = adaptiveAlpha(delta);
+        options.add("cookie.cutter.alpha=" + alpha);
 
         List<String> hootOptions = toHootOptions(options);
 
@@ -54,8 +63,35 @@ class DeriveChangesetReplacementCommand extends GrailCommand {
         substitutionMap.put("HOOT_OPTIONS", hootOptions);
         substitutionMap.put("DEBUG_LEVEL", debugLevel);
 
-        String command = "hoot changeset-derive-replacement --${DEBUG_LEVEL} -C DeriveChangeset.conf ${HOOT_OPTIONS} ${INPUT1} ${INPUT2} ${BOUNDS} ${OSC_FILE} --full-replacement --stats";
+        String command = "hoot changeset-derive-replacement --${DEBUG_LEVEL} -C DeriveChangeset.conf ${HOOT_OPTIONS} ${INPUT1} ${INPUT2} ${BOUNDS} ${OSC_FILE} --stats";
 
         super.configureCommand(command, substitutionMap, caller);
+    }
+
+    public static final Pattern pattern = Pattern.compile("[+\\-]?(?:0|[1-9]\\d*)(?:\\.\\d*)?(?:[eE]([+\\-])?(\\d+))?");
+    /*
+     * returns the power base ten portion of scientific notation for a number
+     * ie. .001 will return -3, while 10 will return 1
+     */
+    public static int exponent(Double d) {
+        int exp = 0;
+        String scinote = String.format("%.3E", d);
+        Matcher matcher = pattern.matcher(scinote);
+        if (matcher.find()) {
+            exp = Integer.parseInt(scinote.split("E")[1]);
+        }
+
+        return exp;
+    }
+
+    /*
+     * returns an adaptive alpha shape based on exent of dataset
+     * (width or height) in decimal degrees
+     */
+    public static int adaptiveAlpha(Double d) {
+        int exp = 4 + exponent(d); //follows table here https://en.wikipedia.org/wiki/Decimal_degrees
+        //to provide an alpha distance one order of magnitude less than the extent of the dataset
+
+        return (int)Math.pow(10, exp);
     }
 }
