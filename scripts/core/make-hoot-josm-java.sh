9c0b6a89e9c6f0047d1bab0482630ceb0126d6f4
Brandon Witham <brandon.witham@maxar.com>
2020-01-30 15:27:33 -0500

    update script

--- scripts/core/make-hoot-josm-java.sh
+++ scripts/core/make-hoot-josm-java.sh
@@ -3,26 +3,26 @@ set -e
 
 cd $HOOT_HOME/hoot-josm
 mkdir -p bin
-rm -f bin/*.jar
 
-# This can be changed downstream to package with other JOSM versions with a file text replacement.
-rm -f $HOOT_HOME/bin/hoot-josm.jar
+# This could be changed downstream to package other JOSM versions with string replace of this var.
 JOSM_JAR=josm-snapshot-15553.jar
-if test -f "bin/$JOSM_JAR"; then
-  echo $JOSM_JAR" already downloaded to bin/josm.jar."
+JOSM_DOWNLOAD_URL=https://josm.openstreetmap.de/download # leave this empty to specify a local file
+if test -f "$JOSM_JAR"; then
+  echo $JOSM_JAR" already downloaded."
+elif test -z "$JOSM_DOWNLOAD_URL"; then
+  echo "Copying "$JOSM_JAR" to bin/josm.jar..."
 else
   echo "Downloading "$JOSM_JAR" to bin/josm.jar..."
-  wget --quiet https://josm.openstreetmap.de/download/josm-snapshot-15553.jar
+  wget --quiet $JOSM_DOWNLOAD_URL/$JOSM_JAR
 fi
 cp $JOSM_JAR bin/josm.jar
+rm -f *.jar.1
 chmod 777 bin/josm.jar
 
 echo "Building hoot-josm jar..."
-#rm -f $HOOT_HOME/bin/josm.jar
 mkdir -p target/classes
-rm -rf target/classes/*
 # The main downside to this is it will always build, regardless of whether the file has been modified. Will worry about that later and maybe
-# get Maven in the mix.
+# get Maven in the mix or check file timestamps.
 find -name "*.java" > sources
 javac -d target/classes -cp bin/josm.jar @sources
 cd target/classes
