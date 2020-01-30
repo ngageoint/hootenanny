ab9920abb16a96d0da2ebe4660f0714a8b5874ec
Brandon Witham <brandon.witham@maxar.com>
2020-01-30 17:11:44 -0500

    cleanup

--- scripts/core/make-hoot-josm-java.sh
+++ scripts/core/make-hoot-josm-java.sh
@@ -4,21 +4,26 @@ set -e
 cd $HOOT_HOME/hoot-josm
 mkdir -p bin
 
-# This could be changed downstream to package other JOSM versions with string replace of this var.
-JOSM_JAR=josm-snapshot-15553.jar
-# JOSM_JAR=me-josm-4.4.6.jar
-JOSM_DOWNLOAD_URL=https://josm.openstreetmap.de/download # leave this empty to specify a local file
-# JOSM_DOWNLOAD_URL=https://github.com/DigitalGlobe/me-josm/releases/download/v4.4.6/
-if test -f "$JOSM_JAR"; then
+mkdir -p tmp
+# These params could be changed downstream to package other JOSM versions with string replace of this var. Since we don't have any compile-time
+# dep checks when using JNI against JOSM, don't blindly update to the latest version without checking tests first.
+JOSM_PATH=https://josm.openstreetmap.de/download # can either be a url or a dir
+JOSM_JAR=josm-snapshot-15796.jar
+#JOSM_PATH=/home/vagrant/hoot/tmp
+#JOSM_JAR=me-josm-4.4.9.jar
+if test -f "tmp/$JOSM_JAR"; then
   echo $JOSM_JAR" already downloaded."
-elif test -z "$JOSM_DOWNLOAD_URL"; then
-  echo "Copying "$JOSM_JAR" to bin/josm.jar..."
-else
+#elif test -z "$JOSM_DOWNLOAD_URL"; then
+elif [[ $JOSM_PATH == http* ]]; then
   echo "Downloading "$JOSM_JAR" to bin/josm.jar..."
   wget --quiet $JOSM_DOWNLOAD_URL/$JOSM_JAR
+  mv $JOSM_JAR tmp
+else
+  echo "Copying "$JOSM_JAR" to bin/josm.jar..."
+  cp $JOSM_PATH/$JOSM_JAR tmp
 fi
-cp $JOSM_JAR bin/josm.jar
-rm -f *.jar.1
+cp tmp/$JOSM_JAR bin/josm.jar
+rm -f *.jar.1 # failed download
 chmod 777 bin/josm.jar
 
 echo "Building hoot-josm jar..."
