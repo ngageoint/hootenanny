022752fb17664e0b11d1afa12b9337c4889ac188
Brandon Witham <brandon.witham@maxar.com>
2020-02-25 16:03:59 +0000

    copyright header updates

--- hoot-core/src/main/cpp/hoot/core/visitors/NonEnglishLanguageDetectionVisitor.h
+++ hoot-core/src/main/cpp/hoot/core/visitors/NonEnglishLanguageDetectionVisitor.h
@@ -22,7 +22,7 @@
  * This will properly maintain the copyright information. DigitalGlobe
  * copyrights will be updated automatically.
  *
- * @copyright Copyright (C) 2018, 2019 DigitalGlobe (http://www.digitalglobe.com/)
+ * @copyright Copyright (C) 2018, 2019, 2020 DigitalGlobe (http://www.digitalglobe.com/)
  */
 
 #ifndef NON_ENGLISH_LANGUAGE_DETECTION_VISITOR_H
