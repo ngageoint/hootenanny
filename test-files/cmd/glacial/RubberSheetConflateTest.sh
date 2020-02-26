453604db8574b0970335c5b461cf2fea840ecc4b
Brandon Witham <brandon.witham@maxar.com>
2020-02-26 14:13:44 -0500

    add dupe way removal to post conflate pipeline

--- test-files/cmd/glacial/RubberSheetConflateTest.sh
+++ test-files/cmd/glacial/RubberSheetConflateTest.sh
@@ -5,7 +5,6 @@ set -e
 
 mkdir -p test-output/cmd/glacial/RubberSheetConflateTest
 
-# Broke this into seperate lines to make reading easier.
 hoot conflate -C Testing.conf -C UnifyingAlgorithm.conf -C ReferenceConflation.conf -D uuid.helper.repeatable=true -D conflate.pre.ops=hoot::RubberSheet -D "rubber.sheet.ref=true" -D "rubber.sheet.minimum.ties=10" -D "unify.optimizer.time.limit=30" test-files/DcGisRoads.osm test-files/DcTigerRoads.osm test-output/cmd/glacial/RubberSheetConflateTest/output.osm
 
 hoot diff -C Testing.conf test-output/cmd/glacial/RubberSheetConflateTest/output.osm test-files/cmd/glacial/RubberSheetConflateTest/output.osm ||
