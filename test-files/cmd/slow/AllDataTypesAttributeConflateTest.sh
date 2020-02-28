da37f35afaca5b0ec411cda459ed5d13cdb7ce57
Brandon Witham <brandon.witham@maxar.com>
2020-02-28 11:59:36 -0500

    fix test

--- test-files/cmd/slow/AllDataTypesAttributeConflateTest.sh
+++ test-files/cmd/slow/AllDataTypesAttributeConflateTest.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-IN_DIR=test-files/cmd/slow/AllDataTypesAttributeConflateTest.sh
-OUT_DIR=test-output/cmd/slow/AllDataTypesAttributeConflateTest.sh
+IN_DIR=test-files/cmd/slow/AllDataTypesAttributeConflateTest
+OUT_DIR=test-output/cmd/slow/AllDataTypesAttributeConflateTest
 mkdir -p $OUT_DIR
 
-CONFIG="-C Testing.conf"
+CONFIG="--warn -C Testing.conf"
 
 hoot conflate $CONFIG -C UnifyingAlgorithm.conf -C AttributeConflation.conf -D writer.include.debug.tags=true test-files/conflate/unified/AllDataTypesA.osm test-files/conflate/unified/AllDataTypesB.osm $OUT_DIR/output.osm
 
