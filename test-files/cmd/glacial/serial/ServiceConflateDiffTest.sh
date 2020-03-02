8d6518918617a903d78bdebfe6fd50b29ad09e9a
Brandon Witham <brandon.witham@maxar.com>
2020-03-02 11:48:34 -0500

    cleanup

--- test-files/cmd/glacial/serial/ServiceConflateDiffTest.sh
+++ test-files/cmd/glacial/serial/ServiceConflateDiffTest.sh
@@ -4,11 +4,11 @@ set -e
 # Test conflates two inputs and then generates a changeset between the reference and the conflated output. The point of the test is to ensure 
 # there are no more create/delete statements generated than what there should be.
 
-CONFIG="--warn -C Testing.conf -D writer.include.debug.tags=true -D uuid.helper.repeatable=true -D api.db.email=OsmApiDbHootApiDbConflate@hoottestcpp.org -D hootapi.db.writer.create.user=true -D hootapi.db.writer.overwrite.map=true -D changeset.user.id=1"
-
 TEST_NAME=ServiceConflateDiffTest
 IN_DIR=/home/vagrant/hoot/tmp/3851
 OUT_DIR=$IN_DIR
+
+CONFIG="--warn -C Testing.conf -D writer.include.debug.tags=true -D uuid.helper.repeatable=true -D debug.maps.write=true -D api.db.email=OsmApiDbHootApiDbConflate@hoottestcpp.org -D hootapi.db.writer.create.user=true -D hootapi.db.writer.overwrite.map=true -D changeset.user.id=1"
 
 rm -f $OUT_DIR/ref.osm
 rm -f $OUT_DIR/sec.osm
@@ -27,30 +27,31 @@ scripts/database/CleanAndInitializeOsmApiDb.sh
 
 echo "Loading the OSM API DB with ref..."
 REF_FILE=$IN_DIR/NOME_40429e.osm
-hoot convert $CONFIG $REF_FILE $OSM_API_DB_URL
+hoot convert $CONFIG -D debug.maps.filename=$IN_DIR/ref-load.osm $REF_FILE $OSM_API_DB_URL
 # pull the ref out for viewing
 hoot convert $CONFIG $OSM_API_DB_URL $OUT_DIR/ref.osm
 
 echo "Loading the HOOT API DB with sec..."
 SEC_FILE=$IN_DIR/OSM_40429e.osm 
 HOOT_DB_URL="hootapidb://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME"
-hoot convert $CONFIG $SEC_FILE "$HOOT_DB_URL/$TEST_NAME-sec"
+hoot convert $CONFIG -D debug.maps.filename=$IN_DIR/sec-load.osm $SEC_FILE "$HOOT_DB_URL/$TEST_NAME-sec"
 # pull the sec out for viewing
 hoot convert $CONFIG "$HOOT_DB_URL/$TEST_NAME-sec" $OUT_DIR/sec.osm
 
 echo "Conflating ref with sec..."
-hoot conflate $CONFIG $OSM_API_DB_URL "$HOOT_DB_URL/$TEST_NAME-sec" "$HOOT_DB_URL/$TEST_NAME-sec-conflated"
+hoot conflate $CONFIG -D debug.maps.filename=$IN_DIR/conflate.osm $OSM_API_DB_URL "$HOOT_DB_URL/$TEST_NAME-sec" "$HOOT_DB_URL/$TEST_NAME-sec-conflated"
 # pull the conflated out for viewing
 hoot convert $CONFIG "$HOOT_DB_URL/$TEST_NAME-sec-conflated" $OUT_DIR/conflated.osm
 
 echo "Deriving a changeset between ref and conflated..."
 # derive the sql version b/c that's what we must apply
-hoot changeset-derive $CONFIG $OSM_API_DB_URL "$HOOT_DB_URL/$TEST_NAME-sec-conflated" $OUT_DIR/diff.osc.sql $OSM_API_DB_URL
+hoot changeset-derive $CONFIG -D debug.maps.filename=$IN_DIR/changeset-derivation.osm $OSM_API_DB_URL "$HOOT_DB_URL/$TEST_NAME-sec-conflated" $OUT_DIR/diff.osc.sql $OSM_API_DB_URL
 # derive an xml version for viewing
 hoot changeset-derive $CONFIG $OSM_API_DB_URL "$HOOT_DB_URL/$TEST_NAME-sec-conflated" $OUT_DIR/diff.osc --stats
 
 echo "Applying the changeset to ref..."
-hoot changeset-apply $CONFIG $OUT_DIR/diff.osc.sql $OSM_API_DB_URL
+hoot changeset-apply $CONFIG -D debug.maps.filename=$IN_DIR/changeset-application.osm $OUT_DIR/diff.osc.sql $OSM_API_DB_URL
 
-echo "Reading out the final map..."
-hoot convert $CONFIG $OSM_API_DB_URL $OUT_DIR/changeset-applied.osm
+echo "Reading out the final map for comparison with expected map..."
+hoot convert $CONFIG -D debug.maps.filename=$IN_DIR/final-map.osm $OSM_API_DB_URL $OUT_DIR/changeset-applied.osm
+#hoot diff $CONFIG $IN_DIR/changeset-applied.osm $OUT_DIR/changeset-applied.osm
