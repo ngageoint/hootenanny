c6d62be5e6f632a72747031727543a1c55bb2a13
Brandon Witham <brandon.witham@maxar.com>
2020-03-10 19:15:48 -0400

    copyright header updates

--- hoot-core/src/main/cpp/hoot/core/criterion/PolygonCriterion.h
+++ hoot-core/src/main/cpp/hoot/core/criterion/PolygonCriterion.h
@@ -22,7 +22,7 @@
  * This will properly maintain the copyright information. DigitalGlobe
  * copyrights will be updated automatically.
  *
- * @copyright Copyright (C) 2019 DigitalGlobe (http://www.digitalglobe.com/)
+ * @copyright Copyright (C) 2019, 2020 DigitalGlobe (http://www.digitalglobe.com/)
  */
 
 #ifndef POLYGON_CRITERION_H
