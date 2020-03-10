b8ad40a60594f23ba1608cafbbd7a3c01b79e844
Brandon Witham <brandon.witham@maxar.com>
2020-03-10 19:08:23 -0400

    update test out

--- test-files/cmd/slow/DiffConflateCmdTest.sh
+++ test-files/cmd/slow/DiffConflateCmdTest.sh
@@ -8,6 +8,7 @@ mkdir -p $OUTPUT_DIR
 LOG_LEVEL=--warn
 
 # Run differential conflation to produce a map output
+echo ""
 echo "Running diff..."
 echo ""
 hoot conflate $LOG_LEVEL -C DifferentialConflation.conf -C NetworkAlgorithm.conf -C Testing.conf \
@@ -15,6 +16,7 @@ hoot conflate $LOG_LEVEL -C DifferentialConflation.conf -C NetworkAlgorithm.conf
  $OUTPUT_DIR/output.osm --differential
 
 # Run changeset w/tags to produce a unified changeset output (geometry and tags)
+echo ""
 echo "Running diff changeset with tags..."
 echo ""
 hoot conflate $LOG_LEVEL -C DifferentialConflation.conf -C NetworkAlgorithm.conf -C Testing.conf  \
@@ -22,6 +24,7 @@ hoot conflate $LOG_LEVEL -C DifferentialConflation.conf -C NetworkAlgorithm.conf
  $OUTPUT_DIR/output_unified.osc --differential --include-tags
 
 # Run changeset w/tags to produce a unified map (osm) output
+echo ""
 echo "Running diff with tags..."
 echo ""
 hoot conflate $LOG_LEVEL -C DifferentialConflation.conf -C NetworkAlgorithm.conf -C Testing.conf \
@@ -29,6 +32,7 @@ hoot conflate $LOG_LEVEL -C DifferentialConflation.conf -C NetworkAlgorithm.conf
  $OUTPUT_DIR/output_unified.osm --differential --include-tags
 
 # Run changeset w/tags to produce separate outputs for geometry and tags
+echo ""
 echo "Running diff changeset with tags, separate outputs..."
 echo ""
 hoot conflate $LOG_LEVEL -C DifferentialConflation.conf -C NetworkAlgorithm.conf -C Testing.conf \
@@ -36,52 +40,73 @@ hoot conflate $LOG_LEVEL -C DifferentialConflation.conf -C NetworkAlgorithm.conf
  $OUTPUT_DIR/output.osc --differential --include-tags --separate-output
 
 # Check differential output
+echo ""
 echo "Checking differential output..."
 echo ""
 hoot diff -C Testing.conf $LOG_LEVEL $OUTPUT_DIR/output.osm $INPUT_DIR/output.osm || \
      diff $OUTPUT_DIR/output.osm $INPUT_DIR/output.osm
 
 # Check changeset output
+echo ""
 echo "Checking Changeset..."
 echo ""
 if diff $OUTPUT_DIR/output.osc $INPUT_DIR/output.osc >/dev/null ; then
+  echo ""
   echo "Changeset Files Match"
+  echo ""
 else
+  echo ""
   echo "Changeset Files Don't Match"
+  echo ""
   diff $OUTPUT_DIR/output.osc t$INPUT_DIR/output.osc
 fi
 
 # Check tag output
+echo ""
 echo "Checking tag diff"
 echo ""
 if diff $OUTPUT_DIR/output.tags.osc $INPUT_DIR/output.tags.osc >/dev/null ; then
+  echo ""
   echo "Tag Changeset Files Match"
+  echo ""
 else
+  echo ""
   echo "Tag Changeset Files Don't Match"
+  echo ""
   diff $OUTPUT_DIR/output.tags.osc $INPUT_DIR/output.tags.osc
 fi
 
 # Check unified changeset output
+echo ""
 echo "Checking unified geometry+tag diff changeset..."
+echo ""
 if diff $OUTPUT_DIR/output_unified.osc $INPUT_DIR/output_unified.osc >/dev/null ; then
+  echo ""
   echo "Unified Changeset Files Match"
+  echo ""
 else
+  echo ""
   echo "Unified Changeset Files Don't Match"
+  echo ""
   diff $OUTPUT_DIR/output_unified.osc $INPUT_DIR/output_unified.osc
 fi
 
 # Check unified osm output
+echo ""
 echo "Checking unified geometry+tag diff osm..."
 echo ""
 hoot diff -C Testing.conf $LOG_LEVEL $OUTPUT_DIR/output_unified.osm $INPUT_DIR/output_unified.osm || \
      diff $OUTPUT_DIR/output_unified.osm $INPUT_DIR/output_unified.osm
 
 # Check to make sure we don't bomb out on empty files
+echo ""
 echo "Checking conflation of empty files..."
+echo ""
 hoot conflate $LOG_LEVEL -C DifferentialConflation.conf -C NetworkAlgorithm.conf -C Testing.conf --warn test-files/Empty.osm test-files/Empty.osm $OUTPUT_DIR/Empty.osm
 hoot diff -C Testing.conf test-files/Empty.osm $OUTPUT_DIR/Empty.osm || cat $OUTPUT_DIR/Empty.osm
 
 # Run with the road snapper
+echo ""
 echo "Checking conflation with road snapping..."
 echo ""
 hoot conflate $LOG_LEVEL -C DifferentialConflation.conf -C NetworkAlgorithm.conf -C Testing.conf \
@@ -92,6 +117,7 @@ hoot diff -C Testing.conf --warn $OUTPUT_DIR/snapped-output.osm $INPUT_DIR/snapp
      diff $OUTPUT_DIR/snapped-output.osm $INPUT_DIR/snapped-output.osm
 
 # Run with the road snapper and keep the ref data
+echo ""
 echo "Checking conflation with road snapping and keeping ref data..."
 echo ""
 hoot conflate $LOG_LEVEL -C DifferentialConflation.conf -C NetworkAlgorithm.conf -C Testing.conf \
@@ -103,6 +129,7 @@ hoot diff -C Testing.conf $LOG_LEVEL $OUTPUT_DIR/snapped-with-ref-output.osm $IN
 
 # Run differential conflation and keep unconflatable features - The only difference you'll see here between this and the first diff
 # conflate execution is that the retaining wall from the second dataset, which hoot is unable to conflate, will pass through to output.
+echo ""
 echo "Running diff and passing unconflatable data through to output..."
 echo ""
 hoot conflate $LOG_LEVEL -C DifferentialConflation.conf -C NetworkAlgorithm.conf -C Testing.conf \
