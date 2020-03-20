241359e0b71e7da3bb4235594bbeed0817c8d013
Brandon Witham <brandon.witham@maxar.com>
2020-03-20 13:57:20 -0400

    copyright header updates

--- hoot-core/src/main/cpp/hoot/core/algorithms/extractors/CentroidDistanceExtractor.cpp
+++ hoot-core/src/main/cpp/hoot/core/algorithms/extractors/CentroidDistanceExtractor.cpp
@@ -23,7 +23,7 @@
  * copyrights will be updated automatically.
  *
  * @copyright Copyright (C) 2005 VividSolutions (http://www.vividsolutions.com/)
- * @copyright Copyright (C) 2015, 2017, 2018, 2019 DigitalGlobe (http://www.digitalglobe.com/)
+ * @copyright Copyright (C) 2015, 2017, 2018, 2019, 2020 DigitalGlobe (http://www.digitalglobe.com/)
  */
 #include "CentroidDistanceExtractor.h"
 
