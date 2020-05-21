791305b50ba1f595849c6854a77f4c526e457060
Brian Hatchl <brian.hatchl@radiantsolutions.com>
2020-05-20 23:22:50 -0400

    give description it's own attribute column (#4031)
    * give description it's own attribute column
    
    refs #4030
    
    * Very close. Need to fix "," inside tag values
    
    refs: #4030
    
    * More code to split and join tags
    
    refs: #4030
    
    * Storing variable length tags is now working
    
    refs: #4030
    
    * Added length checking to the tag storage
    
    refs: #4030
    
    * Fixed a JS error and added a unit test
    
    refs: #4030
    
    Co-authored-by: Matt C. Jackson <matthew.jackson@digitalglobe.com>

---
+++ test-files/cmd/slow/RenderDbTranslationTest.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+IN=test-files/renderDb.osm
+OUT=test-output/cmd/slow/translation/RenderDb
+
+function compareFiles {
+    hoot diff -C Testing.conf $1 $2 || diff $1 $2
+}
+
+# CONFIG="-C Testing.conf"
+CONFIG="-C NodeExport.conf"
+
+# LOTS of debug output
+#CONFIG="-D ogr.debug.dumptags=true -D ogr.debug.lookupcolumn=true -D ogr.debug.lookupclash=true -D ogr.debug.dumpvalidate=true"
+
+mkdir -p $(dirname ${OUT})
+rm -rf "${OUT}_in.osm" "${OUT}_out.osm"
+
+
+hoot convert \
+  $CONFIG \
+  -D schema.translation.direction=toogr \
+  -D convert.ops=hoot::SchemaTranslationVisitor \
+  -D schema.translation.script=$HOOT_HOME/translations/RenderDb.js \
+  ${IN} \
+  "${OUT}_out.osm"
+
+hoot convert \
+  $CONFIG \
+  -D schema.translation.direction=toosm \
+  -D convert.ops=hoot::SchemaTranslationVisitor \
+  -D schema.translation.script=$HOOT_HOME/translations/RenderDb.js \
+  "${OUT}_out.osm" \
+  "${OUT}_in.osm"
+
+#echo $IN  $OUT
+compareFiles "${OUT}_in.osm" ${IN}
