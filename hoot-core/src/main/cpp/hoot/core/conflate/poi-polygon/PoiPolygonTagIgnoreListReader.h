eb2a1735068ed54fd9789f73a567b26be7497e31
Ben Marchant <13385275+bmarchant@users.noreply.github.com>
2020-05-28 16:20:25 -0500

    Copyright

--- hoot-core/src/main/cpp/hoot/core/conflate/poi-polygon/PoiPolygonTagIgnoreListReader.h
+++ hoot-core/src/main/cpp/hoot/core/conflate/poi-polygon/PoiPolygonTagIgnoreListReader.h
@@ -22,7 +22,7 @@
  * This will properly maintain the copyright information. DigitalGlobe
  * copyrights will be updated automatically.
  *
- * @copyright Copyright (C) 2017, 2018, 2019 DigitalGlobe (http://www.digitalglobe.com/)
+ * @copyright Copyright (C) 2017, 2018, 2019, 2020 DigitalGlobe (http://www.digitalglobe.com/)
  */
 #ifndef POIPOLYGONTAGIGNORELISTREADER_H
 #define POIPOLYGONTAGIGNORELISTREADER_H
