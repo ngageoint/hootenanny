ad79abef2c308fb4079f5a15be200ced8ff77623
Ben Marchant <13385275+bmarchant@users.noreply.github.com>
2020-06-11 14:52:52 -0500

    HighwayRfClassifier memory leak (#4080)
    * Use the Meyer's Singleton pattern in the MatchFactory and remove the HighwayClassifier memory leak.
    * Copyright

--- tgs/src/main/cpp/tgs/RandomForest/BaseRandomForest.cpp
+++ tgs/src/main/cpp/tgs/RandomForest/BaseRandomForest.cpp
@@ -22,7 +22,7 @@
  * This will properly maintain the copyright information. DigitalGlobe
  * copyrights will be updated automatically.
  *
- * @copyright Copyright (C) 2015, 2017, 2018, 2019 DigitalGlobe (http://www.digitalglobe.com/)
+ * @copyright Copyright (C) 2015, 2017, 2018, 2019, 2020 DigitalGlobe (http://www.digitalglobe.com/)
  */
 #include "BaseRandomForest.h"
 
@@ -44,7 +44,10 @@ namespace Tgs
   {
   }
 
-  BaseRandomForest::~BaseRandomForest(){}
+  BaseRandomForest::~BaseRandomForest()
+  {
+    clear();
+  }
 
   void BaseRandomForest::clear()
   {
