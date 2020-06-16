b2bdd98e47aa3840c0023e4d4b9b53ead973d442
Brandon Witham <brandon.witham@maxar.com>
2020-06-16 14:44:53 -0400

    fix test

--- test-files/cmd/slow/CookieCutCmdTest.sh
+++ test-files/cmd/slow/CookieCutCmdTest.sh
@@ -5,8 +5,8 @@ mkdir -p test-output/cmd/slow/CookieCutCmdTest
 
 CONFIG="-C Testing.conf"
 
-hoot cookie-cut $CONFIG test-files/cmd/glacial/GenerateAlphaShapeCmdTest/output-1.osm test-files/DcTigerRoads.osm test-output/cmd/slow/CookieCutCmdTest/output-1.osm
+hoot cookie-cut $CONFIG test-files/cmd/slow/GenerateAlphaShapeCmdTest/output-1.osm test-files/DcTigerRoads.osm test-output/cmd/slow/CookieCutCmdTest/output-1.osm
 hoot diff $CONFIG test-output/cmd/slow/CookieCutCmdTest/output-1.osm test-files/cmd/slow/CookieCutCmdTest/output-1.osm || diff test-output/cmd/slow/CookieCutCmdTest/output-1.osm test-files/cmd/slow/CookieCutCmdTest/output-1.osm
 
-hoot cookie-cut $CONFIG test-files/cmd/glacial/GenerateAlphaShapeCmdTest/output-1.osm test-files/DcTigerRoads.osm test-output/cmd/slow/CookieCutCmdTest/output-2.osm --crop
+hoot cookie-cut $CONFIG test-files/cmd/slow/GenerateAlphaShapeCmdTest/output-1.osm test-files/DcTigerRoads.osm test-output/cmd/slow/CookieCutCmdTest/output-2.osm --crop
 hoot diff $CONFIG test-output/cmd/slow/CookieCutCmdTest/output-2.osm test-files/cmd/slow/CookieCutCmdTest/output-2.osm || diff test-output/cmd/slow/CookieCutCmdTest/output-2.osm test-files/cmd/slow/CookieCutCmdTest/output-2.osm
