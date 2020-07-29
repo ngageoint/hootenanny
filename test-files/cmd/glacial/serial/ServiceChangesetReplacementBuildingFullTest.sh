bae9170c2c15d55d029ad17c53b8b2d01aa7f83c
Brandon Witham <brandon.witham@maxar.com>
2020-07-29 17:54:49 -0400

    update test out

--- test-files/cmd/glacial/serial/ServiceChangesetReplacementBuildingFullTest.sh
+++ test-files/cmd/glacial/serial/ServiceChangesetReplacementBuildingFullTest.sh
@@ -3,4 +3,4 @@ set -e
 
 # This tests replacement changeset generation with buildings being replaced throughout the entire AOI.
 
-test-files/cmd/glacial/serial/ServiceChangesetReplacement.sh.off "ServiceChangesetReplacementBuildingFullTest" "test-files/BostonSubsetRoadBuilding_FromOsm.osm" "" "-71.4698,42.4866,-71.4657,42.4902" "-71.4809,42.4808,-71.45688,42.49368" "true" "false" "hoot::BuildingCriterion" "" "false" "" "" "" "" "xml" "5.0" "0.5" "false" "false" "true"
+test-files/cmd/glacial/serial/ServiceChangesetReplacement.sh.off "ServiceChangesetReplacementBuildingFullTest" "test-files/BostonSubsetRoadBuilding_FromOsm.osm" "" "-71.4698,42.4866,-71.4657,42.4902" "-71.4809,42.4808,-71.45688,42.49368" "true" "lenient" "hoot::BuildingCriterion" "" "false" "" "" "" "" "xml" "5.0" "0.5" "false" "false" "true"
