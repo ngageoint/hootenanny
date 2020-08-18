dc7d37e07633dcc003c3b5efad80dcae8898eb16
Ben Marchant <13385275+bmarchant@users.noreply.github.com>
2020-08-18 13:44:59 -0500

    Update script tests to clean up any users and maps used in testing.

--- test-files/cmd/glacial/ServiceDbEmptyTagsTest.sh
+++ test-files/cmd/glacial/ServiceDbEmptyTagsTest.sh
@@ -4,9 +4,12 @@ set -e
 # This script recreates the problem in https://github.com/ngageoint/hootenanny/issues/168
 source $HOOT_HOME/conf/database/DatabaseConfig.sh
 
+export PSQL_DB_AUTH="-h $DB_HOST -p $DB_PORT -U $DB_USER"
 export PGPASSWORD=$DB_PASSWORD
 
-export HOOT_OPTS="--warn -C Testing.conf -D hootapi.db.writer.create.user=true -D api.db.email=ApiDbTest@hoottestcpp.org -D hootapi.db.writer.overwrite.map=true -D writer.include.debug.tags=true -D match.creators=hoot::HighwayMatchCreator -D merger.creators=hoot::HighwayMergerCreator"
+export HOOT_EMAIL="ApiDbTest@hoottestcpp.org"
+
+export HOOT_OPTS="--warn -C Testing.conf -D hootapi.db.writer.create.user=true -D api.db.email=$HOOT_EMAIL -D hootapi.db.writer.overwrite.map=true -D writer.include.debug.tags=true -D match.creators=hoot::HighwayMatchCreator -D merger.creators=hoot::HighwayMergerCreator"
 
 export DB_URL="hootapidb://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME"
 
@@ -20,4 +23,7 @@ export TB_COUNT=`psql -A -t -h $DB_HOST -p $DB_PORT -d $DB_NAME -U $DB_USER -c "
 echo "Tunnel or bridge count (should be zero): " $TB_COUNT
 
 # Clean up the map from the database
-hoot db-delete-map $HOOT_OPTS -D api.db.email=ApiDbTest@hoottestcpp.org $DB_URL/HootApiDbEmptyTagTest
+hoot db-delete-map $HOOT_OPTS -D api.db.email=$HOOT_EMAIL $DB_URL/HootApiDbEmptyTagTest
+
+# Delete the user
+PGPASSWORD=$DB_PASSWORD psql $PSQL_DB_AUTH -c "DELETE FROM users WHERE email='$HOOT_EMAIL';" > /dev/null
--- test-files/cmd/slow/serial/ServiceNodeDensityTilesCmdTest.sh
+++ test-files/cmd/slow/serial/ServiceNodeDensityTilesCmdTest.sh
@@ -11,18 +11,22 @@ mkdir -p $HOOT_HOME/tmp
 
 source conf/database/DatabaseConfig.sh
 export OSM_API_DB_URL="osmapidb://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME_OSMAPI"
-export OSM_API_DB_AUTH="-h $DB_HOST -p $DB_PORT -U $DB_USER"
+export PSQL_DB_AUTH="-h $DB_HOST -p $DB_PORT -U $DB_USER"
 export PGPASSWORD=$DB_PASSWORD_OSMAPI
 HOOT_DB_URL="hootapidb://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME"
+export HOOT_EMAIL="ServiceNodeDensityTilesCmdTest@hoottestcpp.org"
 
 CONFIG="-C Testing.conf"
 
 scripts/database/CleanAndInitializeOsmApiDb.sh
-hoot convert --warn $CONFIG -D api.db.email=test@test.com -D changeset.user.id=1 -D apidb.bulk.inserter.validate.data=true test-files/DcGisRoads.osm $OSM_API_DB_URL
-hoot convert --warn $CONFIG -D hootapi.db.writer.overwrite.map=true -D hootapi.db.writer.create.user=true -D api.db.email=test@test.com test-files/DcTigerRoads.osm $HOOT_DB_URL"/DcTigerRoads"
-hoot node-density-tiles --warn $CONFIG -D api.db.email=test@test.com -D convert.bounding.box="-77.04856,38.8855,-77.0292,38.899" $OSM_API_DB_URL";"$HOOT_DB_URL"/DcTigerRoads" $OUTPUT_DIR/output-cropped.geojson --maxNodeCountPerTile 1000
+hoot convert --warn $CONFIG -D api.db.email=$HOOT_EMAIL -D changeset.user.id=1 -D apidb.bulk.inserter.validate.data=true test-files/DcGisRoads.osm $OSM_API_DB_URL
+hoot convert --warn $CONFIG -D hootapi.db.writer.overwrite.map=true -D hootapi.db.writer.create.user=true -D api.db.email=$HOOT_EMAIL test-files/DcTigerRoads.osm $HOOT_DB_URL"/DcTigerRoads"
+hoot node-density-tiles --warn $CONFIG -D api.db.email=$HOOT_EMAIL -D convert.bounding.box="-77.04856,38.8855,-77.0292,38.899" $OSM_API_DB_URL";"$HOOT_DB_URL"/DcTigerRoads" $OUTPUT_DIR/output-cropped.geojson --maxNodeCountPerTile 1000
 hoot diff $CONFIG $GOLD_FILES_DIR/output-cropped.geojson $OUTPUT_DIR/output-cropped.geojson
 
 # Clean up the map from the database
-hoot db-delete-map --warn $CONFIG -D api.db.email=test@test.com $HOOT_DB_URL"/DcTigerRoads"
+hoot db-delete-map --warn $CONFIG -D api.db.email=$HOOT_EMAIL $HOOT_DB_URL"/DcTigerRoads"
 scripts/database/CleanAndInitializeOsmApiDb.sh > /dev/null
+
+# Delete the user
+PGPASSWORD=$DB_PASSWORD psql $PSQL_DB_AUTH -c "DELETE FROM users WHERE email='$HOOT_EMAIL';" > /dev/null
