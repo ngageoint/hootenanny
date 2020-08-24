da0e2d4f3a8483c3ef5cdbcb7f26df6629e6b976
Ben Marchant <13385275+bmarchant@users.noreply.github.com>
2020-08-24 18:35:50 -0500

    Fix unit tests, include _alpha in initial random points calculation.

--- hoot-core/src/main/cpp/hoot/core/cmd/GenerateAlphaShapeCmd.cpp
+++ hoot-core/src/main/cpp/hoot/core/cmd/GenerateAlphaShapeCmd.cpp
@@ -71,8 +71,8 @@ public:
       throw HootException(QString("%1 takes two, four, or six parameters.").arg(getName()));
       break;
     case 2:   //  Input/output filenames only
-    case 4:   //  Input/output filenames and '--alpha'
-    case 6:   //  Input/output filenames, '--alpha' and '--buffer'
+    case 4:   //  Input/output filenames and one of '--alpha' or '--buffer'
+    case 6:   //  Input/output filenames and both of '--alpha' and '--buffer'
       break;
     }
     //  Parse and remove the alpha value from the arguments, if present
