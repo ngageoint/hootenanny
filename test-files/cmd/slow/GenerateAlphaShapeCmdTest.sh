370490cf7a2d1a0dbe89c4eaeb16e91bf1f8f005
Ben Marchant <13385275+bmarchant@users.noreply.github.com>
2020-08-26 14:55:57 -0500

    Updated unit tests for alpha value generation.

--- test-files/cmd/slow/GenerateAlphaShapeCmdTest.sh
+++ test-files/cmd/slow/GenerateAlphaShapeCmdTest.sh
@@ -5,15 +5,18 @@ mkdir -p test-output/cmd/slow/GenerateAlphaShapeCmdTest
 
 CONFIG="-C Testing.conf"
 
-hoot generate-alpha-shape $CONFIG --alpha 1000 --buffer 0 test-files/DcGisRoads.osm test-output/cmd/slow/GenerateAlphaShapeCmdTest/output-1.osm
+# Test with no buffer specified
+hoot generate-alpha-shape $CONFIG --alpha 1000 test-files/DcGisRoads.osm test-output/cmd/slow/GenerateAlphaShapeCmdTest/output-1.osm
 hoot diff $CONFIG test-output/cmd/slow/GenerateAlphaShapeCmdTest/output-1.osm test-files/cmd/slow/GenerateAlphaShapeCmdTest/output-1.osm || diff test-output/cmd/slow/GenerateAlphaShapeCmdTest/output-1.osm test-files/cmd/slow/GenerateAlphaShapeCmdTest/output-1.osm
 
+# Test with alpha and buffer specified
 hoot generate-alpha-shape $CONFIG --alpha 1000 --buffer 1000 test-files/DcGisRoads.osm test-output/cmd/slow/GenerateAlphaShapeCmdTest/output-2.osm
 hoot diff $CONFIG test-output/cmd/slow/GenerateAlphaShapeCmdTest/output-2.osm test-files/cmd/slow/GenerateAlphaShapeCmdTest/output-2.osm || diff test-output/cmd/slow/GenerateAlphaShapeCmdTest/output-2.osm test-files/cmd/slow/GenerateAlphaShapeCmdTest/output-2.osm
 
+# Test with alpha and negative buffer specified
 hoot generate-alpha-shape $CONFIG --alpha 1000 --buffer -500 test-files/DcGisRoads.osm test-output/cmd/slow/GenerateAlphaShapeCmdTest/output-3.osm
 hoot diff $CONFIG test-output/cmd/slow/GenerateAlphaShapeCmdTest/output-3.osm test-files/cmd/slow/GenerateAlphaShapeCmdTest/output-3.osm || diff test-output/cmd/slow/GenerateAlphaShapeCmdTest/output-3.osm test-files/cmd/slow/GenerateAlphaShapeCmdTest/output-3.osm
 
-#hoot generate-alpha-shape $CONFIG --buffer 0 test-files/Congo_MGCP_Roads_Bridges_subset.osm test-output/cmd/slow/GenerateAlphaShapeCmdTest/output-4.osm
-#hoot diff $CONFIG test-output/cmd/slow/GenerateAlphaShapeCmdTest/output-4.osm test-files/cmd/slow/GenerateAlphaShapeCmdTest/output-4.osm || diff test-output/cmd/slow/GenerateAlphaShapeCmdTest/output-4.osm test-files/cmd/slow/GenerateAlphaShapeCmdTest/output-4.osm
-
+# Test with no alpha and no buffer specified
+hoot generate-alpha-shape $CONFIG test-files/DcGisRoads.osm test-output/cmd/slow/GenerateAlphaShapeCmdTest/output-4.osm
+hoot diff $CONFIG test-output/cmd/slow/GenerateAlphaShapeCmdTest/output-4.osm test-files/cmd/slow/GenerateAlphaShapeCmdTest/output-4.osm || diff test-output/cmd/slow/GenerateAlphaShapeCmdTest/output-4.osm test-files/cmd/slow/GenerateAlphaShapeCmdTest/output-4.osm
