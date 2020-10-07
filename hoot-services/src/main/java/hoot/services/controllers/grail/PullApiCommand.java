8e74f5dbf47de4f21c94ff40101e93e33d23f8fd
Bobby Simic <slobodan.simic@radiantsolutions.com>
2020-10-07 16:33:10 -0400

    remove bbox area check for private overpass pull (#4295)

--- hoot-services/src/main/java/hoot/services/controllers/grail/PullApiCommand.java
+++ hoot-services/src/main/java/hoot/services/controllers/grail/PullApiCommand.java
@@ -98,18 +98,6 @@ public class PullApiCommand implements InternalCommand {
         String url = "";
         try {
             BoundingBox boundingBox = new BoundingBox(params.getBounds());
-            // buffer the reference data bounding box to include neighboring data that may be snapped to
-            // Disable this for now as pulling a buffered extent does not guarantee that connected ways will be present in the output
-            // boundingBox.adjust(Double.parseDouble(CHANGESET_DERIVE_BUFFER));
-
-            double bboxArea = boundingBox.getArea();
-
-            double maxBboxArea = params.getMaxBBoxSize();
-
-            if (bboxArea > maxBboxArea) {
-                String errorMsg = "The bounding box area (" + bboxArea + ") is too large. It must be less than " + maxBboxArea + " degrees";
-                throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST).entity(errorMsg).build());
-            }
 
             InputStream responseStream = null;
             // Checks to see the set pull url is for the private overpass url
@@ -127,6 +115,14 @@ public class PullApiCommand implements InternalCommand {
                     }
                 }
             } else {
+                double bboxArea = boundingBox.getArea();
+                double maxBboxArea = params.getMaxBBoxSize();
+
+                if (bboxArea > maxBboxArea) {
+                    String errorMsg = "The bounding box area (" + bboxArea + ") is too large for rails. It must be less than " + maxBboxArea + " degrees";
+                    throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST).entity(errorMsg).build());
+                }
+
                 url = replaceSensitiveData(params.getPullUrl()) + "?bbox=" + boundingBox.toServicesString();
             }
 
