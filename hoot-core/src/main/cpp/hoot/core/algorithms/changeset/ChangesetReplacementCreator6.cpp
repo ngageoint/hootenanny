6373f7bf3a9f8ae865a2b5c2f9a94f9603ba7b73
bobi7 <slobodan.simic@radiantsolutions.com>
2020-10-08 16:12:37 -0400

    typo fix

--- hoot-core/src/main/cpp/hoot/core/algorithms/changeset/ChangesetReplacementCreator6.cpp
+++ hoot-core/src/main/cpp/hoot/core/algorithms/changeset/ChangesetReplacementCreator6.cpp
@@ -585,7 +585,7 @@ void ChangesetReplacementCreator6::_synchronizeIds(
   // changeset modifications from being generated. Its possible we could do this earlier in the
   // replacement process, however that has proven difficult to accomplish so far.
 
-  assert(mapsBeingReplaced.size() == replacementMaps.size());
+  assert(mapBeingReplaced.size() == replacementMaps.size());
   ChangesetReplacementElementIdSynchronizer idSync;
   OsmMapWriterFactory::writeDebugMap(
     mapBeingReplaced, _changesetId + "-" + mapBeingReplaced->getName() +
