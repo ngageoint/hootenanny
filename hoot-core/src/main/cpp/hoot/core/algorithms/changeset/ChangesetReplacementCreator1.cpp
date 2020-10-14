af57c8c8bb99572ce005e0a7f2646e4a3fcffbd9
Brandon Witham <303724+bwitham@users.noreply.github.com>
2020-10-14 11:35:18 -0400

    remove bad assert (#4309)

--- hoot-core/src/main/cpp/hoot/core/algorithms/changeset/ChangesetReplacementCreator1.cpp
+++ hoot-core/src/main/cpp/hoot/core/algorithms/changeset/ChangesetReplacementCreator1.cpp
@@ -2020,7 +2020,6 @@ void ChangesetReplacementCreator1::_synchronizeIds(
   // changeset modifications from being generated. Its possible we could do this earlier in the
   // replacement process, however that has proven difficult to accomplish so far.
 
-  assert(mapsBeingReplaced.size() == replacementMaps.size());
   ChangesetReplacementElementIdSynchronizer idSync;
   OsmMapWriterFactory::writeDebugMap(
     mapBeingReplaced, _changesetId + "-" + mapBeingReplaced->getName() +
