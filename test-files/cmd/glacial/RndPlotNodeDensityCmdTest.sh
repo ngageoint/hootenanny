4b21ff8a29e0ad36afa081e12d6eb31b4da18535
Brandon Witham <brandon.witham@maxar.com>
2020-10-28 17:31:08 -0400

    hoot-rnd refactoring

--- test-files/cmd/glacial/RndPlotNodeDensityCmdTest.sh
+++ test-files/cmd/glacial/RndPlotNodeDensityCmdTest.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
 
-mkdir -p $HOOT_HOME/test-output/cmd/quick/PlotNodeDensityCmdTest
+mkdir -p $HOOT_HOME/test-output/cmd/quick/RndPlotNodeDensityCmdTest
 
 inputfile=$HOOT_HOME/test-files/DcGisRoads.pbf
-outputfile=$HOOT_HOME/test-output/cmd/quick/PlotNodeDensityCmdTest/out.png
+outputfile=$HOOT_HOME/test-output/cmd/quick/RndPlotNodeDensityCmdTest/out.png
 
 CONFIG="-C Testing.conf"
 
 hoot plot-node-density $CONFIG $inputfile $outputfile 1024 
 
 # Compare to known-good
-goodfile=$HOOT_HOME/test-files/cmd/quick/PlotNodeDensityCmdTest/out.png
+goodfile=$HOOT_HOME/test-files/cmd/quick/RndPlotNodeDensityCmdTest/out.png
 cmp $goodfile $outputfile
 if [ "$?" = "1" ]; then
   echo "PNG files are not equal! Compare $goodfile $testfile visually"
