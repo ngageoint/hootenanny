6ca04dadb6c00742f5fb453fd2a0785988debe83
Brandon Witham <brandon.witham@maxar.com>
2020-12-03 12:09:24 -0500

    add test

--- test-files/cmd/slow/serial/ServiceDiffUnifyingRoadSnapTest.sh
+++ test-files/cmd/slow/serial/ServiceDiffUnifyingRoadSnapTest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-test-files/cmd/slow/serial/ServiceDiffRoadSnapTest.sh.off "ServiceDiffUnifyingRoadSnapTest" "UnifyingAlgorithm.conf"
+test-files/cmd/slow/serial/ServiceDiffRoadSnapTest.sh.off "ServiceDiffUnifyingRoadSnapTest" "test-files/cmd/slow/DiffConflateCmdTest/input3.osm" "test-files/cmd/slow/DiffConflateCmdTest/input4.osm" "UnifyingAlgorithm.conf" "2"
 
 
