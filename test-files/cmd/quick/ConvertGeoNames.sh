eeee27bc68daaec6ec3d8daa97e2273d0ccf1c3a
Matt C. Jackson <matthew.jackson@digitalglobe.com>
2020-12-08 07:49:21 +0000

    Added a geonames.nga.mil test
    refs: #4276

--- test-files/cmd/quick/ConvertGeoNames.sh
+++ test-files/cmd/quick/ConvertGeoNames.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
 
+# GeoNames.org
 hoot convert -C Testing.conf -D map.factory.writer=hoot::OsmXmlWriter -D convert.ops=hoot::SchemaTranslationVisitor -D schema.translation.script=$HOOT_HOME/translations/GeoNames_to_OSM.js test-files/cmd/quick/ConvertGeoNames.geonames /dev/stdout
+
+# geonames.nga.mil
+hoot convert -C Testing.conf -D map.factory.writer=hoot::OsmXmlWriter -D convert.ops=hoot::SchemaTranslationVisitor -D schema.translation.script=$HOOT_HOME/translations/GeoNames_to_OSM.js test-files/cmd/quick/ConvertGeoNamesMil.geonames /dev/stdout
