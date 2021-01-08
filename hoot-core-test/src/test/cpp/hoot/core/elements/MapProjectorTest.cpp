bd04ad5652d3992bff502e4756fef21b93bf8eaf
Brandon Witham <brandon.witham@maxar.com>
2021-01-08 18:18:12 +0000

    copyright header updates

--- hoot-core-test/src/test/cpp/hoot/core/elements/MapProjectorTest.cpp
+++ hoot-core-test/src/test/cpp/hoot/core/elements/MapProjectorTest.cpp
@@ -22,7 +22,7 @@
  * This will properly maintain the copyright information. DigitalGlobe
  * copyrights will be updated automatically.
  *
- * @copyright Copyright (C) 2013, 2014, 2018, 2019, 2020 DigitalGlobe (http://www.digitalglobe.com/)
+ * @copyright Copyright (C) 2013, 2014, 2018, 2019, 2020, 2021 DigitalGlobe (http://www.digitalglobe.com/)
  */
 
 // boost
--- hoot-core/src/main/cpp/hoot/core/elements/RelationMemberComparison.h
+++ hoot-core/src/main/cpp/hoot/core/elements/RelationMemberComparison.h
@@ -22,7 +22,7 @@
  * This will properly maintain the copyright information. DigitalGlobe
  * copyrights will be updated automatically.
  *
- * @copyright Copyright (C) 2020 DigitalGlobe (http://www.digitalglobe.com/)
+ * @copyright Copyright (C) 2020, 2021 DigitalGlobe (http://www.digitalglobe.com/)
  */
 #ifndef RELATION_MEMBER_COMPARISON_H
 #define RELATION_MEMBER_COMPARISON_H
