0333dcd628f6efab6cc3b1f0c58061137d57b0d6
Brandon Witham <brandon.witham@maxar.com>
2021-01-08 19:59:01 +0000

    copyright header updates

--- hoot-core-test/src/test/cpp/hoot/core/io/OgrReaderTest.cpp
+++ hoot-core-test/src/test/cpp/hoot/core/io/OgrReaderTest.cpp
@@ -22,7 +22,7 @@
  * This will properly maintain the copyright information. DigitalGlobe
  * copyrights will be updated automatically.
  *
- * @copyright Copyright (C) 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020 DigitalGlobe (http://www.digitalglobe.com/)
+ * @copyright Copyright (C) 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021 DigitalGlobe (http://www.digitalglobe.com/)
  */
 
 // Hoot
