1cb5047cc6fdeb77df977d2c3fc3985b7553a65e
bobi7 <slobodan.simic@radiantsolutions.com>
2021-02-03 01:55:58 -0500

    remote debugging sucks so much

--- hoot-services/src/main/java/hoot/services/controllers/osm/map/FolderRecord.java
+++ hoot-services/src/main/java/hoot/services/controllers/osm/map/FolderRecord.java
@@ -22,7 +22,7 @@
  * This will properly maintain the copyright information. DigitalGlobe
  * copyrights will be updated automatically.
  *
- * @copyright Copyright (C) 2016, 2017, 2018, 2019 DigitalGlobe (http://www.digitalglobe.com/)
+ * @copyright Copyright (C) 2016, 2017, 2018, 2019, 2021 DigitalGlobe (http://www.digitalglobe.com/)
  */
 package hoot.services.controllers.osm.map;
 
@@ -38,7 +38,7 @@ import java.text.SimpleDateFormat;
 
 public class FolderRecord {
     private long id;
-    private long parentId;
+    private Long parentId;
     private String name;
     private long userId;
     private boolean isPublic;
@@ -57,11 +57,11 @@ public class FolderRecord {
         this.id = id;
     }
 
-    public long getParentId() {
+    public Long getParentId() {
         return parentId;
     }
 
-    public void setParentId(long parentId) {
+    public void setParentId(Long parentId) {
         this.parentId = parentId;
     }
 
