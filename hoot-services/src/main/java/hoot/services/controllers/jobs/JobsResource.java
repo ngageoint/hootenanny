7a992a69e1784d5ca785eb556cba4ed96973feeb
bobi7 <slobodan.simic@radiantsolutions.com>
2021-02-03 02:09:04 -0500

    copyright update

--- hoot-services/src/main/java/hoot/services/controllers/jobs/JobsResource.java
+++ hoot-services/src/main/java/hoot/services/controllers/jobs/JobsResource.java
@@ -22,7 +22,7 @@
  * This will properly maintain the copyright information. DigitalGlobe
  * copyrights will be updated automatically.
  *
- * @copyright Copyright (C) 2018, 2019, 2020 DigitalGlobe (http://www.digitalglobe.com/)
+ * @copyright Copyright (C) 2018, 2019, 2020, 2021 DigitalGlobe (http://www.digitalglobe.com/)
  */
 package hoot.services.controllers.jobs;
 
@@ -96,7 +96,7 @@ public class JobsResource {
         try {
             return jobsStatusesManager.getJobsHistory(user, sort, offset, limit, type, status, groupJobId);
         } catch (IllegalArgumentException iae) {
-            // logger.error(iae.getMessage(), iae);
+            logger.error(iae.getMessage(), iae);
             throw new WebApplicationException(iae, Response.status(Response.Status.BAD_REQUEST).entity(iae.getMessage()).build());
         }
     }
