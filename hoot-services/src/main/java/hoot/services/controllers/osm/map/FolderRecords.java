2451c48deaa4984285bdcfe3b57c68ab016fd136
bobi7 <slobodan.simic@radiantsolutions.com>
2021-02-12 15:37:53 -0500

    revert unchanged files to master version

--- hoot-services/src/main/java/hoot/services/controllers/osm/map/FolderRecords.java
+++ hoot-services/src/main/java/hoot/services/controllers/osm/map/FolderRecords.java
@@ -22,7 +22,7 @@
  * This will properly maintain the copyright information. DigitalGlobe
  * copyrights will be updated automatically.
  *
- * @copyright Copyright (C) 2016, 2017, 2018, 2021 DigitalGlobe (http://www.digitalglobe.com/)
+ * @copyright Copyright (C) 2016, 2017, 2018 DigitalGlobe (http://www.digitalglobe.com/)
  */
 package hoot.services.controllers.osm.map;
 
@@ -42,7 +42,6 @@ public class FolderRecords {
     public void setFolders(FolderRecord[] folders) {
         this.folders = folders;
     }
-
     public FolderRecord getFolderById(Long folderId) {
         for(FolderRecord f : this.folders) {
             if(f.getId() == folderId) {
