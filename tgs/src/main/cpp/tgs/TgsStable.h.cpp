d733d34083f91a844021570b80b13a43ee59e694
Brandon Witham <brandon.witham@maxar.com>
2021-02-12 14:58:49 -0500

    company name change for copyright, etc.

--- tgs/src/main/cpp/tgs/TgsStable.h.cpp
+++ tgs/src/main/cpp/tgs/TgsStable.h.cpp
@@ -19,9 +19,9 @@
  * The following copyright notices are generated automatically. If you
  * have a new notice to add, please use the format:
  * " * @copyright Copyright ..."
- * This will properly maintain the copyright information. DigitalGlobe
+ * This will properly maintain the copyright information. Maxar
  * copyrights will be updated automatically.
  *
- * @copyright Copyright (C) 2015 DigitalGlobe (http://www.digitalglobe.com/)
+ * @copyright Copyright (C) 2015 Maxar (http://www.maxar.com/)
  */
 #include "TgsStable.h"
