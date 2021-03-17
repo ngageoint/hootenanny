01ae17918cab939d9e2ee89fa225046328a0860e
Brandon Witham <brandon.witham@maxar.com>
2021-03-17 16:57:56 -0400

    correction

--- hoot-core/src/main/cpp/hoot/core/cmd/ScoreMatchesCmd.cpp
+++ hoot-core/src/main/cpp/hoot/core/cmd/ScoreMatchesCmd.cpp
@@ -136,7 +136,7 @@ public:
     else
     {
       double score;
-      std::shared_ptr<const MatchThreshold> mt =
+      std::shared_ptr<MatchThreshold> mt =
         std::make_shared<MatchThreshold>(MatchThreshold(0.5, 0.5, 1.0, false));
       const QString result = evaluateThreshold(maps, output, mt, showConfusion, score);
       cout << result;
@@ -155,7 +155,8 @@ public:
     double f(Tgs::Vector v) override
     {
       double score;
-      std::shared_ptr<MatchThreshold> mt(new MatchThreshold(v[0], v[1], v[2], false));
+      std::shared_ptr<MatchThreshold> mt =
+        std::make_shared<MatchThreshold>(MatchThreshold(v[0], v[1], v[2], false));
       _cmd->evaluateThreshold(_maps, "", mt, _showConfusion, score);
       return score;
     }
