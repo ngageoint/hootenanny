b9defc4478297496628c75d25fdffcdcb0b1202a
Brandon Witham <brandon.witham@maxar.com>
2021-03-23 11:21:12 -0400

    minor changes from an rnd branch

--- rules/River.js
+++ rules/River.js
@@ -279,7 +279,8 @@ exports.mergeSets = function(map, pairs, replaced)
     // We add a conflate post op here which will remove conflate merge created multilinestring
     // relations and add waterway type tags to their children. See
     // MultilineStringMergeRelationCollapser for more details. Its possible we may need to replicate
-    // this logic for other matchers going forward. This has to be done within this method, b/c its
+    // this logic for other matchers going forward or maybe MultilineStringMergeRelationCollapser
+    // should be made part of LinearSnapMerger?. This has to be done within this method, b/c its
     // the only js entry point for conflate merging.
     var markMergeMultiLineStringRelations =
       (hoot.get("waterway.mark.merge.created.multilinestring.relations") === 'true');
