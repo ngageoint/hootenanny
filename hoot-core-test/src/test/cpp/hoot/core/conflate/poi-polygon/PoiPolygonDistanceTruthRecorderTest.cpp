6d661a5ec02ce83c85b3480746a5882ade207122
Brandon Witham <brandon.witham@maxar.com>
2021-04-05 10:18:41 -0400

    add test stubs

---
+++ hoot-core-test/src/test/cpp/hoot/core/conflate/poi-polygon/PoiPolygonDistanceTruthRecorderTest.cpp
@@ -0,0 +1,63 @@
+/*
+ * This file is part of Hootenanny.
+ *
+ * Hootenanny is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * --------------------------------------------------------------------
+ *
+ * The following copyright notices are generated automatically. If you
+ * have a new notice to add, please use the format:
+ * " * @copyright Copyright ..."
+ * This will properly maintain the copyright information. Maxar
+ * copyrights will be updated automatically.
+ *
+ * @copyright Copyright (C) 2021 Maxar (http://www.maxar.com/)
+ */
+
+// Hoot
+#include <hoot/core/elements/OsmMap.h>
+#include <hoot/core/TestUtils.h>
+#include <hoot/core/conflate/poi-polygon/PoiPolygonDistanceTruthRecorder.h>
+#include <hoot/core/io/OsmMapReaderFactory.h>
+#include <hoot/core/io/OsmMapWriterFactory.h>
+#include <hoot/core/elements/MapProjector.h>
+
+namespace hoot
+{
+
+class PoiPolygonDistanceTruthRecorderTest : public HootTestFixture
+{
+    CPPUNIT_TEST_SUITE(PoiPolygonDistanceTruthRecorderTest);
+    CPPUNIT_TEST(runBasicTest);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+
+  PoiPolygonDistanceTruthRecorderTest() :
+  HootTestFixture(
+    "test-files/conflate/poi-polygon/PoiPolygonDistanceTruthRecorderTest/",
+    "test-output/conflate/poi-polygon/PoiPolygonDistanceTruthRecorderTest/")
+  {
+    //setResetType(ResetBasic);
+  }
+
+  void runBasicTest()
+  {
+
+  }
+};
+
+CPPUNIT_TEST_SUITE_NAMED_REGISTRATION(PoiPolygonDistanceTruthRecorderTest, "quick");
+
+}
---
+++ hoot-core-test/src/test/cpp/hoot/core/scoring/MapCompareUtilsTest.cpp
@@ -0,0 +1,63 @@
+/*
+ * This file is part of Hootenanny.
+ *
+ * Hootenanny is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * --------------------------------------------------------------------
+ *
+ * The following copyright notices are generated automatically. If you
+ * have a new notice to add, please use the format:
+ * " * @copyright Copyright ..."
+ * This will properly maintain the copyright information. Maxar
+ * copyrights will be updated automatically.
+ *
+ * @copyright Copyright (C) 2021 Maxar (http://www.maxar.com/)
+ */
+
+// Hoot
+#include <hoot/core/elements/OsmMap.h>
+#include <hoot/core/TestUtils.h>
+#include <hoot/core/scoring/MapCompareUtils.h>
+#include <hoot/core/io/OsmMapReaderFactory.h>
+#include <hoot/core/io/OsmMapWriterFactory.h>
+#include <hoot/core/elements/MapProjector.h>
+
+namespace hoot
+{
+
+class MapCompareUtilsTest : public HootTestFixture
+{
+    CPPUNIT_TEST_SUITE(MapCompareUtilsTest);
+    CPPUNIT_TEST(runBasicTest);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+
+  MapCompareUtilsTest() :
+  HootTestFixture(
+    "test-files/scoring/MapCompareUtilsTest/",
+    "test-output/scoring/MapCompareUtilsTest/")
+  {
+    //setResetType(ResetBasic);
+  }
+
+  void runBasicTest()
+  {
+
+  }
+};
+
+CPPUNIT_TEST_SUITE_NAMED_REGISTRATION(MapCompareUtilsTest, "quick");
+
+}
