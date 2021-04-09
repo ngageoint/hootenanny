de1c2dc8173d4f0778a0cbd9637df928a245ade1
Brandon Witham <brandon.witham@maxar.com>
2021-04-09 15:06:29 -0400

    fix test

--- translations/test/WayJs.js
+++ translations/test/WayJs.js
@@ -3,14 +3,14 @@ var assert = require('assert');
 
 var HOOT_HOME = process.env.HOOT_HOME;
 var hoot = require(HOOT_HOME + '/lib/HootJs');
-hoot.Log.setLogLevel('error');
+hoot.Log.setLogLevel('warn');
 
 describe('WayJs', function() {
 
     it('Should determine the number of nodes in a way', function() {
     
         var map = new hoot.OsmMap();
-        hoot.loadMap(map, "test-files/ToyTestA.osm", true, 1);
+        hoot.loadMap(map, HOOT_HOME + "/test-files/ToyTestA.osm", true, 1);
         var element = hoot.MapUtils.getFirstElementWithNote(map, "0");
         assert.equal(30, element.getNodeCount());
 
