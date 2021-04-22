e5a6a555fa8bc3ea7420c43ed16933bea98a7621
Brandon Witham <brandon.witham@maxar.com>
2021-04-22 16:29:34 -0400

    add test

---
+++ test-files/cmd/slow/DiffConflatePartialLinearMatchTest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+mkdir -p $HOOT_HOME/tmp/
+INPUT_DIR=test-files/cmd/slow/DiffConflatePartialLinearMatchTest
+OUTPUT_DIR=test-output/cmd/slow/DiffConflatePartialLinearMatchTest
+mkdir -p $OUTPUT_DIR
+LOG_LEVEL=--warn
+
+# TODO
+ 
+hoot conflate $LOG_LEVEL -C UnifyingAlgorithm.conf -C DifferentialConflation.conf -C Testing.conf -D match.creators="hoot::HighwayMatchCreator" -D merger.creators="hoot::HighwayMergerCreator" -D differential.remove.partial.matches.as.whole=false -D writer.include.debug.tags=true -D uuid.helper.repeatable=true $INPUT_DIR/input1.osm $INPUT_DIR/input2.osm $OUTPUT_DIR/output.osm --differential
+
+hoot diff $LOG_LEVEL -C Testing.conf $INPUT_DIR/output.osm $OUTPUT_DIR/output.osm
