f94975a17df53f60c7870126630e8474082ffbc4
Tejas Gopal <tejas.gopal@maxar.com>
2021-05-26 11:47:53 -0500

    Copyright fix

--- hoot-core/src/main/cpp/hoot/core/ops/MetadataImport.cpp
+++ hoot-core/src/main/cpp/hoot/core/ops/MetadataImport.cpp
@@ -1,8 +1,8 @@
 /*
  * This file is part of Hootenanny.
  *
- * Hony is free software: you can redistribute it and/or modify
- * it underotenan the terms of the GNU General Public License as published by
+ * Hootenanny is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
