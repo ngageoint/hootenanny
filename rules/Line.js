4ca7db6e0a600f7a6fc21fab5b482a91c0c32924
Brandon Witham <brandon.witham@maxar.com>
2021-05-26 16:10:35 -0400

    comment out code revealing underlying problem for this bug

--- rules/Line.js
+++ rules/Line.js
@@ -48,11 +48,11 @@ exports.isMatchCandidate = function(map, e)
     return false;
   }
   // This prevents some of the problems seen in #4149. It should be removed after that issue is fixed.
-  else if (e.getElementId().getType() == "Way" && !hoot.OsmSchema.hasType(e) &&
+  /*else if (e.getElementId().getType() == "Way" && !hoot.OsmSchema.hasType(e) &&
            hoot.RelationMemberUtils.isMemberOfRelationSatisfyingCriterion(map, e.getElementId(), "hoot::CollectionRelationCriterion"))
   {
     return false;
-  }
+  }*/
 
   return hoot.OsmSchema.isLinear(e) && !hoot.OsmSchema.isSpecificallyConflatable(map, e, exports.geometryType);
 };
