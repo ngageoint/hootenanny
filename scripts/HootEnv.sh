2e4c3290f6b57f1dc395b6b4bcf6291db1c367fc
Ben Marchant <13385275+bmarchant@users.noreply.github.com>
2021-06-04 15:23:59 -0500

    Fix 'archive-create' CircleCI test

--- scripts/HootEnv.sh
+++ scripts/HootEnv.sh
@@ -17,8 +17,10 @@ export LD_LIBRARY_PATH=$GDAL_LIB_DIR:$JAVA_HOME/jre/lib/amd64/server:$HOOT_HOME/
 export PATH=$HOOT_HOME/bin/:$PATH
 export QT_SELECT=5
 
-source $HOOT_HOME/VagrantProvisionVars.sh
-source /opt/rh/devtoolset-$DEVTOOLSET_VERSION/enable
+if [ -f /opt/rh/devtoolset-${DEVTOOLSET_VERSION}/enable ]; then
+    source $HOOT_HOME/VagrantProvisionVars.sh
+    source /opt/rh/devtoolset-$DEVTOOLSET_VERSION/enable
+fi
 
 # If the user provided an argument then run as a command.
 if [ ${#@} -gt 0 ]; then
