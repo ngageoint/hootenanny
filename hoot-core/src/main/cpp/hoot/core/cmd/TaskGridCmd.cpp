bb60f1ede696027f83757705ed4f0d70e69f3daf
Brandon Witham <brandon.witham@maxar.com>
2021-06-18 15:16:04 -0400

    simplify task-grid command

--- hoot-core/src/main/cpp/hoot/core/cmd/TaskGridCmd.cpp
+++ hoot-core/src/main/cpp/hoot/core/cmd/TaskGridCmd.cpp
@@ -55,6 +55,7 @@ public:
 
   int runSimple(QStringList& args) override
   {
+    LOG_VARD(args);
     if (args.contains("--uniform") && args.contains("--node-density"))
     {
       throw IllegalArgumentException(
@@ -77,71 +78,89 @@ public:
       args.removeAt(args.indexOf("--node-density"));
     }
 
-    if (!isUniformGrid && args.size() < 2)
+    QElapsedTimer timer;
+    timer.start();
+
+    if (!isUniformGrid)
     {
-      std::cout << getHelp() << std::endl << std::endl;
-      throw IllegalArgumentException(
-        QString("%1 takes at least two parameters with the --node-density option.").arg(getName()));
+      _processNodeDensityGrid(args);
     }
-    else if (isUniformGrid && args.size() < 3)
+    else
+    {
+      _processUniformGrid(args);
+    }
+
+    LOG_STATUS(
+      "Task grid generated in " << StringUtils::millisecondsToDhms(timer.elapsed()) << " total.");
+
+    return 0;
+  }
+
+private:
+
+  void _processUniformGrid(QStringList& args)
+  {
+    if (args.size() < 3)
     {
       std::cout << getHelp() << std::endl << std::endl;
       throw IllegalArgumentException(
         QString("%1 takes at least three parameters with the --uniform option.").arg(getName()));
     }
+    LOG_VARD(args);
 
-    QStringList inputs;
-    QString bounds;
-    const QString input = args[0];
-    if (!isUniformGrid)
+    int gridDimensionSize = 2;
+    bool parseSuccess = false;
+    const int gridDimensionSizeIndex = args.size() - 1;
+    gridDimensionSize = args[gridDimensionSizeIndex].toInt(&parseSuccess);
+    if (!parseSuccess || gridDimensionSize < 2)
     {
-      if (!input.contains(";"))
-      {
-        inputs.append(input);
-      }
-      else
-      {
-        // multiple inputs
-        inputs = input.split(";");
-      }
+      throw IllegalArgumentException(
+        "Invalid grid dimension size value: " + args[2] + ". Must be >= 2.");
+    }
+    args.removeLast();
+    LOG_VARD(gridDimensionSize);
+    LOG_VARD(gridDimensionSizeIndex);
+
+    const int outputIndex = args.size() - 1;
+    const QString output = args[outputIndex];
+    _validateOutput(output);
+    args.removeLast();
+    LOG_VARD(output);
+
+    LOG_VARD(args);
+
+    // Everything left is an input.
+    QString bounds;
+    QStringList inputs;
+    if (args.size() == 1 && GeometryUtils::isEnvelopeString(args[0]))
+    {
+      bounds = args[0];
     }
     else
     {
-      if (GeometryUtils::isEnvelopeString(input))
-      {
-        bounds = input;
-      }
-      else if (!input.contains(";"))
-      {
-        inputs.append(input);
-      }
-      else
-      {
-        // multiple inputs
-        inputs = input.split(";");
-      }
+      inputs = args;
     }
 
-    const QString output = args[1];
-    if (!output.toLower().endsWith(".geojson") && !output.toLower().endsWith(".osm"))
+    if (!bounds.trimmed().isEmpty())
     {
-      throw IllegalArgumentException(
-        "Invalid output file format: " + output + ". Only the GeoJSON (.geojson) and OSM " +
-        "(.osm) output formats are supported.");
+      LOG_STATUS(
+        "Generating uniform task grid over " << bounds << " and writing output to ..." <<
+        output << "...");
+      /*TaskGrid taskGrid =*/
+        UniformTaskGridGenerator(bounds, gridDimensionSize, output).generateTaskGrid();
     }
-
-    int gridDimensionSize = 2;
-    if (isUniformGrid)
+    else
     {
-      bool parseSuccess = false;
-      gridDimensionSize = args[2].toInt(&parseSuccess);
-      if (!parseSuccess || gridDimensionSize < 2)
-      {
-        throw IllegalArgumentException(
-          "Invalid grid dimension size value: " + args[2] + ". Must be >= 2.");
-      }
+      LOG_STATUS(
+        "Generating uniform task grid for " << inputs.size() <<
+        " inputs and writing output to ..." << output << "...");
+      /*TaskGrid taskGrid =*/
+        UniformTaskGridGenerator(inputs, gridDimensionSize, output).generateTaskGrid();
     }
+  }
 
+  void _processNodeDensityGrid(QStringList& args)
+  {
     // optional parameters
 
     int maxNodesPerCell = 1000;
@@ -149,152 +168,151 @@ public:
     int maxAttempts = 3;
     int maxTimePerAttempt = -1;
     int pixelSizeAutoReductionFactor = 10;
+    QString bounds;
+
     // TODO: implement random for uniform task grids as well?
     int randomSeed = -1;
-    if (!isUniformGrid)
+    if (args.contains("--bounds"))
     {
-      if (args.contains("--bounds"))
-      {
-        const int optionNameIndex = args.indexOf("--bounds");
-        bounds = args.at(optionNameIndex + 1).trimmed();
-        args.removeAt(optionNameIndex + 1);
-        args.removeAt(optionNameIndex);
-      }
-      LOG_VARD(bounds);
-      if (args.contains("--maxNodesPerCell"))
-      {
-        const int optionNameIndex = args.indexOf("--maxNodesPerCell");
-        bool parseSuccess = false;
-        const QString optionStrVal = args.at(optionNameIndex + 1).trimmed();
-        maxNodesPerCell = optionStrVal.toInt(&parseSuccess);
-        if (!parseSuccess || maxNodesPerCell < 1)
-        {
-          throw IllegalArgumentException(
-            "Invalid maximum node count per cell value: " + optionStrVal);
-        }
-        args.removeAt(optionNameIndex + 1);
-        args.removeAt(optionNameIndex);
-      }
-      LOG_VARD(maxNodesPerCell);
-      if (args.contains("--pixel-size"))
-      {
-        const int optionNameIndex = args.indexOf("--pixel-size");
-        bool parseSuccess = false;
-        const QString optionStrVal = args.at(optionNameIndex + 1).trimmed();
-        pixelSize = optionStrVal.toDouble(&parseSuccess);
-        if (!parseSuccess || pixelSize <= 0.0)
-        {
-          throw IllegalArgumentException("Invalid pixel size value: " + optionStrVal);
-        }
-        args.removeAt(optionNameIndex + 1);
-        args.removeAt(optionNameIndex);
-      }
-      LOG_VARD(pixelSize);
-      if (args.contains("--maxAttempts"))
-      {
-        const int optionNameIndex = args.indexOf("--maxAttempts");
-        bool parseSuccess = false;
-        const QString optionStrVal = args.at(optionNameIndex + 1).trimmed();
-        maxAttempts = optionStrVal.toInt(&parseSuccess);
-        if (!parseSuccess || maxAttempts < 1)
-        {
-          throw IllegalArgumentException("Invalid maximum attempts value: " + optionStrVal);
-        }
-        args.removeAt(optionNameIndex + 1);
-        args.removeAt(optionNameIndex);
-      }
-      LOG_VARD(maxAttempts);
-      if (args.contains("--maxTimePerAttempt"))
-      {
-        const int optionNameIndex = args.indexOf("--maxTimePerAttempt");
-        bool parseSuccess = false;
-        const QString optionStrVal = args.at(optionNameIndex + 1).trimmed();
-        maxTimePerAttempt = optionStrVal.toInt(&parseSuccess);
-        if (!parseSuccess || maxTimePerAttempt < -1)
-        {
-          throw IllegalArgumentException("Invalid maximum time per attempt value: " + optionStrVal);
-        }
-        args.removeAt(optionNameIndex + 1);
-        args.removeAt(optionNameIndex);
-      }
-      LOG_VARD(maxTimePerAttempt);
-      if (args.contains("--pixelSizeReductionFactor"))
-      {
-        const int optionNameIndex = args.indexOf("--pixelSizeReductionFactor");
-        bool parseSuccess = false;
-        const QString optionStrVal = args.at(optionNameIndex + 1).trimmed();
-        pixelSizeAutoReductionFactor = optionStrVal.toInt(&parseSuccess);
-        if (!parseSuccess || pixelSizeAutoReductionFactor < 1)
-        {
-          throw IllegalArgumentException(
-            "Invalid pixel size automatic reduction factor value: " + optionStrVal);
-        }
-        args.removeAt(optionNameIndex + 1);
-        args.removeAt(optionNameIndex);
-      }
-      LOG_VARD(pixelSizeAutoReductionFactor);
-
-      if (args.contains("--random") && args.contains("--randomSeed"))
-      {
-        const int optionNameIndex = args.indexOf("--randomSeed");
-        bool parseSuccess = false;
-        const QString optionStrVal = args.at(optionNameIndex + 1).trimmed();
-        randomSeed = optionStrVal.toInt(&parseSuccess);
-        if (!parseSuccess || randomSeed < -1)
-        {
-          throw IllegalArgumentException("Invalid random seed value: " + optionStrVal);
-        }
-        args.removeAt(optionNameIndex + 1);
-        args.removeAt(optionNameIndex);
-      }
-      LOG_VARD(randomSeed);
+      const int optionNameIndex = args.indexOf("--bounds");
+      bounds = args.at(optionNameIndex + 1).trimmed();
+      args.removeAt(optionNameIndex + 1);
+      args.removeAt(optionNameIndex);
     }
-
-    QElapsedTimer timer;
-    timer.start();
-
-    if (!isUniformGrid)
+    LOG_VARD(bounds);
+    if (args.contains("--maxNodesPerCell"))
     {
-      LOG_STATUS(
-        "Generating node density based task grid for " << inputs.size() << " inputs and " <<
-        "writing output to ..." << output << "...");
-
-      NodeDensityTaskGridGenerator taskGridGen(inputs, maxNodesPerCell, bounds, output);
-      taskGridGen.setReadInputFullThenCrop(false); //??
-      taskGridGen.setPixelSize(pixelSize);
-      taskGridGen.setPixelSizeRetryReductionFactor(pixelSizeAutoReductionFactor);
-      taskGridGen.setMaxNumTries(maxAttempts);
-      taskGridGen.setMaxTimePerAttempt(maxTimePerAttempt);
-      taskGridGen.setWriteRandomCellOnly(args.contains("--random"));
-      taskGridGen.setRandomSeed(randomSeed);
-      /*TaskGrid taskGrid =*/ taskGridGen.generateTaskGrid();
+      const int optionNameIndex = args.indexOf("--maxNodesPerCell");
+      bool parseSuccess = false;
+      const QString optionStrVal = args.at(optionNameIndex + 1).trimmed();
+      maxNodesPerCell = optionStrVal.toInt(&parseSuccess);
+      if (!parseSuccess || maxNodesPerCell < 1)
+      {
+        throw IllegalArgumentException(
+          "Invalid maximum node count per cell value: " + optionStrVal);
+      }
+      args.removeAt(optionNameIndex + 1);
+      args.removeAt(optionNameIndex);
     }
-    else
+    LOG_VARD(maxNodesPerCell);
+    if (args.contains("--pixelSize"))
     {
-      if (!bounds.trimmed().isEmpty())
+      const int optionNameIndex = args.indexOf("--pixelSize");
+      bool parseSuccess = false;
+      const QString optionStrVal = args.at(optionNameIndex + 1).trimmed();
+      pixelSize = optionStrVal.toDouble(&parseSuccess);
+      if (!parseSuccess || pixelSize <= 0.0)
       {
-        LOG_STATUS(
-          "Generating uniform task grid over " << bounds << " and writing output to ..." <<
-          output << "...");
-
-        /*TaskGrid taskGrid =*/
-          UniformTaskGridGenerator(bounds, gridDimensionSize, output).generateTaskGrid();
+        throw IllegalArgumentException("Invalid pixel size value: " + optionStrVal);
       }
-      else
-      {
-        LOG_STATUS(
-          "Generating uniform task grid for " << inputs.size() <<
-          " inputs and writing output to ..." << output << "...");
-
-        /*TaskGrid taskGrid =*/
-          UniformTaskGridGenerator(inputs, gridDimensionSize, output).generateTaskGrid();
-      }
+      args.removeAt(optionNameIndex + 1);
+      args.removeAt(optionNameIndex);
     }
+    LOG_VARD(pixelSize);
+    if (args.contains("--maxAttempts"))
+    {
+      const int optionNameIndex = args.indexOf("--maxAttempts");
+      bool parseSuccess = false;
+      const QString optionStrVal = args.at(optionNameIndex + 1).trimmed();
+      maxAttempts = optionStrVal.toInt(&parseSuccess);
+      if (!parseSuccess || maxAttempts < 1)
+      {
+        throw IllegalArgumentException("Invalid maximum attempts value: " + optionStrVal);
+      }
+      args.removeAt(optionNameIndex + 1);
+      args.removeAt(optionNameIndex);
+    }
+    LOG_VARD(maxAttempts);
+    if (args.contains("--maxTimePerAttempt"))
+    {
+      const int optionNameIndex = args.indexOf("--maxTimePerAttempt");
+      bool parseSuccess = false;
+      const QString optionStrVal = args.at(optionNameIndex + 1).trimmed();
+      maxTimePerAttempt = optionStrVal.toInt(&parseSuccess);
+      if (!parseSuccess || maxTimePerAttempt < -1)
+      {
+        throw IllegalArgumentException("Invalid maximum time per attempt value: " + optionStrVal);
+      }
+      args.removeAt(optionNameIndex + 1);
+      args.removeAt(optionNameIndex);
+    }
+    LOG_VARD(maxTimePerAttempt);
+    if (args.contains("--pixelSizeReductionFactor"))
+    {
+      const int optionNameIndex = args.indexOf("--pixelSizeReductionFactor");
+      bool parseSuccess = false;
+      const QString optionStrVal = args.at(optionNameIndex + 1).trimmed();
+      pixelSizeAutoReductionFactor = optionStrVal.toInt(&parseSuccess);
+      if (!parseSuccess || pixelSizeAutoReductionFactor < 1)
+      {
+        throw IllegalArgumentException(
+          "Invalid pixel size automatic reduction factor value: " + optionStrVal);
+      }
+      args.removeAt(optionNameIndex + 1);
+      args.removeAt(optionNameIndex);
+    }
+    LOG_VARD(pixelSizeAutoReductionFactor);
+
+    // Being lenient here in case --randomSeed was specified but --random was forgotten.
+    const bool random = args.contains("--random") || args.contains("--randomSeed");
+    if (args.contains("--randomSeed"))
+    {
+      const int optionNameIndex = args.indexOf("--randomSeed");
+      bool parseSuccess = false;
+      const QString optionStrVal = args.at(optionNameIndex + 1).trimmed();
+      randomSeed = optionStrVal.toInt(&parseSuccess);
+      if (!parseSuccess || randomSeed < -1)
+      {
+        throw IllegalArgumentException("Invalid random seed value: " + optionStrVal);
+      }
+      args.removeAt(optionNameIndex + 1);
+      args.removeAt(optionNameIndex);
+    }
+    if (args.contains("--random"))
+    {
+      args.removeAt(args.indexOf("--random"));
+    }
+    LOG_VARD(randomSeed);
+
+    if (args.size() < 2)
+    {
+      std::cout << getHelp() << std::endl << std::endl;
+      throw IllegalArgumentException(
+        QString("%1 takes at least two parameters with the --node-density option.").arg(getName()));
+    }
+    LOG_VARD(args);
+
+    const int outputIndex = args.size() - 1;
+    const QString output = args[outputIndex];
+    args.removeAt(outputIndex);
+    _validateOutput(output);
+
+    // Everything left is an input.
+    const QStringList inputs = args;
 
     LOG_STATUS(
-      "Task grid generated in " << StringUtils::millisecondsToDhms(timer.elapsed()) << " total.");
+      "Generating node density based task grid for " << inputs.size() << " inputs and " <<
+      "writing output to ..." << output << "...");
 
-    return 0;
+    NodeDensityTaskGridGenerator taskGridGen(inputs, maxNodesPerCell, bounds, output);
+    taskGridGen.setReadInputFullThenCrop(false); //??
+    taskGridGen.setPixelSize(pixelSize);
+    taskGridGen.setPixelSizeRetryReductionFactor(pixelSizeAutoReductionFactor);
+    taskGridGen.setMaxNumTries(maxAttempts);
+    taskGridGen.setMaxTimePerAttempt(maxTimePerAttempt);
+    taskGridGen.setWriteRandomCellOnly(random);
+    taskGridGen.setRandomSeed(randomSeed);
+    /*TaskGrid taskGrid =*/ taskGridGen.generateTaskGrid();
+  }
+
+  void _validateOutput(const QString& output)
+  {
+    if (!output.toLower().endsWith(".geojson") && !output.toLower().endsWith(".osm"))
+    {
+      throw IllegalArgumentException(
+        "Invalid output file format: " + output + ". Only the GeoJSON (.geojson) and OSM " +
+        "(.osm) output formats are supported.");
+    }
   }
 };
 
