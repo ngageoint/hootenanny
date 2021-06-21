dc220e4a9676eff7bcf95e0ac43b56ec92bf0188
Brandon Witham <brandon.witham@maxar.com>
2021-06-21 10:02:02 -0400

    update test

--- test-files/cmd/slow/CleanCmdTest.sh
+++ test-files/cmd/slow/CleanCmdTest.sh
@@ -13,12 +13,11 @@ GOLD_FILE_MULTIPLE=$IN_DIR/cleaned-multiple.osm
 INPUT_MULTIPLE="test-files/DcGisRoads.osm test-files/DcTigerRoads.osm"
 OUTPUT_FILE_MULTIPLE=$OUT_DIR/cleaned-multiple.osm
 
+RECURSIVE_INPUT=test-files/cmd/slow/CountCmdTest/inputDir
 GOLD_FILE_RECURSIVE_1=$IN_DIR/cleaned-recursive-1.osm
 GOLD_FILE_RECURSIVE_2=$IN_DIR/cleaned-recursive-2.osm
 OUTPUT_FILE_RECURSIVE_1=$OUT_DIR/cleaned-recursive-1.osm
 OUTPUT_FILE_RECURSIVE_2=$OUT_DIR/cleaned-recursive-2.osm
-
-RECURSIVE_INPUT=test-files/cmd/slow/CountCmdTest/inputDir
 
 CONFIG="-C Testing.conf"
 LOG_LEVEL="--warn"
@@ -37,6 +36,7 @@ hoot diff $LOG_LEVEL $CONFIG $GOLD_FILE_MULTIPLE $OUTPUT_FILE_MULTIPLE || diff $
 
 echo ""
 echo "Cleaning recursively in a directory structure..."
+echo ""
 # Since the data in the dir is duplicated and cleaning removes duplicate features, the output here is just that of
 # a single input.
 hoot clean $LOG_LEVEL $CONFIG $RECURSIVE_INPUT $OUTPUT_FILE_RECURSIVE_1 --recursive "*"
@@ -44,6 +44,7 @@ hoot diff $LOG_LEVEL $CONFIG $GOLD_FILE_RECURSIVE_1 $OUTPUT_FILE_RECURSIVE_1 || 
 
 echo ""
 echo "Cleaning one file and also recursively in a directory structure with one filter..."
+echo ""
 # The inputs here are from two fairly far apart locations, but all you really need to check is that both
 # areas are in the output.
 hoot clean $LOG_LEVEL $CONFIG $inputfile $RECURSIVE_INPUT $OUTPUT_FILE_RECURSIVE_2 --recursive "*.osm"
