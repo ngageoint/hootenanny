fdcc4a92bd598c23473dd9dfcd0a7558f5e19761
Brandon Witham <brandon.witham@maxar.com>
2021-07-08 13:42:19 -0400

    prelim doc updates

--- scripts/core/make-hoot-josm-java.sh
+++ scripts/core/make-hoot-josm-java.sh
@@ -7,8 +7,8 @@ JOSM_ARTIFACT_ID=`awk -F '[<>]' '/<josm.artifactId>/{print $3}' pom.xml`
 JOSM_VERSION=`awk -F '[<>]' '/<josm.version>/{print $3}' pom.xml`
 JOSM_JAR=$JOSM_ARTIFACT_ID-$JOSM_VERSION.jar
 
-# needs to be done first time for the me jar only
-# wget --quiet http://me-josm-url
+# This needs to be done first time for each new version of a local jar only (not in a public repo).
+# wget --quiet http://josm-jar-url # replace this URL
 # mvn install:install-file -Dfile=/home/vagrant/hoot/tmp/$JOSM_JAR -DgroupId=org.openstreetmap.josm -DartifactId=$JOSM_ARTIFACT_ID -Dversion=$JOSM_VERSION -Dpackaging=jar -DgeneratePom=true
 
 mvn -q package
