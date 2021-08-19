381a0d9d5eb589af2296f450f2c27516bcd1e903
Brandon Witham <brandon.witham@maxar.com>
2021-08-19 12:50:26 -0400

    add doc

--- hoot-core/src/main/cpp/hoot/core/validation/Validator.h
+++ hoot-core/src/main/cpp/hoot/core/validation/Validator.h
@@ -31,34 +31,38 @@ namespace hoot
 {
 
 /**
- * TODO
+ * Interface for implementation by validators performing data validation
  */
 class Validator
 {
 public:
 
   /**
-   * @brief TODO
+   * @brief Enables validation on the validator.
+   *
+   * Some validators have dual purposes and have the need to have validation capabilities toggled.
    */
   virtual void enableValidation() = 0;
 
-  /**
-    * @brief TODO
-    */
+    /**
+     * @brief Disables validation on the validator.
+     *
+     * Some validators have dual purposes and have the need to have validation capabilities toggled.
+     */
   virtual void disableValidation() = 0;
 
   /**
-    * @brief TODO
+    * @brief Returns a message with a description of validation errors
     */
   virtual QString getValidationErrorMessage() const = 0;
 
   /**
-    * @brief TODO
+    * @brief Returns the total number of validation errors encountered
     */
   virtual int getNumValidationErrors() const = 0;
 
   /**
-    * @brief TODO
+    * @brief Returns the total number of features validated
     */
   virtual int getNumFeaturesValidated() const = 0;
 };
