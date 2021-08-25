338af7f42114aa51062bcb813d356930c9fd7818
Matt C. Jackson <matthew.jackson@digitalglobe.com>
2021-08-25 11:11:05 -0400

    Very basic Dockerfile for a Nightly container
    refs: #4981

---
+++ docker/hoot_commandline_nightly/build_image.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Really basic script to build a container
+IMGNAME=hoot_core_nightly
+IMGVER=0.1
+
+BASEDIR=$(dirname "$0")
+
+if [[ "$(docker images -q ${IMGNAME}:latest)" == "" ]]; then
+  echo "Building the image"
+  docker build \
+  	--build-arg DEV_UID=$(id -u) \
+  	--build-arg DEV_GID=$(id -g) \
+  	-t ${IMGNAME}:${IMGVER} ${BASEDIR}
+
+  echo "Tagging the new image"
+  docker tag ${IMGNAME}:${IMGVER} ${IMGNAME}:latest
+else
+  echo "Image exists: ${IMGNAME}:latest"
+fi
