5692fbe52bf964ae033236d195c01a1e3b20e66b
jordanmurray35 <jordanmurray5@gmail.com>
2021-10-12 15:12:10 -0400

    remove references to sherlock

--- scripts/docker/CommandsAcrossContainer.py
+++ scripts/docker/CommandsAcrossContainer.py
@@ -13,9 +13,9 @@ def my_callback_fn(context, future):
     print(context, future.result())
 
 #shell2http.register_command(endpoint="saythis", command_name="echo", callback_fn=my_callback_fn, decorators=[])
-shell2http.register_command(endpoint="runsherlockcommands", command_name="./scripts/RunSherlockCommands.sh")
+shell2http.register_command(endpoint="RunCommandsAcrossContainers", command_name="./scripts/docker/RunCommandsAcrossContainers.sh")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
     app.testing = True
-    
+
--- scripts/docker/RunCommandsAcrossContainers.sh
+++ scripts/docker/RunCommandsAcrossContainers.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # This file is part of Hootenanny
-# This file is responsible for executing Sherlock hoot commands across containers
-
-# The line below exists until hoot team is able to fix current bug
-export LD_LIBRARY_PATH=/opt/python39/lib:/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.302.b08-0.el7_9.x86_64/jre/lib/amd64/server/
+# This file is responsible for executing hoot commands across containers
 
 # Commands can be added here
 # TODO: create functions for different commands when they are added and add flag to specify which command to run
