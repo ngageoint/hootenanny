3bef8605ac840f460b33b091b630b0eb68d07acd
Matt C. Jackson <matthew.jackson@digitalglobe.com>
2021-10-14 08:04:26 -0400

    Doh!  Missed the new directory

---
+++ docker/hoot_external_command_nightly/README.md
@@ -0,0 +1,7 @@
+# Overview
+
+This Dockerfile builds an image with the latest nightly version of the Hootenanny core from RPM's and sets up Flask for running conflations on command.
+
+NOTE:
+* This **does not** include the UI
+* This **does not** setup a user in the container.
---
+++ docker/hoot_external_command_nightly/run_image.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+# Really basic run script for the hoot core image
+IMGNAME=hoot_external_command
+
+TIMESTAMP=$( date +"%s" )
+CONTNAME="hoot_${TIMESTAMP}"
+VERSION=latest
+
+#####
+if [[ "$(docker images -q ${IMGNAME}:latest)" == "" ]]; then
+  echo "ERROR: The ${IMGNAME}:latest docker image does not exist!"
+else
+  # Add more -v directories as needed
+  docker run --rm -it \
+   --name=${CONTNAME} \
+   --shm-size=1g \
+   ${IMGNAME}:latest
+fi
+
+#   -v ~:/home/dev \
