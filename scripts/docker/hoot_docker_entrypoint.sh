259e5017cb940e41e033c4dca8b0f7cefe4c4dd7
Matt C. Jackson <matthew.jackson@digitalglobe.com>
2021-10-14 08:01:10 -0400

    Re-arranged some files and cleaned up the Dockerfile a bit

--- scripts/docker/hoot_docker_entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# This file is part of Hootenanny 
-# This file is responsible for starting the flask application for communication across containers
-
-echo "Launching Flask application for container communication..."
-flask run --host=0.0.0.0
