a5271c37594d8e64e0dff4f0b806a0722093c6ee
Matt C. Jackson <matthew.jackson@digitalglobe.com>
2021-10-14 09:09:31 -0400

    Fixed comments

--- docker/hoot_external_command_nightly/test_image.sh
+++ docker/hoot_external_command_nightly/test_image.sh
@@ -2,8 +2,8 @@
 set -e
 
 
-# Really basic "smike test" for the container
-# NOTE: You must have the directory with the datafiles mounted onto the container
+# Really basic "smoke test" for the container
+# NOTE: You must have the directory with the datafiles mounted onto the container or this will fail
 
 curl -s -X POST -H 'Content-Type: application/json' \
   -d '{"args": ["/hoot_data/ToyTestA.osm", "/hoot_data/ToyTestB.osm", "/hoot_data/ToyOutput.osm"]}' \
