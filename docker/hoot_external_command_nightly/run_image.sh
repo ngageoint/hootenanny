0c03f52d1450e13651acc22a0811d16db72af556
jordanmurray35 <87077181+jordanmurray35@users.noreply.github.com>
2021-10-15 01:28:02 -0400

    Launch commands from Sherlock (#5072)
    * launch first command across container
    
    * execute sherlock command across container
    
    * establish connection to sherlock container and execute sherlock command
    
    * remove references to sherlock
    
    * Re-arranged some files and cleaned up the Dockerfile a bit
    
    * Doh!  Missed the new directory
    
    * Missed the name change
    
    * another removed comment
    
    * Added a port to the test script
    
    * Added a test script
    
    * Fixed comments
    
    * Moved the dir in the container
    
    * change WORKDIR
    
    Co-authored-by: Matt C. Jackson <matthew.jackson@digitalglobe.com>

---
+++ docker/hoot_external_command_nightly/run_image.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# Really basic run script for the hoot core image
+IMGNAME=hoot_external_command
+
+TIMESTAMP=$( date +"%s" )
+CONTNAME="hoot_${TIMESTAMP}"
+VERSION=latest
+
+#####
+if [[ "$(docker images -q ${IMGNAME}:latest)" == "" ]]; then
+  echo "ERROR: The ${IMGNAME}:latest docker image does not exist!"
+else
+  # Add more -v directories as needed
+  docker run --rm -it \
+   -p 5000:5000 \
+   --name=${CONTNAME} \
+   --shm-size=1g \
+   -v ${PWD}:/hoot_data \
+   ${IMGNAME}:latest
+fi
+
