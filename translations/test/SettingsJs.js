4cb533a1614e6e3ca3bdde2f2375ec00ea960dd0
Brandon Witham <brandon.witham@maxar.com>
2021-10-19 16:05:34 -0400

    fix test

--- translations/test/SettingsJs.js
+++ translations/test/SettingsJs.js
@@ -9,34 +9,61 @@ describe('SettingsJs', function() {
 
    it('Should append a value to a configuration list option', function() {
     
+      var defaultConflatePostOpsStr =  hoot.get('conflate.post.ops');
+      var conflatePostOps = defaultConflatePostOpsStr.split(";");
+      assert.equal(false, conflatePostOps.includes('RailwayOneToManySecondaryMatchElementRemover'));
+      
       hoot.appendToList({'conflate.post.ops': 'RailwayOneToManySecondaryMatchElementRemover'}, false);
-      var conflatePostOps = hoot.get('conflate.post.ops');
+      conflatePostOps = hoot.get('conflate.post.ops').split(";");
       assert.equal('RailwayOneToManySecondaryMatchElementRemover', conflatePostOps[conflatePostOps.length - 1]);
+      
+      hoot.set({'conflate.post.ops': defaultConflatePostOpsStr});
 
     }).timeout(5000);
     
     it('Should prepend a value to a configuration list option', function() {
     
+      var defaultConflatePostOpsStr =  hoot.get('conflate.post.ops');
+      var conflatePostOps = defaultConflatePostOpsStr.split(";");
+      assert.equal(false, conflatePostOps.includes('RailwayOneToManySecondaryMatchElementRemover'));
+      
       hoot.prependToList({'conflate.post.ops': 'RailwayOneToManySecondaryMatchElementRemover'}, false);
-      var conflatePostOps = hoot.get('conflate.post.ops');
+      conflatePostOps = hoot.get('conflate.post.ops').split(";");
       assert.equal('RailwayOneToManySecondaryMatchElementRemover', conflatePostOps[0]);
+      
+      hoot.set({'conflate.post.ops': defaultConflatePostOpsStr});
 
     }).timeout(5000);
     
     it('Should remove a value from a configuration list option', function() {
     
+      var defaultConflatePostOpsStr =  hoot.get('conflate.post.ops');
+      var conflatePostOps = defaultConflatePostOpsStr.split(";");
+      assert.equal(true, conflatePostOps.includes('DuplicateWayRemover'));
+      
       hoot.removeFromList({'conflate.post.ops': 'DuplicateWayRemover'});
-      var conflatePostOps = hoot.get('conflate.post.ops');
-      assert.equal(false, conflatePostOps.includes('DuplicateWayRemover');
+      conflatePostOps = hoot.get('conflate.post.ops').split(";");
+      assert.equal(false, conflatePostOps.includes('DuplicateWayRemover'));
+      
+      hoot.set({'conflate.post.ops': defaultConflatePostOpsStr});
 
     }).timeout(5000);
     
     it('Should replace a value in a configuration list option', function() {
     
-      hoot.replaceInList({'conflate.post.ops': 'DuplicateWayRemover'}, 'RailwayOneToManySecondaryMatchElementRemover');
-      var conflatePostOps = hoot.get('conflate.post.ops');
-      assert.equal(false, conflatePostOps.includes('DuplicateWayRemover');
-      assert.equal(true, conflatePostOps.includes('RailwayOneToManySecondaryMatchElementRemover');
+      var defaultConflatePostOpsStr =  hoot.get('conflate.post.ops');
+      var conflatePostOps = defaultConflatePostOpsStr.split(";");
+      assert.equal(true, conflatePostOps.includes('DuplicateWayRemover'));
+      assert.equal(false, conflatePostOps.includes('RailwayOneToManySecondaryMatchElementRemover'));
+      assert.equal(false, conflatePostOps.includes('RubberSheet'));
+      
+      hoot.replaceInList({'conflate.post.ops': 'DuplicateWayRemover->RailwayOneToManySecondaryMatchElementRemover;RemoveMissingElementsVisitor->RubberSheet'});
+      conflatePostOps = hoot.get('conflate.post.ops').split(";");
+      assert.equal(false, conflatePostOps.includes('DuplicateWayRemover'));
+      assert.equal(true, conflatePostOps.includes('RailwayOneToManySecondaryMatchElementRemover'));
+      assert.equal(true, conflatePostOps.includes('RubberSheet'));
+      
+      hoot.set({'conflate.post.ops': defaultConflatePostOpsStr});
 
     }).timeout(5000);
 
