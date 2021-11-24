661aa561392ead7e669228762c12fe6321103f78
Brian Hatchl <brian.hatchl@radiantsolutions.com>
2021-11-23 20:06:42 -0500

    Move option to use source ids from Attribute conf to adv opts General… (#5159)
    * move option to use source ids from Attribute conf to adv opts General section
    
    fixes #5157
    
    * add params moved out of Attribute conf into test config
    
    * add source id params to other attribute conflate tests

--- test-files/cmd/slow/AreaAttributeConflateStandaloneTest.sh
+++ test-files/cmd/slow/AreaAttributeConflateStandaloneTest.sh
@@ -9,7 +9,10 @@ mkdir -p $OUT_DIR
 CONFIG="--warn -C Testing.conf"
 source scripts/core/ScriptTestUtils.sh
 
-hoot conflate $CONFIG -C UnifyingAlgorithm.conf -C AttributeConflation.conf -D uuid.helper.repeatable=true -D match.creators="ScriptMatchCreator,Area.js" -D merger.creators="ScriptMergerCreator" test-files/cmd/glacial/PoiPolygonConflateStandaloneTest/PoiPolygon1.osm test-files/cmd/glacial/PoiPolygonConflateStandaloneTest/PoiPolygon2.osm $OUT_DIR/output.osm
+hoot conflate $CONFIG -C UnifyingAlgorithm.conf -C AttributeConflation.conf \
+  -D conflate.use.data.source.ids.1="false" -D conflate.use.data.source.ids.2="true" \
+  -D uuid.helper.repeatable=true -D match.creators="ScriptMatchCreator,Area.js" -D merger.creators="ScriptMergerCreator" \
+  test-files/cmd/glacial/PoiPolygonConflateStandaloneTest/PoiPolygon1.osm test-files/cmd/glacial/PoiPolygonConflateStandaloneTest/PoiPolygon2.osm $OUT_DIR/output.osm
 hoot diff $CONFIG $IN_DIR/output.osm $OUT_DIR/output.osm || diff $IN_DIR/output.osm $OUT_DIR/output.osm
 
 validateTestOutput $OUT_DIR/output.osm $OUT_DIR/output-validation-report \
