c8570996ed2f2229e42d1f2c1cdf754e82445ea1
Micah Schicker <micah.schicker@maxar.com>
2022-04-14 21:15:36 -0400

    Revert "Lock npm to 6.14.12 during vagrant provisioning" Pushed to the wrong branch.
    This reverts commit f1ed7e373c8848042afd4b2430d1f91a0f44129d.

--- VagrantProvisionCentOS7.sh
+++ VagrantProvisionCentOS7.sh
@@ -90,7 +90,6 @@ else
          libphonenumber-devel \
          nodejs \
          nodejs-devel \
-	 npm \
          proj \
          proj-devel \
          stxxl \
@@ -115,7 +114,6 @@ sudo yum install -y \
      libphonenumber-devel-$LIBPHONENUMBER_VERSION \
      liboauthcpp-$LIBOAUTHCPP_VERSION \
      liboauthcpp-devel-$LIBOAUTHCPP_VERSION \
-     npm-$NPM_VERSION \
      nodejs-$NODE_VERSION \
      nodejs-devel-$NODE_VERSION \
      proj-$PROJ_VERSION \
@@ -141,7 +139,6 @@ sudo yum versionlock add \
      libphonenumber-devel-$LIBPHONENUMBER_VERSION \
      liboauthcpp-$LIBOAUTHCPP_VERSION \
      liboauthcpp-devel-$LIBOAUTHCPP_VERSION \
-     npm-$NPM_VERSION \
      nodejs-$NODE_VERSION \
      nodejs-devel-$NODE_VERSION \
      proj-$PROJ_VERSION \
