e7c874aaca5cf4082b4ceee8f1a618674c01371c
Brian Hatchl <brian.hatchl@maxar.com>
2022-06-24 09:21:43 -0400

    remove attempt to order filters

--- hoot-services/src/main/java/hoot/services/SpringWebContainerInitializer.java
+++ hoot-services/src/main/java/hoot/services/SpringWebContainerInitializer.java
@@ -30,16 +30,12 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
-import org.springframework.core.annotation.Order;
 import org.springframework.session.jdbc.config.annotation.web.http.JdbcHttpSessionConfiguration;
 import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
-import org.springframework.stereotype.Component;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
-@Component
-@Order(1)
 public class SpringWebContainerInitializer extends AbstractHttpSessionApplicationInitializer {
 
     @Override
