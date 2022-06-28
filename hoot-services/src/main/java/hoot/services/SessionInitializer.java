8541711183c5b9ae55bdd76111dc4cd136175243
Brian Hatchl <brian.hatchl@maxar.com>
2022-06-28 08:36:39 -0400

    remove session initializer
    used when trying order of session vs security initializer

--- hoot-services/src/main/java/hoot/services/SessionInitializer.java
+++
@@ -1,7 +0,0 @@
-package hoot.services;
-
-import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
-
-public class SessionInitializer extends AbstractHttpSessionApplicationInitializer {
-
-}
