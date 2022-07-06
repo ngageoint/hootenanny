2003ef2406fdd27b4f6d78639be3a19a521ce97d
Ben Marchant <13385275+bmarchant@users.noreply.github.com>
2022-07-05 22:36:01 -0500

    Enable coverage fixes and other miscellaneous fixes (#5384)
    * Enable coverage fixes and other miscellaneous fixes
    * AWS provider update

--- scripts/EnableCoverage.sh
+++ scripts/EnableCoverage.sh
@@ -5,10 +5,10 @@ set -e
 # Enable coverage in VagrantBuild script
 sed -i 's/--with-uitests/--with-uitests --with-coverage/g' $HOOT_HOME/VagrantBuild.sh
 # Update compiler optimization flags for use with building coverage
-sed -i 's/QMAKE_CXXFLAGS+=-O3/QMAKE_CXXFLAGS+=-O/' $HOOT_HOME/LocalConfig.pri.orig
+sed -i 's/QMAKE_CXXFLAGS+=-O3/#QMAKE_CXXFLAGS+=-O3/' $HOOT_HOME/LocalConfig.pri.orig
 if [ -w $HOOT_HOME/LocalConfig.pri ]
 then
-  sed -i 's/QMAKE_CXXFLAGS+=-O3/QMAKE_CXXFLAGS+=-O/' $HOOT_HOME/LocalConfig.pri
+  sed -i 's/QMAKE_CXXFLAGS+=-O3/#QMAKE_CXXFLAGS+=-O3/' $HOOT_HOME/LocalConfig.pri
 fi
 
 exit 0
