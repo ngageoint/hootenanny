ab829830f2677a82047bd0c2ec674ecf7af12e49
Ben Marchant <13385275+bmarchant@users.noreply.github.com>
2022-10-11 10:04:06 -0500

    Copyright

--- hoot-core/src/main/cpp/hoot/core/elements/WayData.h
+++ hoot-core/src/main/cpp/hoot/core/elements/WayData.h
@@ -22,7 +22,7 @@
  * This will properly maintain the copyright information. Maxar
  * copyrights will be updated automatically.
  *
- * @copyright Copyright (C) 2015, 2016, 2017, 2018, 2019, 2020, 2021 Maxar (http://www.maxar.com/)
+ * @copyright Copyright (C) 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022 Maxar (http://www.maxar.com/)
  */
 #ifndef __ELEMENT_WAY_DATA_H__
 #define __ELEMENT_WAY_DATA_H__
