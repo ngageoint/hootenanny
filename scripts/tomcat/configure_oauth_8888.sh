8ac7a6724c0adaeec6a8fed8a8521c7fcbf772f2
Brian Hatchl <brian.hatchl@maxar.com>
2023-01-19 13:50:48 -0500

    don't specify vagrant user to run this
    this script is used by both default and hoot_centos7_rpm vms
    which have diff users (vagrant vs tomcat) respectively
    so just sudo it without user

--- scripts/tomcat/configure_oauth_8888.sh
+++ scripts/tomcat/configure_oauth_8888.sh
@@ -3,6 +3,6 @@
 set -e
 
 # Configure OAuth2 redirect for port 8888
-sudo -u vagrant sed -i "\|https://localhost:8080/login.html|s|^|# |" /var/lib/tomcat8/webapps/hoot-services/WEB-INF/classes/oauth2-client.properties
-sudo -u vagrant sed -i "\|https://localhost:8443/hootenanny-id/login.html|s|^# ||" /var/lib/tomcat8/webapps/hoot-services/WEB-INF/classes/oauth2-client.properties
+sudo sed -i "\|https://localhost:8080/login.html|s|^|# |" /var/lib/tomcat8/webapps/hoot-services/WEB-INF/classes/oauth2-client.properties
+sudo sed -i "\|https://localhost:8443/hootenanny-id/login.html|s|^# ||" /var/lib/tomcat8/webapps/hoot-services/WEB-INF/classes/oauth2-client.properties
 sudo systemctl restart tomcat8
