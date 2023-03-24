cbbaa64860f9e61521d7468a8c582284692a0c49
Brian Hatchl <brian.hatchl@maxar.com>
2023-03-24 18:09:44 -0400

    Add a commit date to the About version info (#5610)
    * Add a commit date to the version info
    
    * remove this force check for vagrant marker file
    
    was causing infinite loop in make clean
    not sure why, but also don't think this step is useful anymore
    
    * remove FORCE reference
    
    * update test expectation
    
    * copyright

--- scripts/GenerateBuildInfoFiles.sh
+++ scripts/GenerateBuildInfoFiles.sh
@@ -3,8 +3,11 @@
 HOOT_VERSION=`cat ../HOOT_VERSION_FILE | grep "HOOT_VERSION" | awk '{print $3;}'`
 #echo "HOOT_VERSION: " $HOOT_VERSION
 #echo "USER: " $USER
+HOOT_VERSION_DATE=`cat ../HOOT_VERSION_FILE | grep "HOOT_DATE" | awk '{print $3;}'`
+HOOT_BUILT_BY=`cat ../HOOT_VERSION_FILE | grep "HOOT_BUILT_BY" | awk '{print $3;}'`
 
 SERVICES_BUILD_INFO_FILE=./src/main/resources/build.info
-echo "name=Hootenanny Web Services" > $SERVICES_BUILD_INFO_FILE
+echo "name=Services" > $SERVICES_BUILD_INFO_FILE
 echo "version="$HOOT_VERSION >> $SERVICES_BUILD_INFO_FILE
-echo "user="$USER >> $SERVICES_BUILD_INFO_FILE
+echo "date="$HOOT_VERSION_DATE >> $SERVICES_BUILD_INFO_FILE
+echo "user="$HOOT_BUILT_BY >> $SERVICES_BUILD_INFO_FILE
