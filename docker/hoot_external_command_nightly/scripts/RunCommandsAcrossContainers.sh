97c6de0758a2c4df6f580e7486a91edbe2cd6f30
jordanmurray35 <jordanmurray5@gmail.com>
2023-04-17 11:09:48 -0400

    add comments

--- docker/hoot_external_command_nightly/scripts/RunCommandsAcrossContainers.sh
+++ docker/hoot_external_command_nightly/scripts/RunCommandsAcrossContainers.sh
@@ -5,22 +5,25 @@
 # Commands can be added here
 # TODO: create functions for different commands when they are added and add flag to specify which command to run
 
-
+# Match conflate different inputs
 run-conflate()
 {
     hoot conflate -C UnifyingAlgorithm.conf -C ReferenceConflation.conf -D conflate.match.only=true -D writer.include.conflate.score.tags=true -D match.creators="BuildingMatchCreator;HighwayMatchCreator" -D writer.include.debug.tags=true -D uuid.helper.repeatable=true -D conflate.post.ops-="WayJoinerOp" -D conflate.tag.disable.value.truncation=true -D building.match.threshold=$HOOT_MATCH_THRESH -D building.review.threshold=$HOOT_REVIEW_THRESH $HOOT_REF_FILE_NAME $HOOT_TARGET_FILE_NAME $HOOT_MATCH_FILE_NAME
 }
 
+# Generate a task grid
 run-task-grid()
 {
     hoot task-grid $FILE_NAME $OUTPUT_GEOJSON --maxNodesPerCell 800000 --node-density
 }
 
+# Split a file
 run-split()
 {
     hoot split $OUTPUT_GEOJSON $FILE_NAME $OUTPUT_SPLIT_OSM
 }
 
+# Translate a directory
 run-translate-directory()
 {
     if [ $AFTER_SPLIT == 0 ] 
@@ -31,11 +34,13 @@ run-translate-directory()
     fi
 }
 
+# Translate a file
 run-translate()
 {
     hoot convert -D schema.translation.script=$TRANSLATION_SCRIPT $FILE_NAME $OUTPUT_FILE
 }
 
+# Import data from OWT overpass using specific bounding box coordinates
 run-owt()
 {
     hoot convert -D bounds=$MIN_LON,$MIN_LAT,$MAX_LON,$MAX_LAT -D overpass.api.query.path=$OQF -D schema.translation.script=$TRANSLATE_SCRIPT \
@@ -47,6 +52,7 @@ run-owt()
         "${OP_URL}" $OUTPUT_OSM
 }
 
+# Import data from OWT overpass using a bounding box file
 run-owt-bounds()
 {
     hoot convert -D bounds.input.file=$BOUNDING_BOX -D overpass.api.query.path=$OQF -D schema.translation.script=$TRANSLATE_SCRIPT \
@@ -58,6 +64,7 @@ run-owt-bounds()
         "${OP_URL}" $OUTPUT_OSM
 }
 
+# Export data from the database as an OSM file
 run-owt-osm()
 {
     hoot convert -D schema.translation.script=$TRANSLATE_SCRIPT PG:"dbname=$PGDATABASE host=$PGHOST port=5432 user=$PGUSER schemas=$SCHEMAS" $OUTPUT_OSM
