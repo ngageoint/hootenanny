11d262c91af9efdd3bed7c0f84911334cf8a7f76
jordanmurray35 <87077181+jordanmurray35@users.noreply.github.com>
2023-04-18 11:34:08 -0400

    add ability to provide bounding box file for sherlock capability (#5624)
    * add ability to provide bounding box file
    
    * create env var for api host
    
    * generalize functions
    
    * add comments
    
    * generalize functions

--- docker/hoot_external_command_nightly/scripts/RunCommandsAcrossContainers.sh
+++ docker/hoot_external_command_nightly/scripts/RunCommandsAcrossContainers.sh
@@ -5,22 +5,25 @@
 # Commands can be added here
 # TODO: create functions for different commands when they are added and add flag to specify which command to run
 
-
+# Match conflate different inputs
 run-conflate()
 {
     hoot conflate -C UnifyingAlgorithm.conf -C ReferenceConflation.conf -D conflate.match.only=true -D writer.include.conflate.score.tags=true -D match.creators="BuildingMatchCreator;HighwayMatchCreator" -D writer.include.debug.tags=true -D uuid.helper.repeatable=true -D conflate.post.ops-="WayJoinerOp" -D conflate.tag.disable.value.truncation=true -D building.match.threshold=$HOOT_MATCH_THRESH -D building.review.threshold=$HOOT_REVIEW_THRESH $HOOT_REF_FILE_NAME $HOOT_TARGET_FILE_NAME $HOOT_MATCH_FILE_NAME
 }
 
+# Generate a task grid
 run-task-grid()
 {
     hoot task-grid $FILE_NAME $OUTPUT_GEOJSON --maxNodesPerCell 800000 --node-density
 }
 
+# Split a file
 run-split()
 {
     hoot split $OUTPUT_GEOJSON $FILE_NAME $OUTPUT_SPLIT_OSM
 }
 
+# Translate a directory
 run-translate-directory()
 {
     if [ $AFTER_SPLIT == 0 ] 
@@ -31,23 +34,38 @@ run-translate-directory()
     fi
 }
 
+# Translate a file
 run-translate()
 {
     hoot convert -D schema.translation.script=$TRANSLATION_SCRIPT $FILE_NAME $OUTPUT_FILE
 }
 
-run-owt-import()
+# Import data from overpass instance using specific bounding box coordinates
+run-overpass()
 {
     hoot convert -D bounds=$MIN_LON,$MIN_LAT,$MAX_LON,$MAX_LAT -D overpass.api.query.path=$OQF -D schema.translation.script=$TRANSLATE_SCRIPT \
-        -D hoot.pkcs12.key.path=$CERT_PATH -D hoot.pkcs12.key.phrase=$CERT_PW -D overpass.api.host=owt.maxarmaps.com \
+        -D hoot.pkcs12.key.path=$CERT_PATH -D hoot.pkcs12.key.phrase=$CERT_PW -D overpass.api.host=$OVERPASS_API_HOST \
         "${OP_URL}" ${OUTPUT_GPKG} 
 
     hoot convert -D bounds=$MIN_LON,$MIN_LAT,$MAX_LON,$MAX_LAT -D overpass.api.query.path=$OQF \
-        -D hoot.pkcs12.key.path=$CERT_PATH -D hoot.pkcs12.key.phrase=$CERT_PW -D overpass.api.host=owt.maxarmaps.com \
+        -D hoot.pkcs12.key.path=$CERT_PATH -D hoot.pkcs12.key.phrase=$CERT_PW -D overpass.api.host=$OVERPASS_API_HOST \
         "${OP_URL}" $OUTPUT_OSM
 }
 
-run-owt-export-osm()
+# Import data from overpass instance using a bounding box file
+run-overpass-bounds()
+{
+    hoot convert -D bounds.input.file=$BOUNDING_BOX -D overpass.api.query.path=$OQF -D schema.translation.script=$TRANSLATE_SCRIPT \
+        -D hoot.pkcs12.key.path=$CERT_PATH -D hoot.pkcs12.key.phrase=$CERT_PW -D overpass.api.host=$OVERPASS_API_HOST \
+        "${OP_URL}" ${OUTPUT_GPKG} 
+
+    hoot convert -D bounds.input.file=$BOUNDING_BOX -D overpass.api.query.path=$OQF \
+        -D hoot.pkcs12.key.path=$CERT_PATH -D hoot.pkcs12.key.phrase=$CERT_PW -D overpass.api.host=$OVERPASS_API_HOST \
+        "${OP_URL}" $OUTPUT_OSM
+}
+
+# Export data from the database as an OSM file
+run-export-database-osm()
 {
     hoot convert -D schema.translation.script=$TRANSLATE_SCRIPT PG:"dbname=$PGDATABASE host=$PGHOST port=5432 user=$PGUSER schemas=$SCHEMAS" $OUTPUT_OSM
 }
@@ -91,7 +109,19 @@ case "$1" in
         OP_URL=${10}
         OUTPUT_GPKG=${11}
         OUTPUT_OSM=${12}
-        run-owt-import ;;
+        OVERPASS_API_HOST=${13}
+        run-overpass ;;
+    -b)
+        BOUNDING_BOX=$2
+        OQF=$3
+        TRANSLATE_SCRIPT=$4
+        CERT_PATH=$5
+        CERT_PW=$6
+        OP_URL=$7
+        OUTPUT_GPKG=$8
+        OUTPUT_OSM=$9
+        OVERPASS_API_HOST=${10}
+        run-overpass-bounds ;;
     -e)
         TRANSLATE_SCRIPT=$2
         PGDATABASE=$3
@@ -99,5 +129,5 @@ case "$1" in
         PGUSER=$5
         SCHEMAS=$6
         OUTPUT_OSM=$7
-        run-owt-export-osm ;;
+        run-export-database-osm ;;
 esac
