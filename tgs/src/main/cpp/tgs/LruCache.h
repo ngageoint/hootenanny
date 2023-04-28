9b55f368ecb08a7fc4957afd8250336d59befb9b
Ben Marchant <13385275+bmarchant@users.noreply.github.com>
2023-04-27 21:11:49 -0500

    Copyright

--- tgs/src/main/cpp/tgs/LruCache.h
+++ tgs/src/main/cpp/tgs/LruCache.h
@@ -22,7 +22,7 @@
  * This will properly maintain the copyright information. Maxar
  * copyrights will be updated automatically.
  *
- * @copyright Copyright (C) 2015, 2016, 2018, 2020, 2021 Maxar (http://www.maxar.com/)
+ * @copyright Copyright (C) 2015-2023 Maxar (http://www.maxar.com/)
  */
 #ifndef LRUCACHE_H
 #define LRUCACHE_H
