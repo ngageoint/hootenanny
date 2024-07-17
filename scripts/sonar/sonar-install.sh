43e1dfc94ac5a9deca3f80b1888eb1a10abd01f1
jordanmurray35 <jordanmurray5@gmail.com>
2024-07-17 14:44:46 -0400

    remove debug statements

--- scripts/sonar/sonar-install.sh
+++ scripts/sonar/sonar-install.sh
@@ -40,10 +40,8 @@ if [ ! -f $SONAR_PATH/bin/$SONAR_BLD_PKG-64 ]; then
 fi
 
 # Install Java 17 for the scan only
-echo "before java 17 install"
 wget -q $SONAR_JDK_URL
 sudo yum localinstall -y -q $SONAR_JDK_RPM
-echo "after java 17 install"
 
 # Install an updated version of Maven for Sonar
 wget -q --no-check-certificate $SONAR_MVN_URL
@@ -51,13 +49,8 @@ sudo tar xzf $SONAR_MVN_TAR -C /opt
 sudo ln -s /opt/$SONAR_MVN_VERSION /opt/maven
 
 # Display the installed sonar version
-echo "before java home"
-echo "command output: $(ls /usr/lib/jvm/)"
 JAVA_HOME=$SONAR_JDK_PATH sonar-scanner --version
-echo "command output again: $(ls /usr/lib/jvm/)"
-echo "Sonar JDK Path: $SONAR_JDK_PATH"
 JAVA_HOME=$SONAR_JDK_PATH mvn --version
-echo "after setting java home"
 
 # In order to run correctly in sonar, pre-compiled headers need to be turned off
 # Remove when https://jira.sonarsource.com/browse/CPP-2897 is resolved
