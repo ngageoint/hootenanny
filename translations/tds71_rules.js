ebd763867d1bde7a3efa79d4ca81f4b45b86fefe
E.J. Lane <ej.lane@maxar.com>
2025-02-19 07:49:56 -0500

    landuse:brownfield should be soil surface region, not built up area

--- translations/tds71_rules.js
+++ translations/tds71_rules.js
@@ -63,6 +63,7 @@ tds71.rules = {
   // Note: These get swapped around before being used
   fcodeOne2oneOut : [
     ['F_CODE','DA010','natural','desert'], // EE030 Desert - mapped to DA010 Soil Surface Region
+    ['F_CODE','DA010','landuse','brownfield'], // DA010 Soil Surface Region
     ['F_CODE','FA091','geophysical_data_track','yes'], // Geophysical Data Track Line
     ['F_CODE','GA005','airspace','yes'], // Airspace
     ['F_CODE','GA035','navaid:aeronautical','yes'], // Aeronautical Navaids
