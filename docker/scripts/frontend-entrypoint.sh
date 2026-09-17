ad272b86b97ab4deafceececbcf18d5360c1cbdd
E.J. Lane <ej.lane@maxar.com>
2026-09-17 13:42:38 -0400

    fix for clean builds on updated hoot frontend

--- docker/scripts/frontend-entrypoint.sh
+++ docker/scripts/frontend-entrypoint.sh
@@ -2,7 +2,8 @@
 set -euo pipefail
 
 if [ "${HOOT_NPM_INSTALL:-0}" == "1" ] || [ ! -d node_modules ]; then
-    npm install && npm run all
+    npm ci
+    npm run all
 fi
 
-npm start
+exec npm start
